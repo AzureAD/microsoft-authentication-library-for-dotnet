7bbf65093d0f659156714f2543a3ba86111d0e7b
afshins <afshin.sepehri@microsoft.com>
2014-08-26 12:06:00 -0700

    Adding comment

--- src/ADAL.Common/OAuthConstants.cs
+++ src/ADAL.Common/OAuthConstants.cs
@@ -120,6 +120,9 @@ namespace Microsoft.IdentityModel.Clients.ActiveDirectory
     {
         public const string Login = "login";
         public const string RefreshSession = "refresh_session";
+
+        // The behavior of this value is identical to prompt=none for managed users; However, for federated users, AAD
+        // redirects to ADFS as it cannot determine in advance whether ADFS can login user silently (e.g. via WIA) or not.
         public const string AttemptNone = "attempt_none";        
     }
 }
