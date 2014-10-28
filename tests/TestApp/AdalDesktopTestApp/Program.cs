75792b6cbf5cd12323d3073a5aeb696f18a0e708
afshins <afshin.sepehri@microsoft.com>
2014-10-27 17:34:50 -0700

    Convert samples to tests

---
+++ tests/TestApp/AdalDesktopTestApp/Program.cs
@@ -0,0 +1,51 @@
+﻿//----------------------------------------------------------------------
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+// Apache License 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+using TestApp.PCL;
+
+namespace AdalDesktopTestApp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            AcquireTokenAsync().Wait();
+            Console.ReadKey();
+        }
+
+        private static async Task AcquireTokenAsync()
+        {
+            TokenBroker tokenBroker = new TokenBroker();
+            string token = await tokenBroker.GetTokenInteractiveAsync(new AuthorizationParameters(PromptBehavior.Auto, null));
+            Console.WriteLine(token + "\n");
+            token = await tokenBroker.GetTokenWithUsernamePasswordAsync();
+            Console.WriteLine(token + "\n");
+            token = await tokenBroker.GetTokenWithClientCredentialAsync();
+            Console.WriteLine(token);
+        }
+    }
+}
---
+++ tests/TestApp/TestApp.PCL/TokenBroker.cs
@@ -0,0 +1,98 @@
+﻿//----------------------------------------------------------------------
+// Copyright (c) Microsoft Open Technologies, Inc.
+// All Rights Reserved
+// Apache License 2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Test.ADAL.Common;
+
+namespace TestApp.PCL
+{
+    public class TokenBroker
+    {
+        private AuthenticationContext context;
+
+        private Sts sts;
+
+        public TokenBroker()
+        {
+            this.sts = new AadSts();
+            context = new AuthenticationContext(this.sts.Authority, true, null);            
+        }
+
+        public async Task<string> GetTokenInteractiveAsync(IAuthorizationParameters parameters)
+        {
+            try
+            {
+                var result = await context.AcquireTokenAsync(sts.ValidResource, sts.ValidClientId, sts.ValidNonExistingRedirectUri, parameters, new UserIdentifier(sts.ValidUserName, UserIdentifierType.OptionalDisplayableId));
+
+                return result.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public async Task<string> GetTokenInteractiveWithMsAppAsync(IAuthorizationParameters parameters)
+        {
+            try
+            {
+                var result = await context.AcquireTokenAsync(sts.ValidResource, sts.ValidClientId, null, parameters, new UserIdentifier(sts.ValidUserName, UserIdentifierType.OptionalDisplayableId));
+
+                return result.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public async Task<string> GetTokenWithUsernamePasswordAsync()
+        {
+            try
+            {
+                var result = await context.AcquireTokenAsync(sts.ValidResource, sts.ValidClientId, new UserCredential(sts.ValidUserName, sts.ValidPassword));
+
+                return result.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public async Task<string> GetTokenWithClientCredentialAsync()
+        {
+            try
+            {
+                var result = await context.AcquireTokenAsync(sts.ValidResource, new ClientCredential(sts.ValidConfidentialClientId, sts.ValidConfidentialClientSecret));
+
+                return result.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
