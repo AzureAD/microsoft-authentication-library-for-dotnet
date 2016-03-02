2a59c28c30f290984fea64abefe9937d2343d253
Kanishk Panwar <kpanwar@microsoft.com>
2016-03-02 00:18:48 -0800

    base checkin.

---
+++ tests/Test.MSAL.NET.Unit/PublicClientApplicationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.MSAL.NET.Unit
+{
+    [TestClass]
+    public class PublicClientApplicationTests
+    {
+        [TestMethod]
+        [TestCategory("PublicClientApplicationTests")]
+        public void TestConstructors()
+        {
+        }
+    }
+}
