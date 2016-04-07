75ceba73bb87cdb54a871f4f17965714a76cb5f7
abhishek58g <abgun@microsoft.com>
2016-04-07 10:21:07 -0700

    Code Cleaning :- Adding Appropriate headers #32
    Code Cleaning :- Adding Appropriate headers #32

--- tests/TestApp/UniversalTestApp/UniversalTestApp.Windows/MainPage.xaml.cs
+++ tests/TestApp/UniversalTestApp/UniversalTestApp.Windows/MainPage.xaml.cs
@@ -1,20 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Microsoft.Identity.Client;
-using TestApp.PCL;
-
+﻿//------------------------------------------------------------------------------
 //
 // Copyright (c) Microsoft Corporation.
 // All rights reserved.
@@ -40,6 +24,25 @@ using TestApp.PCL;
 // THE SOFTWARE.
 //
 //------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Identity.Client;
+using TestApp.PCL;
+
+
 namespace UniversalTestApp
 {
     /// <summary>
