236b45a8030131fe181f196d69b83492a93530c2
REDMOND\kpanwar <kpanwar@microsoft.com>
2016-12-29 12:17:36 -0800

    remove userassertionHash from TokenCacheItem #146

--- src/MSAL.PCL/Internal/Requests/OnBehalfOfRequest.cs
+++ src/MSAL.PCL/Internal/Requests/OnBehalfOfRequest.cs
@@ -48,10 +48,6 @@ namespace Microsoft.Identity.Client.Internal.Requests
         protected override async Task SendTokenRequestAsync()
         {
             await base.SendTokenRequestAsync().ConfigureAwait(false);
-            if (Response != null)
-            {
-                Response.UserAssertionHash = AuthenticationRequestParameters.UserAssertion.AssertionHash;
-            }
         }
 
         protected override void SetAdditionalRequestParameters(OAuth2Client client)
