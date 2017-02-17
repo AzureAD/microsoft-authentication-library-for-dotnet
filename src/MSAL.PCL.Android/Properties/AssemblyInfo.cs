1cb2245b68825340537ae91df8bf38faf01d88f5
Oren Novotny <oren@novotny.org>
2017-02-17 08:25:23 -0500

    update key path in assembly info

--- src/MSAL.PCL.Android/Properties/AssemblyInfo.cs
+++ src/MSAL.PCL.Android/Properties/AssemblyInfo.cs
@@ -42,5 +42,4 @@ using System.Runtime.InteropServices;
 
 // Assembly version information is in file MSAL.Common\CommonAssemblyInfo.cs
 
-[assembly: AssemblyDelaySign(true)]
-[assembly: AssemblyKeyFile(@"..\..\build\35MSSharedLib1024.snk")]
+[assembly: AssemblyKeyFile(@"..\..\build\MSAL.snk")]
