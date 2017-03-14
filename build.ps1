caa14f8602c2fe97463879f66f3acbf64bb27003
Bogdan Gavril <bgavril@microsoft.com>
2017-03-14 15:16:46 +0000

    Stop Appveyor from swallowing MSBuild errors

--- build.ps1
+++ build.ps1
@@ -1,3 +1,12 @@
+function ExitOnMSBuildError
+{
+    if ($LastExitCode -ne 0) {
+        # Force the PS script to exit with an error code, signalling the error to AppVeyor 
+        $host.SetShouldExit($LastExitCode)
+        throw "MSBuild failed with code $LastExitCode - see the log for details."
+    }
+}
+
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 $configuration = "Release"
@@ -10,9 +19,13 @@ New-Item -ItemType Directory -Force -Path $artifacts
 
 Write-Host "Restoring packages for $scriptPath\MSAL.sln" -Foreground Green
 msbuild "$scriptPath\MSAL.sln" /m /t:restore /p:Configuration=$configuration 
+ExitOnMSBuildError
+
 
 Write-Host "Building $scriptPath\MSAL.sln" -Foreground Green
-msbuild "$scriptPath\MSAL.sln" /m /t:build /p:Configuration=$configuration 
+msbuild "$scriptPath\MSAL.sln" /m /t:build /p:Configuration=$configuration
+ExitOnMSBuildError
 
 Write-Host "Building Packages" -Foreground Green
 msbuild "$scriptPath\src\Microsoft.Identity.Client\Microsoft.Identity.Client.csproj" /t:pack /p:Configuration=$configuration /p:PackageOutputPath=$artifacts /p:NoPackageAnalysis=true /p:NuGetBuildTasksPackTargets="workaround"
+ExitOnMSBuildError
