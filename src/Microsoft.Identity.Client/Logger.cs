05782f41ca0e3f5e7e8969e255474d8cb0b81078
jennyf19 <ferriesj@gmail.com>
2017-03-14 20:42:30 -0700

    Added XML comments in LogLevel enum in Logger.cs

--- src/Microsoft.Identity.Client/Logger.cs
+++ src/Microsoft.Identity.Client/Logger.cs
@@ -35,9 +35,24 @@ namespace Microsoft.Identity.Client
     {
         public enum LogLevel
         {
+            /// <summary>
+            /// Error Log level
+            /// </summary>
             Error = 0,
+
+            /// <summary>
+            /// Warning Log level
+            /// </summary>
             Warning = 1,
+
+            /// <summary>
+            /// Information Log level
+            /// </summary>
             Info = 2,
+
+            /// <summary>
+            /// Verbose Log level
+            /// </summary>
             Verbose = 3
         }
 
