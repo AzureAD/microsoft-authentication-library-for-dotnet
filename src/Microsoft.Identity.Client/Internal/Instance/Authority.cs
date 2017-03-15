e809a1071c43227c963e4fd2d6f3b21b1335fe31
Bogdan Gavril <bgavril@microsoft.com>
2017-03-15 17:30:45 +0000

    bgavril/globfxcop

--- src/Microsoft.Identity.Client/Internal/Instance/Authority.cs
+++ src/Microsoft.Identity.Client/Internal/Instance/Authority.cs
@@ -244,7 +244,7 @@ namespace Microsoft.Identity.Client.Internal.Instance
                 uri = uri + "/";
             }
 
-            return uri.ToLower();
+            return uri.ToLowerInvariant();
         }
 
         private void ReplaceTenantlessTenant(string tenantId)
