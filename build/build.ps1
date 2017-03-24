1de080baae025f3836a61184e111c3d6f41c2414
Sam Chen <sschen@microsoft.com>
2017-03-24 16:06:41 +0000

    Appveyor CI exits on failure.

--- build/build.ps1
+++ build/build.ps1
@@ -1,8 +1,28 @@
-MSBuild ADAL.NET.sln /t:Clean
+$ErrorActionPreference = "Stop"
+
+function ExitOnError
+{
+    if ($LastExitCode -ne 0) {
+        # Force the PS script to exit with an error code, signalling the error to AppVeyor
+        $host.SetShouldExit($LastExitCode)
+        exit $LastExitCode
+    }
+}
+
+Log ("Building product code...")
 MSBuild ./src/ADAL.PCL/ADAL.PCL.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
 MSBuild ./src/ADAL.PCL.Android/ADAL.PCL.Android.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
 MSBuild ./src/ADAL.PCL.CoreCLR/ADAL.PCL.CoreCLR.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
 MSBuild ./src/ADAL.PCL.Desktop/ADAL.PCL.Desktop.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
 MSBuild ./src/ADAL.PCL.iOS/ADAL.PCL.iOS.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
 MSBuild ./src/ADAL.PCL.WinRT/ADAL.PCL.WinRT.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
+
+Log("Building Tests...")
 MSBuild ./tests/Test.ADAL.NET.Unit/Test.ADAL.NET.Unit.csproj /t:Build /p:runcodeanalysis=false
+ExitOnError
