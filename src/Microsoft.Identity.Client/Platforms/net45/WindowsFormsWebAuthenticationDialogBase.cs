723ed264c45e1483b16beb2b0a8dcbd17da54a37
Ryan Robertson <rjr.devry@gmail.com>
2017-03-26 10:10:20 -0700

    Restore parenthesis for readability as [requested](https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/pull/265#discussion_r108056394).

--- src/Microsoft.Identity.Client/Platforms/net45/WindowsFormsWebAuthenticationDialogBase.cs
+++ src/Microsoft.Identity.Client/Platforms/net45/WindowsFormsWebAuthenticationDialogBase.cs
@@ -324,7 +324,7 @@ namespace Microsoft.Identity.Client
             Name = "BrowserAuthenticationWindow";
 
             // Move the window to the center of the parent window only if owner window is set.
-            StartPosition = ownerWindow != null
+            StartPosition = (ownerWindow != null)
                 ? FormStartPosition.CenterParent
                 : FormStartPosition.CenterScreen;
             Text = string.Empty;
@@ -334,7 +334,7 @@ namespace Microsoft.Identity.Client
 
             // If we don't have an owner we need to make sure that the pop up browser 
             // window is in the task bar so that it can be selected with the mouse.
-            ShowInTaskbar = null == ownerWindow;
+            ShowInTaskbar = (null == ownerWindow);
 
             _webBrowserPanel.ResumeLayout(false);
             ResumeLayout(false);
