922c269ed8ab0fd279510fa0f0eb134461f20ddb
jennyf19 <ferriesj@gmail.com>
2017-03-30 15:03:17 -0700

    Format changes to main UI for Public Client

--- dev apps/DesktopTestApp/MainForm.Designer.cs
+++ dev apps/DesktopTestApp/MainForm.Designer.cs
@@ -39,7 +39,6 @@
             this.PiiLoggingLabel = new System.Windows.Forms.Label();
             this.ScopeResult = new System.Windows.Forms.ListBox();
             this.Scope = new System.Windows.Forms.Label();
-            this.IdTokenResult = new System.Windows.Forms.Label();
             this.IdToken = new System.Windows.Forms.Label();
             this.UserResult = new System.Windows.Forms.Label();
             this.User = new System.Windows.Forms.Label();
@@ -130,6 +129,7 @@
             this.confClientAcquireTokenBtn = new System.Windows.Forms.Button();
             this.confClientAcquireTokenSilentBtn = new System.Windows.Forms.Button();
             this.accessTokenResult = new System.Windows.Forms.TextBox();
+            this.IdTokenResult = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.publicClientTabPage.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -182,6 +182,7 @@
             // publicClientTabPage
             // 
             this.publicClientTabPage.BackColor = System.Drawing.SystemColors.Control;
+            this.publicClientTabPage.Controls.Add(this.IdTokenResult);
             this.publicClientTabPage.Controls.Add(this.accessTokenResult);
             this.publicClientTabPage.Controls.Add(this.label12);
             this.publicClientTabPage.Controls.Add(this.PiiLoggingDisabled);
@@ -189,7 +190,6 @@
             this.publicClientTabPage.Controls.Add(this.PiiLoggingLabel);
             this.publicClientTabPage.Controls.Add(this.ScopeResult);
             this.publicClientTabPage.Controls.Add(this.Scope);
-            this.publicClientTabPage.Controls.Add(this.IdTokenResult);
             this.publicClientTabPage.Controls.Add(this.IdToken);
             this.publicClientTabPage.Controls.Add(this.UserResult);
             this.publicClientTabPage.Controls.Add(this.User);
@@ -225,9 +225,10 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(25, 716);
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(11, 361);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(101, 13);
+            this.label12.Size = new System.Drawing.Size(204, 25);
             this.label12.TabIndex = 32;
             this.label12.Text = "Pii Logging Enabled";
             // 
@@ -235,7 +236,7 @@
             // 
             this.PiiLoggingDisabled.AutoSize = true;
             this.PiiLoggingDisabled.Checked = true;
-            this.PiiLoggingDisabled.Location = new System.Drawing.Point(229, 709);
+            this.PiiLoggingDisabled.Location = new System.Drawing.Point(366, 361);
             this.PiiLoggingDisabled.Name = "PiiLoggingDisabled";
             this.PiiLoggingDisabled.Size = new System.Drawing.Size(79, 26);
             this.PiiLoggingDisabled.TabIndex = 31;
@@ -246,7 +247,7 @@
             // PiiLoggingEnabled
             // 
             this.PiiLoggingEnabled.AutoSize = true;
-            this.PiiLoggingEnabled.Location = new System.Drawing.Point(146, 709);
+            this.PiiLoggingEnabled.Location = new System.Drawing.Point(262, 361);
             this.PiiLoggingEnabled.Name = "PiiLoggingEnabled";
             this.PiiLoggingEnabled.Size = new System.Drawing.Size(77, 26);
             this.PiiLoggingEnabled.TabIndex = 30;
@@ -264,33 +265,24 @@
             // ScopeResult
             // 
             this.ScopeResult.FormattingEnabled = true;
-            this.ScopeResult.Location = new System.Drawing.Point(134, 534);
+            this.ScopeResult.Location = new System.Drawing.Point(130, 632);
             this.ScopeResult.Name = "ScopeResult";
-            this.ScopeResult.Size = new System.Drawing.Size(150, 69);
+            this.ScopeResult.Size = new System.Drawing.Size(150, 30);
             this.ScopeResult.TabIndex = 28;
             // 
             // Scope
             // 
             this.Scope.AutoSize = true;
-            this.Scope.Location = new System.Drawing.Point(34, 532);
+            this.Scope.Location = new System.Drawing.Point(34, 632);
             this.Scope.Name = "Scope";
             this.Scope.Size = new System.Drawing.Size(38, 13);
             this.Scope.TabIndex = 27;
             this.Scope.Text = "Scope";
             // 
-            // IdTokenResult
-            // 
-            this.IdTokenResult.AutoSize = true;
-            this.IdTokenResult.Location = new System.Drawing.Point(132, 504);
-            this.IdTokenResult.Name = "IdTokenResult";
-            this.IdTokenResult.Size = new System.Drawing.Size(109, 13);
-            this.IdTokenResult.TabIndex = 26;
-            this.IdTokenResult.Text = "Id Token Placeholder";
-            // 
             // IdToken
             // 
             this.IdToken.AutoSize = true;
-            this.IdToken.Location = new System.Drawing.Point(34, 504);
+            this.IdToken.Location = new System.Drawing.Point(34, 592);
             this.IdToken.Name = "IdToken";
             this.IdToken.Size = new System.Drawing.Size(50, 13);
             this.IdToken.TabIndex = 25;
@@ -299,7 +291,7 @@
             // UserResult
             // 
             this.UserResult.AutoSize = true;
-            this.UserResult.Location = new System.Drawing.Point(132, 472);
+            this.UserResult.Location = new System.Drawing.Point(127, 568);
             this.UserResult.Name = "UserResult";
             this.UserResult.Size = new System.Drawing.Size(88, 13);
             this.UserResult.TabIndex = 24;
@@ -308,7 +300,7 @@
             // User
             // 
             this.User.AutoSize = true;
-            this.User.Location = new System.Drawing.Point(34, 472);
+            this.User.Location = new System.Drawing.Point(34, 568);
             this.User.Name = "User";
             this.User.Size = new System.Drawing.Size(29, 13);
             this.User.TabIndex = 23;
@@ -317,7 +309,7 @@
             // TenantIdResult
             // 
             this.TenantIdResult.AutoSize = true;
-            this.TenantIdResult.Location = new System.Drawing.Point(132, 438);
+            this.TenantIdResult.Location = new System.Drawing.Point(127, 542);
             this.TenantIdResult.Name = "TenantIdResult";
             this.TenantIdResult.Size = new System.Drawing.Size(112, 13);
             this.TenantIdResult.TabIndex = 22;
@@ -326,7 +318,7 @@
             // TenantId
             // 
             this.TenantId.AutoSize = true;
-            this.TenantId.Location = new System.Drawing.Point(32, 438);
+            this.TenantId.Location = new System.Drawing.Point(34, 542);
             this.TenantId.Name = "TenantId";
             this.TenantId.Size = new System.Drawing.Size(53, 13);
             this.TenantId.TabIndex = 21;
@@ -335,7 +327,7 @@
             // ExpiresOnResult
             // 
             this.ExpiresOnResult.AutoSize = true;
-            this.ExpiresOnResult.Location = new System.Drawing.Point(132, 406);
+            this.ExpiresOnResult.Location = new System.Drawing.Point(127, 512);
             this.ExpiresOnResult.Name = "ExpiresOnResult";
             this.ExpiresOnResult.Size = new System.Drawing.Size(117, 13);
             this.ExpiresOnResult.TabIndex = 20;
@@ -344,7 +336,7 @@
             // ExpiresOn
             // 
             this.ExpiresOn.AutoSize = true;
-            this.ExpiresOn.Location = new System.Drawing.Point(34, 406);
+            this.ExpiresOn.Location = new System.Drawing.Point(34, 512);
             this.ExpiresOn.Name = "ExpiresOn";
             this.ExpiresOn.Size = new System.Drawing.Size(58, 13);
             this.ExpiresOn.TabIndex = 19;
@@ -353,7 +345,7 @@
             // AccessToken
             // 
             this.AccessToken.AutoSize = true;
-            this.AccessToken.Location = new System.Drawing.Point(32, 372);
+            this.AccessToken.Location = new System.Drawing.Point(32, 420);
             this.AccessToken.Name = "AccessToken";
             this.AccessToken.Size = new System.Drawing.Size(76, 13);
             this.AccessToken.TabIndex = 17;
@@ -370,10 +362,10 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(8, 312);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(146, 44);
+            this.label9.Size = new System.Drawing.Size(84, 25);
             this.label9.TabIndex = 14;
             this.label9.Text = "Scopes";
             // 
@@ -433,7 +425,7 @@
             // 
             // callResult
             // 
-            this.callResult.Location = new System.Drawing.Point(12, 352);
+            this.callResult.Location = new System.Drawing.Point(15, 403);
             this.callResult.Multiline = true;
             this.callResult.Name = "callResult";
             this.callResult.ReadOnly = true;
@@ -444,10 +436,10 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(8, 172);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(217, 44);
+            this.label8.Size = new System.Drawing.Size(123, 25);
             this.label8.TabIndex = 9;
             this.label8.Text = "UI Behavior";
             // 
@@ -463,16 +455,16 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(8, 266);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 44);
+            this.label7.Size = new System.Drawing.Size(57, 25);
             this.label7.TabIndex = 11;
             this.label7.Text = "User";
             // 
             // acquireTokenSilent
             // 
-            this.acquireTokenSilent.Location = new System.Drawing.Point(453, 635);
+            this.acquireTokenSilent.Location = new System.Drawing.Point(453, 683);
             this.acquireTokenSilent.Name = "acquireTokenSilent";
             this.acquireTokenSilent.Size = new System.Drawing.Size(164, 46);
             this.acquireTokenSilent.TabIndex = 10;
@@ -482,7 +474,7 @@
             // 
             // acquireTokenInteractive
             // 
-            this.acquireTokenInteractive.Location = new System.Drawing.Point(37, 635);
+            this.acquireTokenInteractive.Location = new System.Drawing.Point(35, 683);
             this.acquireTokenInteractive.Name = "acquireTokenInteractive";
             this.acquireTokenInteractive.Size = new System.Drawing.Size(164, 46);
             this.acquireTokenInteractive.TabIndex = 9;
@@ -532,20 +524,20 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(8, 228);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(191, 44);
+            this.label6.Size = new System.Drawing.Size(109, 25);
             this.label6.TabIndex = 5;
             this.label6.Text = "Login Hint";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(8, 109);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(319, 44);
+            this.label5.Size = new System.Drawing.Size(181, 25);
             this.label5.TabIndex = 4;
             this.label5.Text = "Validate Authority";
             // 
@@ -559,20 +551,20 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(8, 58);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(524, 44);
+            this.label4.Size = new System.Drawing.Size(297, 25);
             this.label4.TabIndex = 2;
             this.label4.Text = "Overridden Authority for 1 call";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(8, 19);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 44);
+            this.label3.Size = new System.Drawing.Size(97, 25);
             this.label3.TabIndex = 0;
             this.label3.Text = "Authority";
             // 
@@ -964,7 +956,7 @@
             // 
             this.publicClient.Location = new System.Drawing.Point(1, 817);
             this.publicClient.Name = "publicClient";
-            this.publicClient.Size = new System.Drawing.Size(97, 45);
+            this.publicClient.Size = new System.Drawing.Size(116, 45);
             this.publicClient.TabIndex = 1;
             this.publicClient.Text = "Public Client";
             this.publicClient.UseVisualStyleBackColor = true;
@@ -972,9 +964,9 @@
             // 
             // settings
             // 
-            this.settings.Location = new System.Drawing.Point(307, 817);
+            this.settings.Location = new System.Drawing.Point(303, 817);
             this.settings.Name = "settings";
-            this.settings.Size = new System.Drawing.Size(92, 45);
+            this.settings.Size = new System.Drawing.Size(96, 45);
             this.settings.TabIndex = 2;
             this.settings.Text = "Settings";
             this.settings.UseVisualStyleBackColor = true;
@@ -1176,11 +1168,19 @@
             // 
             // accessTokenResult
             // 
-            this.accessTokenResult.Location = new System.Drawing.Point(133, 364);
+            this.accessTokenResult.Location = new System.Drawing.Point(130, 417);
             this.accessTokenResult.Multiline = true;
             this.accessTokenResult.Name = "accessTokenResult";
-            this.accessTokenResult.Size = new System.Drawing.Size(483, 20);
+            this.accessTokenResult.Size = new System.Drawing.Size(483, 92);
             this.accessTokenResult.TabIndex = 33;
+            // 
+            // IdTokenResult
+            // 
+            this.IdTokenResult.Location = new System.Drawing.Point(131, 586);
+            this.IdTokenResult.Multiline = true;
+            this.IdTokenResult.Name = "IdTokenResult";
+            this.IdTokenResult.Size = new System.Drawing.Size(481, 40);
+            this.IdTokenResult.TabIndex = 34;
             // 
             // MainForm
             // 
@@ -1262,7 +1262,6 @@
         private System.Windows.Forms.TextBox environmentQP;
         private System.Windows.Forms.Label AccessToken;
         private System.Windows.Forms.Label Scope;
-        private System.Windows.Forms.Label IdTokenResult;
         private System.Windows.Forms.Label IdToken;
         private System.Windows.Forms.Label UserResult;
         private System.Windows.Forms.Label User;
@@ -1318,6 +1317,7 @@
         private System.Windows.Forms.RadioButton confClientPiiDisabledButton;
         private System.Windows.Forms.Label confClientPiiEnabledLabel;
         private System.Windows.Forms.TextBox accessTokenResult;
+        private System.Windows.Forms.TextBox IdTokenResult;
     }
 }
 
