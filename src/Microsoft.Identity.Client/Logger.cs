d7664bcfabecbc5aa101c1db59dcdf2efa230b6d
REDMOND\kpanwar <kpanwar@microsoft.com>
2017-04-19 00:04:11 -0700

    fix logger dictionary failure

--- src/Microsoft.Identity.Client/Logger.cs
+++ src/Microsoft.Identity.Client/Logger.cs
@@ -223,10 +223,16 @@ namespace Microsoft.Identity.Client
                 ? string.Empty
                 : " - " + CorrelationId;
 
+            string os = "N/A";
+            if (MsalIdHelper.GetMsalIdParameters().ContainsKey(MsalIdParameter.OS))
+            {
+                os = MsalIdHelper.GetMsalIdParameters()[MsalIdParameter.OS];
+            }
+
             string log = string.Format(CultureInfo.InvariantCulture, "MSAL {0} {1} {2} [{3}{4}] {5}",
                 MsalIdHelper.GetMsalVersion(),
                 PlatformPlugin.PlatformInformation.GetOperatingSystem(),
-                MsalIdHelper.GetMsalIdParameters()[MsalIdParameter.OS], DateTime.UtcNow, correlationId, logMessage);
+                os, DateTime.UtcNow, correlationId, logMessage);
 
             if (DefaultLoggingEnabled)
             {
