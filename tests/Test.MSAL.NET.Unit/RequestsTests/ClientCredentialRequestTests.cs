34a4cf2eb3086fb7403e26970cc3447ae497095d
Petro Somka <pesomka@microsoft.com>
2017-04-20 15:11:04 -0700

    formatting

--- tests/Test.MSAL.NET.Unit/RequestsTests/ClientCredentialRequestTests.cs
+++ tests/Test.MSAL.NET.Unit/RequestsTests/ClientCredentialRequestTests.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Internal;
 using Microsoft.Identity.Client.Internal.Http;
 using Microsoft.Identity.Client.Internal.Instance;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Identity.Client.Internal.Requests;
-using Microsoft.Identity.Client.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Test.MSAL.NET.Unit.Mocks;
 
 namespace Test.MSAL.NET.Unit.RequestsTests
@@ -21,7 +18,7 @@ namespace Test.MSAL.NET.Unit.RequestsTests
         [TestInitialize]
         public void TestInitialize()
         {
-            _cache = new TokenCache()
+            _cache = new TokenCache
             {
                 ClientId = TestConstants.ClientId
             };
@@ -50,20 +47,22 @@ namespace Test.MSAL.NET.Unit.RequestsTests
                 Scope = TestConstants.Scope,
                 TokenCache = _cache,
                 RequestContext = new RequestContext(Guid.Empty),
-                User = new User()
+                User = new User
                 {
                     Identifier = TestConstants.UserIdentifier,
                     DisplayableId = TestConstants.DisplayableId
                 }
             };
- 
-            var clientCredentialRequest = new ClientCredentialRequest(parameters, forceRefresh: false);
-            await clientCredentialRequest.PreTokenRequest();
-            Assert.IsNotNull(clientCredentialRequest.AccessTokenItem, "forceRefresh == false -> Access Token from cache was used");;
 
-            clientCredentialRequest = new ClientCredentialRequest(parameters, forceRefresh: true);
+            var clientCredentialRequest = new ClientCredentialRequest(parameters, false);
             await clientCredentialRequest.PreTokenRequest();
-            Assert.IsNull(clientCredentialRequest.AccessTokenItem, "forceRefresh == true -> Access Token from cache was not used");
+            Assert.IsNotNull(clientCredentialRequest.AccessTokenItem,
+                "forceRefresh == false -> Access Token from cache was used");
+
+            clientCredentialRequest = new ClientCredentialRequest(parameters, true);
+            await clientCredentialRequest.PreTokenRequest();
+            Assert.IsNull(clientCredentialRequest.AccessTokenItem,
+                "forceRefresh == true -> Access Token from cache was not used");
         }
     }
 }
