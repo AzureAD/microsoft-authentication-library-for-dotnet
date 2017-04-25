0698771f7de49c673d2ed968921121ac57370d89
jennyf19 <ferriesj@gmail.com>
2017-04-25 10:23:32 -0700

    Remove web app default values from Controllers and Views

--- ConflidentialClientWebApp/Controllers/HomeController.cs
+++ ConflidentialClientWebApp/Controllers/HomeController.cs
@@ -12,21 +12,7 @@ namespace ConflidentialClientWebApp.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View();
