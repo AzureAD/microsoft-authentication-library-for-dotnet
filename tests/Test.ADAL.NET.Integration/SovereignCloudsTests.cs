5e3e24ec4c3b82b996c996e40a723f0df8f3bfe8
Ray Luo <rayluo@microsoft.com>
2017-09-19 15:00:12 -0700

    Rollback modifications on sovereign cloud test cases

--- tests/Test.ADAL.NET.Integration/SovereignCloudsTests.cs
+++ tests/Test.ADAL.NET.Integration/SovereignCloudsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -20,7 +19,6 @@ namespace Test.ADAL.NET.Unit
         {
             HttpMessageHandlerFactory.ClearMockHandlers();
             platformParameters = new PlatformParameters(PromptBehavior.Auto);
-            InstanceDiscovery.InstanceCache.Clear();
         }
 
 /*        [TestMethod]
@@ -44,8 +42,6 @@ namespace Test.ADAL.NET.Unit
                 // validate that authorizationUri passed to WebUi contains instance_aware query parameter
                 new Dictionary<string, string> {{"instance_aware", "true"}});
 
-            HttpMessageHandlerFactory.AddMockHandler(MockHelpers.CreateInstanceDiscoveryMockHandler());
-            HttpMessageHandlerFactory.AddMockHandler(MockHelpers.CreateInstanceDiscoveryMockHandler());
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
             {
                 Method = HttpMethod.Post,
@@ -63,6 +59,7 @@ namespace Test.ADAL.NET.Unit
             var authenticationResult = await authenticationContext.AcquireTokenAsync(TestConstants.DefaultResource,
                 TestConstants.DefaultClientId,
                 TestConstants.DefaultRedirectUri, platformParameters, UserIdentifier.AnyUser, "instance_aware=true");
+
             // make sure that tenant spesific sovereign Authority returned to the app in AuthenticationResult
             Assert.AreEqual(sovereignTenantSpesificAuthority, authenticationResult.Authority);
 
