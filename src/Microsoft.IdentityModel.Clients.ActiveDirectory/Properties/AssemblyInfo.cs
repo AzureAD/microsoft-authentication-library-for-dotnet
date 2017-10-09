1a3bf8cea1cfb8819845ad7414410d7028b051e9
Kanishk Panwar <kpanwar@microsoft.com>
2017-10-09 13:12:53 -0700

    update version for next release

--- src/Microsoft.IdentityModel.Clients.ActiveDirectory/Properties/AssemblyInfo.cs
+++ src/Microsoft.IdentityModel.Clients.ActiveDirectory/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
 
-[assembly: AssemblyFileVersion("3.17.0.0")]
+[assembly: AssemblyFileVersion("3.17.1.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
