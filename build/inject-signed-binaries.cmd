8620dba76170fd8c1d7686e1d63c9f885a9f6af2
REDMOND\kpanwar <kpanwar@microsoft.com>
2017-10-23 12:50:45 -0700

    remove if check

--- build/inject-signed-binaries.cmd
+++ build/inject-signed-binaries.cmd
@@ -1,21 +1,22 @@
 set BUILD_PATH=bin\%BUILD_BUILDCONFIGURATION%
 set SIGNED_TARGET=%BUILD_ARTIFACTSTAGINGDIRECTORY%\SignedProductBinaries
 set NUGET_TARGET=%BUILD_ARTIFACTSTAGINGDIRECTORY%
+set NUGET_SYMBOLS_PLACEHOLDER=%BUILD_ARTIFACTSTAGINGDIRECTORY%\symbols
 set NUGET_TEMP_TARGET=%BUILD_SOURCESDIRECTORY%\NuGet-Temp
 set LIBRARY_NAME=Microsoft.Identity.Client
 setlocal ENABLEDELAYEDEXPANSION
 
 md %NUGET_TEMP_TARGET%
+md %NUGET_SYMBOLS_PLACEHOLDER%
+
+@echo Move symbols package to placeholder
+mv %NUGET_TARGET%\*.symbols.nupkg %NUGET_SYMBOLS_PLACEHOLDER%
 
 @echo ==========================
 @echo Decompressing nuget...
 for %%i in (%NUGET_TARGET%\*.nupkg) do (
 	set NUGET_NAME=%%~ni
-	If NOT "%NUGET_NAME:symbols=%"=="%NUGET_NAME%" (
-		echo Skipping symbols package file %NUGET_NAME%
-	) else (
-		7z e %NUGET_TARGET%\%%~ni.nupkg -o%NUGET_TEMP_TARGET%\ -y
-	)
+	7z e %NUGET_TARGET%\%%~ni.nupkg -o%NUGET_TEMP_TARGET%\ -y
 )
 
 @echo Saved nuget name as %NUGET_NAME%
@@ -32,6 +33,9 @@ del %NUGET_TARGET%\*.lastcodeanalysissucceeded /s
 @echo Creating NuGet Package....
 7z a %NUGET_TARGET%\%NUGET_NAME%.zip -r %NUGET_TEMP_TARGET%\*
 copy /Y %NUGET_TARGET%\%NUGET_NAME%.zip %NUGET_TARGET%\%NUGET_NAME%.nupkg
+
+@echo Move symbols package back to original location
+mv %NUGET_SYMBOLS_PLACEHOLDER%\*.symbols.nupkg %NUGET_TARGET%\
 
 @echo ====================================
 @echo MSAL-NET nuget creation script complete with no errors
