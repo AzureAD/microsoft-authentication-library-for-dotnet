98abd198830d0e33c9b17544dde60e248e5402ce
REDMOND\kpanwar <kpanwar@microsoft.com>
2017-10-23 12:22:16 -0700

    fix bug to create separate nugets for dlls

--- build/inject-signed-binaries.cmd
+++ build/inject-signed-binaries.cmd
@@ -11,14 +11,15 @@ md %NUGET_TEMP_TARGET%
 @echo Decompressing nuget...
 for %%i in (%NUGET_TARGET%\*.nupkg) do (
 	set NUGET_NAME=%%~ni
-	7z e %NUGET_TARGET%\%%~ni.nupkg -o%NUGET_TEMP_TARGET%\ -y
+	if not x%NUGET_NAME:symbols=%==x%NUGET_NAME%
+	(
+		echo Skipping symbols package file %NUGET_NAME%
+	) else (
+		7z e %NUGET_TARGET%\%%~ni.nupkg -o%NUGET_TEMP_TARGET%\ -y
+	)
 )
 
 @echo Saved nuget name as %NUGET_NAME%
-@echo ==========================
-@echo Copying dlls from %NUGET_TEMP_TARGET%\lib to %TO_SIGN_TARGET%...
-	ROBOCOPY %NUGET_TEMP_TARGET%\lib %TO_SIGN_TARGET%\ *.dll /E
-
 @echo ==========================
 @echo Copying signed files to %NUGET_TEMP_TARGET%\lib ...
 ROBOCOPY %SIGNED_TARGET%\ %NUGET_TEMP_TARGET%\lib\ *.dll /E
@@ -31,16 +32,16 @@ del %NUGET_TARGET%\*.lastcodeanalysissucceeded /s
 @echo ==========================
 @echo Creating NuGet Package....
 7z a %NUGET_TARGET%\%NUGET_NAME%.zip -r %NUGET_TEMP_TARGET%\*
-copy %NUGET_TARGET%\%NUGET_NAME%.zip %NUGET_TARGET%\%NUGET_NAME%.nupkg
+copy /Y %NUGET_TARGET%\%NUGET_NAME%.zip %NUGET_TARGET%\%NUGET_NAME%.nupkg
 
 @echo ====================================
-@echo MSAL-NET Postbuild script complete with no errors
+@echo MSAL-NET nuget creation script complete with no errors
 @echo ====================================
 exit 0
 
 :ERROREXIT
 @echo **********************************************
-@echo MSAL-NET Build failed to SIGN properly
+@echo MSAL-NET Build failed to create nuget properly
 @echo MSAL-NET Postbuild Script Complete WITH ERRORS
 @echo **********************************************
 exit 1
