a8fe076a2768c72d3695ef89eb50c1eac0e39dcc
Kanishk Panwar <kpanwar@microsoft.com>
2017-10-23 18:24:47 -0700

    Fix build to create separate nuget files (#487)
    * fix bug to create separate nugets for dlls
    
    * fix syntax
    
    * remove if check
    
    * rename mv to move

--- build/inject-signed-binaries.cmd
+++ build/inject-signed-binaries.cmd
@@ -1,11 +1,16 @@
 set BUILD_PATH=bin\%BUILD_BUILDCONFIGURATION%
 set SIGNED_TARGET=%BUILD_ARTIFACTSTAGINGDIRECTORY%\SignedProductBinaries
 set NUGET_TARGET=%BUILD_ARTIFACTSTAGINGDIRECTORY%
+set NUGET_SYMBOLS_PLACEHOLDER=%BUILD_ARTIFACTSTAGINGDIRECTORY%\symbols
 set NUGET_TEMP_TARGET=%BUILD_SOURCESDIRECTORY%\NuGet-Temp
 set LIBRARY_NAME=Microsoft.Identity.Client
 setlocal ENABLEDELAYEDEXPANSION
 
 md %NUGET_TEMP_TARGET%
+md %NUGET_SYMBOLS_PLACEHOLDER%
+
+@echo Move symbols package to placeholder
+move /Y %NUGET_TARGET%\*.symbols.nupkg %NUGET_SYMBOLS_PLACEHOLDER%
 
 @echo ==========================
 @echo Decompressing nuget...
@@ -15,10 +20,6 @@ for %%i in (%NUGET_TARGET%\*.nupkg) do (
 )
 
 @echo Saved nuget name as %NUGET_NAME%
-@echo ==========================
-@echo Copying dlls from %NUGET_TEMP_TARGET%\lib to %TO_SIGN_TARGET%...
-	ROBOCOPY %NUGET_TEMP_TARGET%\lib %TO_SIGN_TARGET%\ *.dll /E
-
 @echo ==========================
 @echo Copying signed files to %NUGET_TEMP_TARGET%\lib ...
 ROBOCOPY %SIGNED_TARGET%\ %NUGET_TEMP_TARGET%\lib\ *.dll /E
@@ -31,16 +32,19 @@ del %NUGET_TARGET%\*.lastcodeanalysissucceeded /s
 @echo ==========================
 @echo Creating NuGet Package....
 7z a %NUGET_TARGET%\%NUGET_NAME%.zip -r %NUGET_TEMP_TARGET%\*
-copy %NUGET_TARGET%\%NUGET_NAME%.zip %NUGET_TARGET%\%NUGET_NAME%.nupkg
+copy /Y %NUGET_TARGET%\%NUGET_NAME%.zip %NUGET_TARGET%\%NUGET_NAME%.nupkg
+
+@echo Move symbols package back to original location
+move /Y %NUGET_SYMBOLS_PLACEHOLDER%\*.symbols.nupkg %NUGET_TARGET%\
 
 @echo ====================================
-@echo MSAL-NET Postbuild script complete with no errors
+@echo MSAL-NET nuget creation script complete with no errors
 @echo ====================================
 exit 0
 
 :ERROREXIT
 @echo **********************************************
-@echo MSAL-NET Build failed to SIGN properly
+@echo MSAL-NET Build failed to create nuget properly
 @echo MSAL-NET Postbuild Script Complete WITH ERRORS
 @echo **********************************************
 exit 1
