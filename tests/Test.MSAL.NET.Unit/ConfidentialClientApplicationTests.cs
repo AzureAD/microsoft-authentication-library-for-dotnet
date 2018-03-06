0fe01c0d7481cf095a5a4032f3a411aec4b18c3b
trwalke <Trwalke@microsoft.com>
2018-03-06 09:47:04 -0800

    Fixed spelling.

--- tests/Test.MSAL.NET.Unit/ConfidentialClientApplicationTests.cs
+++ tests/Test.MSAL.NET.Unit/ConfidentialClientApplicationTests.cs
@@ -527,7 +527,7 @@ namespace Test.MSAL.NET.Unit
                 }
             });
 
-            await app.AcquireTokenForClientAsync(TestConstants.Scope.ToArray()).configureAwait(false);
+            await app.AcquireTokenForClientAsync(TestConstants.Scope.ToArray()).ConfigureAwait(false);
         }
 
         [TestMethod]
@@ -595,7 +595,7 @@ namespace Test.MSAL.NET.Unit
                     MockHelpers.CreateSuccessfulClientCredentialTokenResponseMessage(tokenRetrievedFromNetCall)
             });
 
-            var result = await app.AcquireTokenForClientAsync(TestConstants.Scope, true).configureAwait(false);
+            var result = await app.AcquireTokenForClientAsync(TestConstants.Scope, true).ConfigureAwait(false);
             Assert.AreEqual(tokenRetrievedFromNetCall, result.AccessToken);
 
             // make sure token in Cache was updated
