f71abf988797a85e0bd9a2abdf4e2e16240b902f
jennyf19 <jeferrie@microsoft.com>
2018-08-15 15:57:58 -0700

    Add AT silent flow to UWP app (#1203)

--- adal/devApps/AdalUAPTestApp/MainPage.xaml.cs
+++ adal/devApps/AdalUAPTestApp/MainPage.xaml.cs
@@ -1,4 +1,31 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -115,6 +142,26 @@ namespace UAPTestApp
             catch (Exception exc)
             {
                 this.AccessToken.Text = exc.Message;
+            }
+        }
+
+        private async void AccessTokenSilentButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.AccessToken.Text = string.Empty;
+            AuthenticationContext context = new AuthenticationContext("https://login.microsoftonline.com/common");
+            try
+            {
+                AuthenticationResult authResult = await context.AcquireTokenSilentAsync("https://graph.microsoft.com", ClientId).ConfigureAwait(false);
+
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                       () =>
+                       {
+                           AccessToken.Text = "Acquire Token Silent:\nSigned in User - " + authResult.UserInfo.DisplayableId + "\nAccessToken: \n" + authResult.AccessToken;
+                       });
+            }
+            catch (Exception exc)
+            {
+                this.AccessToken.Text = "Auth failed: " + exc.Message;
             }
         }
     }
