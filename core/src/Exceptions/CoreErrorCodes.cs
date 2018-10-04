AzureAD/azure-activedirectory-library-for-dotnet

Ensure OAuth client always throws a service exception when http fails

core/src/Exceptions/CoreErrorCodes.cs
@@ -77,6 +77,7 @@ namespace Microsoft.Identity.Core
         public const string UapCannotFindUpn = "uap_cannot_find_upn";
 
         public const string GetUserNameFailed = "get_user_name_failed";
+        public const string OAuthNonJsonError = "oauth_non_json_error";
 
 
 #if ANDROID

core/src/OAuth2/OAuth2Client.cs
@@ -97,7 +97,17 @@ namespace Microsoft.Identity.Core.OAuth2
                 httpEvent.UserAgent = response.UserAgent;
                 if (response.StatusCode != HttpStatusCode.OK)
                 {
-                    httpEvent.OauthErrorCode = JsonHelper.DeserializeFromJson<MsalTokenResponse>(response.Body).Error;
+                    try
+                    {
+                        httpEvent.OauthErrorCode = JsonHelper.DeserializeFromJson<MsalTokenResponse>(response.Body).Error;
+                    }
+                    catch (SerializationException) // in the rare case we get an error response we cannot deserialize
+                    {
+                        throw CoreExceptionFactory.Instance.GetServiceException(
+                            CoreErrorCodes.OAuthNonJsonError,
+                            CoreErrorMessages.OAuthNonJsonError,
+                            response);
+                    }
                 }
             }
             finally

msal/tests/Test.MSAL.NET.Unit/RequestsTests/InteractiveRequestTests.cs
@@ -236,20 +236,95 @@ namespace Test.MSAL.NET.Unit.RequestsTests
         }
 
         [TestMethod]
-        public void Foo()
+        [TestCategory("InteractiveRequestTests")]
+        public void OAuthClient_FailsWithServiceExceptionWhenItCannotParseJsonResponse()
         {
             Authority authority = Authority.CreateAuthority(TestConstants.AuthorityHomeTenant, false);
 
-            HttpMessageHandlerFactory.AddMockHandler(
-               MockHelpers.CreateInstanceDiscoveryMockHandler(
-               TestConstants.GetDiscoveryEndpoint(TestConstants.AuthorityCommonTenant)));
-
-            //add mock response for tenant endpoint discovery
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
             {
                 Method = HttpMethod.Get,
-                ResponseMessage = MockHelpers.CreateOpenIdConfigurationResponse(TestConstants.AuthorityHomeTenant)
+                ResponseMessage = MockHelpers.CreateTooManyRequestsNonJsonResponse() // returns a non json response
             });
+
+            AuthenticationRequestParameters parameters = new AuthenticationRequestParameters()
+            {
+                Authority = authority,
+                ClientId = TestConstants.ClientId,
+                Scope = TestConstants.Scope,
+                TokenCache = null,
+                RequestContext = new RequestContext(new MsalLogger(Guid.NewGuid(), null)),
+            };
+            parameters.RedirectUri = new Uri("some://uri");
+
+            MockWebUI ui = new MockWebUI();
+
+            InteractiveRequest request = new InteractiveRequest(parameters,
+                TestConstants.ScopeForAnotherResource.ToArray(),
+                 TestConstants.DisplayableId,
+                UIBehavior.SelectAccount, ui);
+         
+
+            try
+            {
+                request.PreTokenRequestAsync(CancellationToken.None).Wait();
+                
+                Assert.Fail("MsalException should have been thrown here");
+            }
+            catch (Exception exc)
+            {
+                MsalServiceException serverEx = exc.InnerException as MsalServiceException;
+                Assert.IsNotNull(serverEx);
+                Assert.AreEqual(429, serverEx.StatusCode);
+                Assert.AreEqual(MockHelpers.TooManyRequestsContent, serverEx.ResponseBody);
+                Assert.AreEqual(MockHelpers.TestRetryAfterDuration, serverEx.Headers.RetryAfter.Delta);
+                Assert.AreEqual(CoreErrorCodes.OAuthNonJsonError, serverEx.ErrorCode);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("InteractiveRequestTests")]
+        public void OAuthClient_FailsWithServiceExceptionWhenItCanParseJsonResponse()
+        {
+            Authority authority = Authority.CreateAuthority(TestConstants.AuthorityHomeTenant, false);
+
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
+            {
+                Method = HttpMethod.Get,
+                ResponseMessage = MockHelpers.CreateTooManyRequestsJsonResponse() // returns a non json response
+            });
+
+            AuthenticationRequestParameters parameters = new AuthenticationRequestParameters()
+            {
+                Authority = authority,
+                ClientId = TestConstants.ClientId,
+                Scope = TestConstants.Scope,
+                TokenCache = null,
+                RequestContext = new RequestContext(new MsalLogger(Guid.NewGuid(), null)),
+            };
+            parameters.RedirectUri = new Uri("some://uri");
+
+            MockWebUI ui = new MockWebUI();
+
+            InteractiveRequest request = new InteractiveRequest(parameters,
+                TestConstants.ScopeForAnotherResource.ToArray(),
+                 TestConstants.DisplayableId,
+                UIBehavior.SelectAccount, ui);
+
+            try
+            {
+                request.PreTokenRequestAsync(CancellationToken.None).Wait();
+
+                Assert.Fail("MsalException should have been thrown here");
+            }
+            catch (Exception exc)
+            {
+                MsalServiceException serverEx = exc.InnerException as MsalServiceException;
+                Assert.IsNotNull(serverEx);
+                Assert.AreEqual(429, serverEx.StatusCode);
+                Assert.AreEqual(MockHelpers.TestRetryAfterDuration, serverEx.Headers.RetryAfter.Delta);
+                Assert.AreEqual("Server overload", serverEx.ErrorCode);
+            }
         }
 
         [TestMethod]

AzureAD/microsoft-authentication-library-for-dotnet

core/src/Exceptions/CoreErrorCodes.cs
@@ -77,6 +77,7 @@ namespace Microsoft.Identity.Core
         public const string UapCannotFindUpn = "uap_cannot_find_upn";
 
         public const string GetUserNameFailed = "get_user_name_failed";
+        public const string NonParsableOAuthError = "non_parsable_oauth_error";
 
 
 #if ANDROID

core/src/OAuth2/OAuth2Client.cs
@@ -97,7 +97,17 @@ namespace Microsoft.Identity.Core.OAuth2
                 httpEvent.UserAgent = response.UserAgent;
                 if (response.StatusCode != HttpStatusCode.OK)
                 {
-                    httpEvent.OauthErrorCode = JsonHelper.DeserializeFromJson<MsalTokenResponse>(response.Body).Error;
+                    try
+                    {
+                        httpEvent.OauthErrorCode = JsonHelper.DeserializeFromJson<MsalTokenResponse>(response.Body).Error;
+                    }
+                    catch (SerializationException) // in the rare case we get an error response we cannot deserialize
+                    {
+                        throw CoreExceptionFactory.Instance.GetServiceException(
+                            CoreErrorCodes.NonParsableOAuthError,
+                            CoreErrorMessages.OAuthNonJsonError,
+                            response);
+                    }
                 }
             }
             finally

msal/tests/Test.MSAL.NET.Unit/RequestsTests/InteractiveRequestTests.cs
@@ -236,20 +236,98 @@ namespace Test.MSAL.NET.Unit.RequestsTests
         }
 
         [TestMethod]
-        public void Foo()
+        [TestCategory("InteractiveRequestTests")]
+        public void OAuthClient_FailsWithServiceExceptionWhenItCannotParseJsonResponse()
         {
             Authority authority = Authority.CreateAuthority(TestConstants.AuthorityHomeTenant, false);
 
-            HttpMessageHandlerFactory.AddMockHandler(
-               MockHelpers.CreateInstanceDiscoveryMockHandler(
-               TestConstants.GetDiscoveryEndpoint(TestConstants.AuthorityCommonTenant)));
-
-            //add mock response for tenant endpoint discovery
             HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
             {
                 Method = HttpMethod.Get,
-                ResponseMessage = MockHelpers.CreateOpenIdConfigurationResponse(TestConstants.AuthorityHomeTenant)
+                ResponseMessage = MockHelpers.CreateTooManyRequestsNonJsonResponse() // returns a non json response
             });
+
+            AuthenticationRequestParameters parameters = new AuthenticationRequestParameters()
+            {
+                Authority = authority,
+                ClientId = TestConstants.ClientId,
+                Scope = TestConstants.Scope,
+                TokenCache = null,
+                RequestContext = new RequestContext(new MsalLogger(Guid.NewGuid(), null)),
+            };
+            parameters.RedirectUri = new Uri("some://uri");
+
+            MockWebUI ui = new MockWebUI();
+
+            InteractiveRequest request = new InteractiveRequest(parameters,
+                TestConstants.ScopeForAnotherResource.ToArray(),
+                 TestConstants.DisplayableId,
+                UIBehavior.SelectAccount, ui);
+
+            try
+            {
+                request.PreTokenRequestAsync(CancellationToken.None).Wait();
+                
+                Assert.Fail("MsalException should have been thrown here");
+            }
+            catch (Exception exc)
+            {
+                MsalServiceException serverEx = exc.InnerException as MsalServiceException;
+                Assert.IsNotNull(serverEx);
+                Assert.AreEqual(429, serverEx.StatusCode);
+                Assert.AreEqual(MockHelpers.TooManyRequestsContent, serverEx.ResponseBody);
+                Assert.AreEqual(MockHelpers.TestRetryAfterDuration, serverEx.Headers.RetryAfter.Delta);
+                Assert.AreEqual(CoreErrorCodes.NonParsableOAuthError, serverEx.ErrorCode);
+            }
+
+            Assert.IsTrue(HttpMessageHandlerFactory.IsMocksQueueEmpty, "All mocks should have been consumed");
+        }
+
+        [TestMethod]
+        [TestCategory("InteractiveRequestTests")]
+        public void OAuthClient_FailsWithServiceExceptionWhenItCanParseJsonResponse()
+        {
+            Authority authority = Authority.CreateAuthority(TestConstants.AuthorityHomeTenant, false);
+
+            HttpMessageHandlerFactory.AddMockHandler(new MockHttpMessageHandler
+            {
+                Method = HttpMethod.Get,
+                ResponseMessage = MockHelpers.CreateTooManyRequestsJsonResponse() // returns a non json response
+            });
+
+            AuthenticationRequestParameters parameters = new AuthenticationRequestParameters()
+            {
+                Authority = authority,
+                ClientId = TestConstants.ClientId,
+                Scope = TestConstants.Scope,
+                TokenCache = null,
+                RequestContext = new RequestContext(new MsalLogger(Guid.NewGuid(), null)),
+            };
+            parameters.RedirectUri = new Uri("some://uri");
+
+            MockWebUI ui = new MockWebUI();
+
+            InteractiveRequest request = new InteractiveRequest(parameters,
+                TestConstants.ScopeForAnotherResource.ToArray(),
+                 TestConstants.DisplayableId,
+                UIBehavior.SelectAccount, ui);
+
+            try
+            {
+                request.PreTokenRequestAsync(CancellationToken.None).Wait();
+
+                Assert.Fail("MsalException should have been thrown here");
+            }
+            catch (Exception exc)
+            {
+                MsalServiceException serverEx = exc.InnerException as MsalServiceException;
+                Assert.IsNotNull(serverEx);
+                Assert.AreEqual(429, serverEx.StatusCode);
+                Assert.AreEqual(MockHelpers.TestRetryAfterDuration, serverEx.Headers.RetryAfter.Delta);
+                Assert.AreEqual("Server overload", serverEx.ErrorCode);
+            }
+
+            Assert.IsTrue(HttpMessageHandlerFactory.IsMocksQueueEmpty, "All mocks should have been consumed");
         }
 
         [TestMethod]