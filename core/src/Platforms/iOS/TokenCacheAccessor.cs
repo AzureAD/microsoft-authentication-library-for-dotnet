55d215f778b555930f1573edb359c416a97c9795
jennyf19 <jeferrie@microsoft.com>
2018-10-15 19:22:25 -0700

    - fix for accessible setting for TeamId (#1324)
    * - fix for accessible setting for TeamId
    - change teamIdKey default value
    
    * change readonly to const and remove "." from TeamId.

--- core/src/Platforms/iOS/TokenCacheAccessor.cs
+++ core/src/Platforms/iOS/TokenCacheAccessor.cs
@@ -58,8 +58,9 @@ namespace Microsoft.Identity.Core
         private const bool _defaultSyncSetting = false;
         private const SecAccessible _defaultAccessiblityPolicy = SecAccessible.AfterFirstUnlockThisDeviceOnly;
 
-        private readonly string DefaultKeychainGroup = "com.microsoft.adalcache";
-        private readonly string TeamIdKey = "teamIDHint";
+        private const string DefaultKeychainGroup = "com.microsoft.adalcache";
+        // Identifier for the keychain item used to retrieve current team ID
+        private const string TeamIdKey = "DotNetTeamIDHint";
 
         private string keychainGroup;
         private RequestContext _requestContext;
@@ -87,7 +88,7 @@ namespace Microsoft.Identity.Core
             {
                 Service = "",
                 Account = TeamIdKey,
-                Accessible = _defaultAccessiblityPolicy
+                Accessible = SecAccessible.Always
             };
 
             SecRecord match = SecKeyChain.QueryAsRecord(queryRecord, out SecStatusCode resultCode);
