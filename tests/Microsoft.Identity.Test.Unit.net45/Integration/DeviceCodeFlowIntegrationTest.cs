bbb361c966ef82c5cc8b0b5fe4e3d2257eed61d6
trwalke <trwalke@microsoft.com>
2019-01-09 03:50:08 -0800

    Reverting version upgrade

--- tests/Microsoft.Identity.Test.Unit.net45/Integration/DeviceCodeFlowIntegrationTest.cs
+++ tests/Microsoft.Identity.Test.Unit.net45/Integration/DeviceCodeFlowIntegrationTest.cs
@@ -30,8 +30,8 @@ using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Test.Common;
-using Microsoft.Identity.Test.Core.UIAutomation;
 using Microsoft.Identity.Test.LabInfrastructure;
+using Microsoft.Identity.Test.UIAutomation.infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 
