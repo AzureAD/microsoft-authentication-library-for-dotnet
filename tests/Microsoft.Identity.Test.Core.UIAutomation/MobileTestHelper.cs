8ff52c7913c1902d0da36d6ed9bc6e916f74c389
Mark Zuber <mzuber@microsoft.com>
2019-01-28 15:30:32 -0800

    pr feedback

--- tests/Microsoft.Identity.Test.Core.UIAutomation/MobileTestHelper.cs
+++ tests/Microsoft.Identity.Test.Core.UIAutomation/MobileTestHelper.cs
@@ -42,7 +42,6 @@ namespace Microsoft.Identity.Test.UIAutomation.Infrastructure
         private string _acquirePageId;
         private string _cachePageId;
         private string _settingsPageId;
-        // private string _logPageId;
 
         public MobileTestHelper(Xamarin.UITest.Platform platform)
         {
@@ -517,13 +516,11 @@ namespace Microsoft.Identity.Test.UIAutomation.Infrastructure
                     _cachePageId = CoreUiTestConstants.CachePageAndroidID;
                     _acquirePageId = CoreUiTestConstants.AcquirePageAndroidId;
                     _settingsPageId = CoreUiTestConstants.SettingsPageAndroidId;
-                    //_logPageId = CoreUiTestConstants.LogPageAndroidId;
                     break;
                 case Xamarin.UITest.Platform.iOS:
                     _cachePageId = CoreUiTestConstants.CachePageID;
                     _acquirePageId = CoreUiTestConstants.AcquirePageId;
                     _settingsPageId = CoreUiTestConstants.SettingsPageId;
-                    //_logPageId = CoreUiTestConstants.LogPageId;
                     break;
             }
         }
