0d270b5ffe97efd0d05f28a69b75cd6368a18c82
Bogdan Gavril <bogavril@microsoft.com>
2019-02-27 19:10:26 +0000

    A few more changes

--- tests/devapps/XForms/XForms.iOS/AppDelegate.cs
+++ tests/devapps/XForms/XForms.iOS/AppDelegate.cs
@@ -58,13 +58,8 @@ namespace XForms.iOS
                 (new Microsoft.Identity.Client.AriaTelemetryProvider.ClientTelemetryHandler()).OnEvents);
 #endif
 
-            // Default system browser
-            //App.UIParent = new UIParent();
-
             //App.MsalPublicClient.iOSKeychainSecurityGroup = "com.microsoft.adalcache";
 
-            // To activate embedded webview, remove '//' below
-            App.UIParent = new UIParent(true);
             return base.FinishedLaunching(app, options);
         }
 
