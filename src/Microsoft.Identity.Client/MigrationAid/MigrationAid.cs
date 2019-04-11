aec2b72ce31edf0e7fff566c3b2c204745e9e126
Bogdan Gavril <bogavril@microsoft.com>
2019-04-11 13:47:53 +0100

    Update migration fix message

--- src/Microsoft.Identity.Client/MigrationAid/MigrationAid.cs
+++ src/Microsoft.Identity.Client/MigrationAid/MigrationAid.cs
@@ -2102,7 +2102,7 @@ namespace Microsoft.Identity.Client
     /// <summary>
     ///
     /// </summary>
-    [Obsolete("In MSAL.NET 3.x, you should directly pass the Activity (on Xamarin.Android), or Window (on .NET Framework and UWP) as second parameter of AcquireTokenInteractive()" + MsalErrorMessage.AkaMsmsalnet3BreakingChanges, true)]
+    [Obsolete("In MSAL.NET 3.x, you should directly pass the Activity (on Xamarin.Android), or Window (on .NET Framework and UWP) using AcquireTokenInteractiveParameterBuilder.WithParentActivityOrWindow" + MsalErrorMessage.AkaMsmsalnet3BreakingChanges, true)]
     public sealed class UIParent
     {
         /// <summary>
