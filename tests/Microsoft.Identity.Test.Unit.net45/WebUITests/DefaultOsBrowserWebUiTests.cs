eb599ad6f9d19ca08701e50029f07ca4e41a211f
Bogdan Gavril <bogavril@microsoft.com>
2019-05-13 10:36:50 +0100

    [System Browser] Improve error messages and add more unit tests

--- tests/Microsoft.Identity.Test.Unit.net45/WebUITests/DefaultOsBrowserWebUiTests.cs
+++ tests/Microsoft.Identity.Test.Unit.net45/WebUITests/DefaultOsBrowserWebUiTests.cs
@@ -27,38 +27,73 @@ namespace Microsoft.Identity.Test.Unit.WebUITests
         private const string TestAuthorizationResponseUri = "http://localhost:50997/?code=OAQABAAIAAADCoMpjJXrxTq9VG9te-7FX1n0H3n1cOvvLstfUYt_wAqcm96iwQKYzTWLqkz44aUnx4mswa7tn53DFy03fIJie9zOUjk5y6R9vU-rhCSUTLTJR6wUdqsbZfqgRpcCHRPHgmOFk7c3MqJ6WF5Y9AfQgXLaXsZN5vy7ZqS9viU0-NXxKDuBx17yqsT0FPvuoO_0yEZkuVkwd_x_fuUpejHqmORRPfdS-rN6e-7TwfbpsjvUl_eZ2BbzOSJu9rRltWqK-cBVkBhmt3jYEXVWsuTFRD9GHPELscdMJxkwqeOyA8-Lt6zCskKQMq_aAwSPR34CxA9YXoLy-psqjeMDLA5ieP5rmdoNcGBPSXS-imNMKfFSxHN_df6rqpQCOShJ_SmuBFY6qfcARgXpAlobRiUHat-K5heDVJTude47uE_NCSdmRJVZzY1dOeVEJ6f6O1TgR8EHq_MOSyc9HTUU0CpYvf8zePZIjn4jFPv4CZwvdmc4sOCntWrPxxj0JfRval58-aueRgnyhkm9G23FG4oCWWjydaKp5EytHhyYYf_qztsycUkL3Z2Ox7brQ8_Sj1IQr14J3G2FUYgwjuvi6RYK3cvXPM6oUrhOlQcvx03y10xAtizogcA5UR2m8GIpDkm4GEMYX4yYcvBUI6y0qKHmjnZuS5UsymUhUbNG8kEsnI0WTODZ4zYlEHweTsTXq1QNawZqxAW-ZsQ9EbrEbuDFaybJtNYFuHkm1kUjUwpsbZXFLnTUI6CKKDNlUdvPpbiENgapB_p_AgLl3L5KihfY8AkVbVgHZVAcpDClEu_autQZa2jGvPEQka-oKpHqIFZbDEi4qB4yrkU_hDsjf-EqnIAA&state=901e7d87-6f49-4f9f-9fa7-e6b8c32d5b9595bc1797-dacc-4ff1-b9e9-0df81be286c7&session_state=1b37b349-61fe-4ad5-a049-9f8eadfded26";
         private const int TestPort = 50997;
 
+        ICoreLogger _logger;
+        ITcpInterceptor _tcpInterceptor;
+        IPlatformProxy _platformProxy;
+
+
         [TestInitialize]
         public void TestInitialize()
         {
             TestCommon.ResetInternalStaticCaches();
+            _logger = Substitute.For<ICoreLogger>();
+            _tcpInterceptor = Substitute.For<ITcpInterceptor>();
+            _platformProxy = Substitute.For<IPlatformProxy>();
         }
 
         [TestMethod]
         public async Task DefaultOsBrowserWebUi_HappyPath_Async()
         {
-            // Arrange
-            var logger = Substitute.For<ICoreLogger>();
-            var tcpInterceptor = Substitute.For<ITcpInterceptor>();
-            var platformProxy = Substitute.For<IPlatformProxy>();
-
-            IWebUI webUI = new DefaultOsBrowserWebUi(logger, platformProxy, tcpInterceptor);
-
-            tcpInterceptor.ListenToSingleRequestAndRespondAsync(
-                TestPort,
-                Arg.Any<Func<Uri, string>>(),
-                CancellationToken.None)
-               .Returns(Task.FromResult(new Uri(TestAuthorizationResponseUri)));
-
-            // Act
-            AuthorizationResult authorizationResult = await webUI.AcquireAuthorizationAsync(
-                new Uri(TestAuthorizationRequestUri),
-                new Uri(TestRedirectUri),
-                RequestContext.CreateForTest(),
-                CancellationToken.None).ConfigureAwait(false);
+            AuthorizationResult authorizationResult = await AcquireAuthCodeAsync()
+               .ConfigureAwait(false);
 
             // Assert
             Assert.AreEqual(AuthorizationStatus.Success, authorizationResult.Status);
-            Assert.IsTrue(string.IsNullOrEmpty(authorizationResult.Code));
+            Assert.IsFalse(string.IsNullOrEmpty(authorizationResult.Code));
+
+            await _tcpInterceptor.Received(1).ListenToSingleRequestAndRespondAsync(
+                TestPort, Arg.Any<Func<Uri, string>>(), CancellationToken.None).ConfigureAwait(false);
+        }
+
+        [TestMethod]  // TODO: bogavril - expect this case to be removed - MSAL should accept different port
+        public void DefaultOsBrowserWebUi_ReturnUriInvalid()
+        {
+            string differentPortRedirectUri = TestAuthorizationResponseUri.Replace(TestRedirectUri, "http://localhost:1111");
+
+            MsalClientException ex = AssertException.TaskThrows<MsalClientException>(() => AcquireAuthCodeAsync(
+               responseUriString: differentPortRedirectUri));
+
+            Assert.AreEqual(MsalError.LoopbackResponseUriMisatch, ex.ErrorCode);
+        }
+
+        private async Task<AuthorizationResult> AcquireAuthCodeAsync(
+            string redirectUri = TestRedirectUri,
+            string requestUri = TestAuthorizationRequestUri,
+            string responseUriString = TestAuthorizationResponseUri)
+        {
+            // Arrange
+            var responseUri = new Uri(responseUriString);
+            IWebUI webUI = new DefaultOsBrowserWebUi(_logger, _platformProxy, _tcpInterceptor);
+
+            _tcpInterceptor.ListenToSingleRequestAndRespondAsync(
+                TestPort,
+                Arg.Any<Func<Uri, string>>(),
+                CancellationToken.None)
+               .Returns(Task.FromResult(responseUri));
+
+            // Act
+            AuthorizationResult authorizationResult = await webUI.AcquireAuthorizationAsync(
+                new Uri(requestUri),
+                new Uri(redirectUri),
+                RequestContext.CreateForTest(),
+                CancellationToken.None).ConfigureAwait(false);
+
+            // Assert that we opened the browser
+            await _platformProxy.Received(1).StartDefaultOsBrowserAsync(requestUri)
+                .ConfigureAwait(false);
+
+            return authorizationResult;
+
         }
 
         [TestMethod]
