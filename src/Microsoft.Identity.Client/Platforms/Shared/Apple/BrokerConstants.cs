d2157d064dfd34e39c88d2fa1c4c4835b4794fa0
Bogdan Gavril <bogavril@microsoft.com>
2019-05-20 10:54:36 +0100

    [System Browser] Move around some Apple specific logic

---
+++ src/Microsoft.Identity.Client/Platforms/Shared/Apple/BrokerConstants.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+namespace Microsoft.Identity.Client.Platforms.Shared.Apple
+{
+    internal static class BrokerConstants
+    {
+        public const string ChallengeResponseHeader = "Authorization";
+
+        public const string ChallengeResponseType = "PKeyAuth";
+
+        public const string ChallengeResponseToken = "AuthToken";
+
+        public const string ChallengeResponseContext = "Context";
+
+        public const string ChallengeResponseVersion = "Version";
+
+        public const string BrowserExtPrefix = "browser://";
+
+        public const string BrowserExtInstallPrefix = "msauth://";
+
+        public const string DeviceAuthChallengeRedirect = "urn:http-auth:PKeyAuth";
+        public const string ChallengeHeaderKey = "x-ms-PKeyAuth";
+        public const string ChallengeHeaderValue = "1.0";
+    }
+}
---
+++ src/Microsoft.Identity.Client/Platforms/Shared/Apple/DeviceAuthHelper.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Microsoft.Identity.Client.Platforms.Shared.Apple
+{
+    internal class DeviceAuthHelper
+    {
+        public static bool CanHandleDeviceAuthChallenge { get { return false; } }
+
+        public static Task<string> CreateDeviceAuthChallengeResponseAsync(IDictionary<string, string> challengeData)
+        {
+            return Task.FromResult(string.Format(CultureInfo.InvariantCulture, @"PKeyAuth Context=""{0}"",Version=""{1}""", challengeData[BrokerConstants.ChallengeResponseContext], challengeData[BrokerConstants.ChallengeResponseVersion]));
+        }
+    }
+}
