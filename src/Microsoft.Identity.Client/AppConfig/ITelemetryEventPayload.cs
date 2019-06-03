a697289144d6d3ff195466b3d1379c2920b6718b
Bogdan Gavril <bogavril@microsoft.com>
2019-06-03 15:17:20 +0100

    Add an "experimental" remark to telemetry APIs (#1194)

--- src/Microsoft.Identity.Client/AppConfig/ITelemetryEventPayload.cs
+++ src/Microsoft.Identity.Client/AppConfig/ITelemetryEventPayload.cs
@@ -6,38 +6,46 @@ using System.Collections.Generic;
 namespace Microsoft.Identity.Client
 {
     /// <summary>
+    /// Data that represents a single snapshot in the series of events that are collected
     /// </summary>
+    /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
     public interface ITelemetryEventPayload
     {
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         /// <returns></returns>
         string Name { get; }
 
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         IReadOnlyDictionary<string, bool> BoolValues { get; }
 
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         IReadOnlyDictionary<string, long> Int64Values { get; }
 
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         IReadOnlyDictionary<string, int> IntValues { get; }
 
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         IReadOnlyDictionary<string, string> StringValues { get; }
 
         /// <summary>
         /// Used for debugging and testing.
         /// </summary>
+        /// <remarks>This API is experimental and it may change in future versions of the library without an major version increment</remarks>
         /// <returns></returns>
         string ToJsonString();
     }
