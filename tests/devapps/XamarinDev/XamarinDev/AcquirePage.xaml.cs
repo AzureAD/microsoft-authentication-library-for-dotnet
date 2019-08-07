8e34e48cc05d51ede68a2d3c612b54a714289dbf
Jenny Ferries <jeferrie@microsoft.com>
2019-08-06 18:20:10 -0700

    add IsSystemWebviewAvailable to dev app

--- tests/devapps/XamarinDev/XamarinDev/AcquirePage.xaml.cs
+++ tests/devapps/XamarinDev/XamarinDev/AcquirePage.xaml.cs
@@ -184,11 +184,17 @@ namespace XamarinDev
             {
                 acquireResponseTitleLabel.Text = EmptyResult;
 
-                var request = App.MsalPublicClient.AcquireTokenInteractive(GetScopes())
-                    .WithPrompt(GetPrompt())
-                    .WithParentActivityOrWindow(App.RootViewController)
-                    .WithUseEmbeddedWebView(true)
-                    .WithExtraQueryParameters(GetExtraQueryParams());
+                AcquireTokenInteractiveParameterBuilder request;
+
+                request = App.MsalPublicClient.AcquireTokenInteractive(GetScopes())
+                   .WithPrompt(GetPrompt())
+                   .WithParentActivityOrWindow(App.RootViewController)
+                   .WithExtraQueryParameters(GetExtraQueryParams());
+
+                if (!App.MsalPublicClient.IsSystemWebViewAvailable)
+                {
+                    request.WithUseEmbeddedWebView(true);
+                }
 
                 var result = await request.ExecuteAsync().ConfigureAwait(true);
 
