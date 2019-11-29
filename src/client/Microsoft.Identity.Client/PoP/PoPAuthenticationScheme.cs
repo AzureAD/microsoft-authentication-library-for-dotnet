782287dbcd3e1a27614406f2783262e946ef61ee
Bogdan Gavril <bogavril@microsoft.com>
2019-11-29 15:30:44 +0000

    [POP] Implementation of the POP authentication scheme

---
+++ src/client/Microsoft.Identity.Client/PoP/PoPAuthenticationScheme.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Identity.Client.Cache.Items;
+using Microsoft.Identity.Client.Internal;
+using Microsoft.Identity.Client.OAuth2;
+using Microsoft.Identity.Client.Utils;
+using Microsoft.Identity.Json.Linq;
+
+namespace Microsoft.Identity.Client.PoP
+{
+    internal class PoPAuthenticationScheme : IAuthenticationScheme
+    {
+        private static readonly DateTime s_jwtBaselineTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        private readonly HttpMethod _httpMethod;
+        private readonly Uri _requestUri;
+        private readonly IPoPCryptoProvider _popCryptoProvider;
+        private readonly byte[] _keyThumbprint;
+
+        /// <summary>
+        /// Creates POP tokens, i.e. tokens that are bound to an HTTP request and are digitally signed.
+        /// </summary>
+        /// <remarks>
+        /// Currently the signing credential algorithm is hard-coded to RSA with SHA256. Extensibility should be done
+        /// by integrating Wilson's SigningCredentials
+        /// </remarks>
+        public PoPAuthenticationScheme(Uri requestUri, HttpMethod httpMethod, IPoPCryptoProvider popCryptoProvider)
+        {
+            _httpMethod = httpMethod ?? throw new ArgumentNullException(nameof(httpMethod));
+            _requestUri = requestUri ?? throw new ArgumentNullException(nameof(requestUri));
+            _popCryptoProvider = popCryptoProvider ?? throw new ArgumentNullException(nameof(popCryptoProvider));
+
+            _keyThumbprint = ComputeRsaThumbprint(_popCryptoProvider.CannonicalPublicKeyJwk);
+            KeyId = Base64UrlHelpers.Encode(_keyThumbprint);
+        }
+
+        public string AuthorizationHeaderPrefix => PoPRequestParameters.PoPAuthHeaderPrefix;
+
+        /// <summary>
+        /// For PoP, we chose to use the base64(jwk_thumbprint)
+        /// </summary>
+        public string KeyId { get; }
+
+        public IDictionary<string, string> GetTokenRequestParams()
+        {
+            return new Dictionary<string, string>() {
+                { OAuth2Parameter.TokenType, PoPRequestParameters.PoPTokenType},
+                { PoPRequestParameters.RequestConfirmation, ComputeReqCnf()}
+            };
+        }
+
+        public string FormatAccessToken(MsalAccessTokenCacheItem atItem)
+        {
+            var header = new JObject
+            {
+                { JsonWebTokenConstants.ReservedHeaderParameters.Algorithm, JsonWebTokenConstants.Algorithms.RsaSha256 },
+                { JsonWebTokenConstants.ReservedHeaderParameters.KeyId, KeyId },
+                { JsonWebTokenConstants.ReservedHeaderParameters.Type, PoPRequestParameters.PoPTokenType}
+            };
+
+            JToken popAssertion = JToken.Parse(_popCryptoProvider.CannonicalPublicKeyJwk);
+
+            var payload = new JObject(
+                new JProperty(PoPClaimTypes.At, atItem.Secret),
+                new JProperty(PoPClaimTypes.HttpMethod, _httpMethod.ToString()),
+                new JProperty(PoPClaimTypes.Host, _requestUri.Host),
+                new JProperty(PoPClaimTypes.Path, _requestUri.AbsolutePath),
+                new JProperty(PoPClaimTypes.Nonce, CreateNonce()),
+                new JProperty(PoPClaimTypes.Cnf, new JObject(
+                    new JProperty(PoPClaimTypes.JWK, popAssertion))));
+
+            return CreateJWS(payload.ToString(Json.Formatting.None), header.ToString(Json.Formatting.None));
+        }
+
+        private static string CreateNonce()
+        {
+            // Guid with no hyphens
+#if NETSTANDARD || WINDOWS_APP
+            return Guid.NewGuid().ToString("N");
+#else
+            return Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture);
+#endif
+        }
+
+
+        private string ComputeReqCnf()
+        {
+            // There are 4 possible formats for a JWK, but Evo supports only this one for simplicity
+            var jwk = $@"{{""{JsonWebKeyParameterNames.Kid}"":""{KeyId}""}}";
+            return Base64UrlHelpers.Encode(jwk);
+        }
+
+        /// <summary>
+        /// A key ID that uniquely describes a public / private key pair. While KeyID is not normally
+        /// strict, AAD support for PoP requires that we use the base64 encoded JWK thumbprint, as described by 
+        /// https://tools.ietf.org/html/rfc7638
+        /// </summary>
+        private static byte[] ComputeRsaThumbprint(string cannonicalJwk)
+        {
+            // Cannot be easily generalized in UAP and NetStandard 1.3
+            using (SHA256 hash = SHA256.Create())
+            {
+                return hash.ComputeHash(Encoding.UTF8.GetBytes(cannonicalJwk));
+            }
+        }
+
+        /// <summary>
+        /// Creates a JWS (json web signature) as per: https://tools.ietf.org/html/rfc7515
+        /// Format: header.payload.signed_payload
+        /// </summary>
+        private string CreateJWS(string payload, string header)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Base64UrlHelpers.Encode(Encoding.UTF8.GetBytes(header)));
+            sb.Append(".");
+            sb.Append(Base64UrlHelpers.Encode(payload));
+            string headerAndPayload = sb.ToString();
+
+            sb.Append(".");
+            sb.Append(Base64UrlHelpers.Encode(_popCryptoProvider.Sign(Encoding.UTF8.GetBytes(headerAndPayload))));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ tests/Microsoft.Identity.Test.Unit.net45/pop/PopAuthenticationSchemeTests.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Cache.Items;
+using Microsoft.Identity.Client.PoP;
+using Microsoft.Identity.Json.Linq;
+using Microsoft.Identity.Test.Common;
+using Microsoft.Identity.Test.Common.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Microsoft.Identity.Test.Unit
+{
+    [TestClass]
+    public class PopAuthenticationSchemeTests
+    {
+        // Key and JWT copied from the JWT spec https://tools.ietf.org/html/rfc7638#section-3
+        private const string JWK = "{\"e\":\"AQAB\",\"kty\":\"RSA\",\"n\":\"0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw\"}";
+        private const string JWT = "NzbLsXh8uDCcd-6MNwXF4W_7noWXFZAfHkxZsRGC9Xs"; // for the JWK key
+
+        [TestMethod]
+        public void NullArgsTest()
+        {
+            Uri uri = new Uri("https://www.contoso.com/path1/path2?queryParam1=a&queryParam2=b");
+            HttpMethod method = HttpMethod.Post;
+            var popCryptoProvider = Substitute.For<IPoPCryptoProvider>();
+
+            AssertException.Throws<ArgumentException>(() => new PoPAuthenticationScheme(null, method, popCryptoProvider));
+            AssertException.Throws<ArgumentException>(() => new PoPAuthenticationScheme(uri, null, popCryptoProvider));
+            AssertException.Throws<ArgumentException>(() => new PoPAuthenticationScheme(uri, method, null));
+        }
+
+        [TestMethod]
+        public void ValidatePopRequestAndToken()
+        {
+            // Arrange
+            Uri uri = new Uri("https://www.contoso.com/path1/path2?queryParam1=a&queryParam2=b");
+            HttpMethod method = HttpMethod.Post;
+            var popCryptoProvider = Substitute.For<IPoPCryptoProvider>();
+            popCryptoProvider.CannonicalPublicKeyJwk.Returns(JWK);
+            const string AtSecret = "secret";
+            MsalAccessTokenCacheItem msalAccessTokenCacheItem = new MsalAccessTokenCacheItem()
+            {
+                Secret = AtSecret
+            };
+
+            // Act
+            PoPAuthenticationScheme authenticationScheme = new PoPAuthenticationScheme(uri, method, popCryptoProvider);
+            var tokenParams = authenticationScheme.GetTokenRequestParams();
+            var popTokenString = authenticationScheme.FormatAccessToken(msalAccessTokenCacheItem);
+            JwtSecurityToken decodedPopToken = new JwtSecurityToken(popTokenString);
+
+            // Assert
+            Assert.AreEqual("POP", authenticationScheme.AuthorizationHeaderPrefix);
+            Assert.AreEqual(JWT, authenticationScheme.KeyId);
+            Assert.AreEqual(2, tokenParams.Count);
+            Assert.AreEqual("pop", tokenParams["token_type"]);
+
+            // This is the base64 URL encoding of the JWK containing only the KeyId
+            Assert.AreEqual("eyJraWQiOiJOemJMc1hoOHVEQ2NkLTZNTndYRjRXXzdub1dYRlpBZkhreFpzUkdDOVhzIn0", tokenParams["req_cnf"]);
+            Assert.AreEqual("RS256", decodedPopToken.Header.Alg);
+            Assert.AreEqual(JWT, decodedPopToken.Header.Kid);
+            Assert.AreEqual("pop", decodedPopToken.Header.Typ);
+            Assert.AreEqual("RS256", decodedPopToken.SignatureAlgorithm);
+
+            AssertSimpleClaim(decodedPopToken, "at", AtSecret);
+            AssertSimpleClaim(decodedPopToken, "m", HttpMethod.Post.ToString());
+            AssertSimpleClaim(decodedPopToken, "u", "www.contoso.com");
+            AssertSimpleClaim(decodedPopToken, "p", "/path1/path2");
+
+            string nonce = AssertSimpleClaim(decodedPopToken, "nonce");
+            Assert.IsFalse(string.IsNullOrEmpty(nonce));
+            string jwk = AssertSimpleClaim(decodedPopToken, "cnf");
+            var jwkFromPopAssertion = JToken.Parse(jwk);
+            
+            var initialJwk = JToken.Parse(JWK);
+            Assert.IsTrue(jwkFromPopAssertion["jwk"].DeepEquals(initialJwk));
+        }
+
+        private static string AssertSimpleClaim(JwtSecurityToken jwt, string expectedKey, string optionalExpectedValue = null)
+        {
+            string value = jwt.Claims.Single(c => c.Type.Equals(expectedKey, StringComparison.InvariantCultureIgnoreCase)).Value;
+            if (optionalExpectedValue != null)
+            {
+                Assert.AreEqual(optionalExpectedValue, value);
+            }
+            return value;
+        }
+    }
+}
