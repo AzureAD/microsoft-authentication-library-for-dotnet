67793f96601ee2683767ef4aa6633f116d6357d7
Jenny Ferries <jeferrie@microsoft.com>
2020-02-12 14:01:54 -0800

    add check for ios13 on embedded webview for configuring the preferred content mode

--- src/client/Microsoft.Identity.Client/Platforms/iOS/EmbeddedWebview/MsalAuthenticationAgentUIViewController.cs
+++ src/client/Microsoft.Identity.Client/Platforms/iOS/EmbeddedWebview/MsalAuthenticationAgentUIViewController.cs
@@ -53,9 +53,12 @@ namespace Microsoft.Identity.Client.Platforms.iOS.EmbeddedWebview
         protected WKWebView PrepareWKWebView()
         {
             WKWebViewConfiguration wkconfg = new WKWebViewConfiguration() { };
-#if !IS_APPCENTER_BUILD
-            wkconfg.DefaultWebpagePreferences.PreferredContentMode = WKContentMode.Mobile;
-#endif
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                wkconfg.DefaultWebpagePreferences.PreferredContentMode = WKContentMode.Mobile;
+            }
+
             _wkWebView = new WKWebView(View.Bounds, wkconfg)
             {
                 UIDelegate = new WKWebNavigationDelegate.WKWebViewUIDelegate(this),
