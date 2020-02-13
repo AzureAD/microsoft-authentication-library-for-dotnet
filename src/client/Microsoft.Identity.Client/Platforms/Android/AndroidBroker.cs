f066d60253674d92e788b77b5a1ee48540456e6d
trwalke <trwalke@microsoft.com>
2020-02-13 11:49:07 -0800

    Adding error message for broker version

--- src/client/Microsoft.Identity.Client/Platforms/Android/AndroidBroker.cs
+++ src/client/Microsoft.Identity.Client/Platforms/Android/AndroidBroker.cs
@@ -59,7 +59,8 @@ namespace Microsoft.Identity.Client.Platforms.Android
             }
             catch (Exception ex)
             {
-                _logger.Error("Broker Operation Failed to complete.");
+                _logger.Error("Broker Operation Failed to complete. In order to perform brokered authentication," +
+                    " you need to ensure that you have installed either Intune Company Portal (Version 5.0.4689.0 or greater) or Microsoft Authenticator (6.2001.0140 or greater).");
                 if (ex is MsalException)
                     throw;
                 else
