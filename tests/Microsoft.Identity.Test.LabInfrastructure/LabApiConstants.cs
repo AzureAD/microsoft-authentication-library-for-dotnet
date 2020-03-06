f544c6f4715cf315d96512e5836f2c6b4544d45e
Neha Bhargava <nebharg@microsoft.com>
2020-03-06 09:37:27 -0800

    Update lab endpoint for test infrastructure.

--- tests/Microsoft.Identity.Test.LabInfrastructure/LabApiConstants.cs
+++ tests/Microsoft.Identity.Test.LabInfrastructure/LabApiConstants.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Identity.Test.LabInfrastructure
         public const string False = "false";
 
         public const string LabEndPoint = "https://msidlab.com/api/user";
-        public const string LabUserCredentialEndpoint = "https://msidlab.com/api/LabUserSecret";
+        public const string LabUserCredentialEndpoint = "https://msidlab.com/api/LabSecret";
         public const string LabAppEndpoint = "https://msidlab.com/api/app/";
         public const string LabInfoEndpoint = "https://msidlab.com/api/Lab/";
     }
