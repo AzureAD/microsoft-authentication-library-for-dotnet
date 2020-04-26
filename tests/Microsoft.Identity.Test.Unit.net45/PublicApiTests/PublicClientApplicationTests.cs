6430bc6eee79119522f502019b3db50fff62ea35
Bogdan Gavril <bogavril@microsoft.com>
2020-04-26 09:03:06 +0100

    test fix

--- tests/Microsoft.Identity.Test.Unit.net45/PublicApiTests/PublicClientApplicationTests.cs
+++ tests/Microsoft.Identity.Test.Unit.net45/PublicApiTests/PublicClientApplicationTests.cs
@@ -800,8 +800,9 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
 
                 // Act
                 var accounts = await pca.GetAccountsAsync().ConfigureAwait(false);
+                var account = accounts.Single(a => a.HomeAccountId.TenantId == tenant1);
                 AuthenticationResult response = await
-                    pca.AcquireTokenSilent(new[] { "User.Read" }, accounts.First())
+                    pca.AcquireTokenSilent(new[] { "User.Read" }, account)
                     .WithAuthority(tenantedAuthority1)
                     .ExecuteAsync()
                     .ConfigureAwait(false);
@@ -811,8 +812,9 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
 
                 // Act
                 accounts = await pca.GetAccountsAsync().ConfigureAwait(false);
+                account = accounts.Single(a => a.HomeAccountId.TenantId == tenant2);
                 response = await
-                    pca.AcquireTokenSilent(new[] { "User.Read" }, accounts.First())
+                    pca.AcquireTokenSilent(new[] { "User.Read" }, account)
                     .WithAuthority(tenantedAuthority2)
                     .ExecuteAsync()
                     .ConfigureAwait(false);
@@ -847,8 +849,9 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
 
                 // Act
                 var accounts = await pca.GetAccountsAsync().ConfigureAwait(false);
+                var account = accounts.Single(a => a.HomeAccountId.TenantId == tenant1);
                 AuthenticationResult response = await
-                    pca.AcquireTokenSilent(new[] { "User.Read" }, accounts.First())
+                    pca.AcquireTokenSilent(new[] { "User.Read" }, account)
                     .ExecuteAsync()
                     .ConfigureAwait(false);
 
@@ -860,8 +863,9 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
 
                 // Act
                 accounts = await pca2.GetAccountsAsync().ConfigureAwait(false);
+                account = accounts.Single(a => a.HomeAccountId.TenantId == tenant2);
                 response = await
-                    pca2.AcquireTokenSilent(new[] { "User.Read" }, accounts.First())
+                    pca2.AcquireTokenSilent(new[] { "User.Read" }, account)
                     .ExecuteAsync()
                     .ConfigureAwait(false);
 
