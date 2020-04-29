ba46c7716c5ab56ee2873c990f31123f6bd3b62c
Trwalke <trwalke@microsoft.com>
2020-04-29 15:02:17 -0700

    Update

--- tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
+++ tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Net;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Test.Integration.net45.Infrastructure;
+using Microsoft.Identity.Json.Linq;
 using Microsoft.Identity.Test.LabInfrastructure;
-using Microsoft.Identity.Test.Unit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Identity.Test.Integration.net45.HeadlessTests
 {
@@ -21,21 +18,17 @@ namespace Microsoft.Identity.Test.Integration.net45.HeadlessTests
         private const string _publicClientId = "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0";
         private const string _deviceAuthuser = "idlabca@msidlab8.onmicrosoft.com";
 
-#if DESKTOP
         [TestMethod]
-#if !IS_WIN8_TESTRUN
-        [Ignore]
-#endif
+
         public void PKeyAuthNonInteractiveTest()
         {
             //Arrange
             var msalPublicClient = PublicClientApplicationBuilder
                 .Create(_publicClientId)
-                .WithTestLogging()
                 .Build();
 
             //Act
-            var authResult = msalPublicClient.AcquireTokenByUsernamePassword(TestConstants.s_scope, _deviceAuthuser, new NetworkCredential("", LabUserHelper.FetchUserPassword("msidlab8")).SecurePassword)
+            var authResult = msalPublicClient.AcquireTokenByUsernamePassword(new[] { "user.read" }, _deviceAuthuser, new NetworkCredential("", LabUserHelper.FetchUserPassword("msidlab8")).SecurePassword)
             .WithClaims(JObject.Parse(_claims).ToString())
             .ExecuteAsync(CancellationToken.None).Result;
 
@@ -45,6 +38,6 @@ namespace Microsoft.Identity.Test.Integration.net45.HeadlessTests
             Assert.IsNotNull(authResult.IdToken);
             Assert.IsTrue(string.Equals(_deviceAuthuser, authResult.Account.Username, StringComparison.InvariantCultureIgnoreCase));
         }
-#endif
+
     }
 }
