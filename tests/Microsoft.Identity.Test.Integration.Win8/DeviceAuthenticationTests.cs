80c95d4ecbb818f7cb74bbdb95c5214ba27a2e7f
Trwalke <trwalke@microsoft.com>
2020-05-19 09:00:38 -0700

    Addressing PR feedback

--- tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
+++ tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
@@ -2,10 +2,12 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Json.Linq;
+using Microsoft.Identity.Test.Integration.net45.Infrastructure;
 using Microsoft.Identity.Test.LabInfrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,20 +17,28 @@ namespace Microsoft.Identity.Test.Integration.net45.HeadlessTests
     public class DeviceAuthenticationTests
     {
         private const string _claims = "{\"access_token\":{\"deviceid\":{\"essential\":true}}}";
-        private const string _publicClientId = "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0";
         private const string _deviceAuthuser = "idlabca@msidlab8.onmicrosoft.com";
 
         [TestMethod]
 
-        public void PKeyAuthNonInteractiveTest()
+        public async void PKeyAuthNonInteractiveTestAsync()
         {
+            if (Environment.OSVersion.Version.Major > 10)
+            {
+                //This test cannot run on Windows 10 or greater
+                Assert.Inconclusive();
+            }
+
             //Arrange
+            var labResponse = await LabUserHelper.GetSpecificUserAsync(_deviceAuthuser).ConfigureAwait(false);
+            var factory = new HttpSnifferClientFactory();
             var msalPublicClient = PublicClientApplicationBuilder
-                .Create(_publicClientId)
+                .Create(labResponse.App.AppId)
+                .WithHttpClientFactory(factory)
                 .Build();
 
             //Act
-            var authResult = msalPublicClient.AcquireTokenByUsernamePassword(new[] { "user.read" }, _deviceAuthuser, new NetworkCredential("", LabUserHelper.FetchUserPassword("msidlab8")).SecurePassword)
+            var authResult = msalPublicClient.AcquireTokenByUsernamePassword(new[] { "user.read" }, _deviceAuthuser, new NetworkCredential("", labResponse.User.GetOrFetchPassword()).SecurePassword)
             .WithClaims(JObject.Parse(_claims).ToString())
             .ExecuteAsync(CancellationToken.None).Result;
 
@@ -37,6 +47,15 @@ namespace Microsoft.Identity.Test.Integration.net45.HeadlessTests
             Assert.IsNotNull(authResult.AccessToken);
             Assert.IsNotNull(authResult.IdToken);
             Assert.IsTrue(string.Equals(_deviceAuthuser, authResult.Account.Username, StringComparison.InvariantCultureIgnoreCase));
+
+            var (req, res) = factory.RequestsAndResponses
+                .Where(x => x.Item1.RequestUri.AbsoluteUri == labResponse.Lab.Authority + "organizations/oauth2/v2.0/token" 
+                         && x.Item2.StatusCode == HttpStatusCode.OK).ElementAt(1);
+
+            var AuthHeader = req.Headers.Single(h => h.Key == "Authorization").Value.FirstOrDefault();
+
+            Assert.IsTrue(!string.IsNullOrEmpty(AuthHeader));
+            Assert.IsTrue(AuthHeader.Contains("PKeyAuth"));
         }
 
     }
