7d76ec2f1dbf8c8f6398508101ccebe3b49dcb47
Trwalke <trwalke@microsoft.com>
2020-07-07 08:47:42 -0700

    Remove Appcenter flag

--- src/client/Microsoft.Identity.Client/Platforms/iOS/SystemWebview/ASWebAuthenticationPresentationContextProviderWindow.cs
+++ src/client/Microsoft.Identity.Client/Platforms/iOS/SystemWebview/ASWebAuthenticationPresentationContextProviderWindow.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#if !IS_APPCENTER_BUILD
 using AuthenticationServices;
-#endif
 using Foundation;
 using UIKit;
 
@@ -11,7 +9,6 @@ namespace Microsoft.Identity.Client.Platforms.iOS.SystemWebview
     /* For app center builds, this will need to build on a hosted mac agent. The mac agent does not have the latest SDK's required to build 'ASWebAuthenticationSession'
 * Until the agents are updated, appcenter build will need to ignore the use of 'ASWebAuthenticationSession' for iOS 12.*/
 
-#if !IS_APPCENTER_BUILD
     internal class ASWebAuthenticationPresentationContextProviderWindow : NSObject, IASWebAuthenticationPresentationContextProviding
     {
         public UIWindow GetPresentationAnchor(ASWebAuthenticationSession session)
@@ -19,5 +16,4 @@ namespace Microsoft.Identity.Client.Platforms.iOS.SystemWebview
             return UIApplication.SharedApplication.KeyWindow;
         }
     }
-#endif
 }
