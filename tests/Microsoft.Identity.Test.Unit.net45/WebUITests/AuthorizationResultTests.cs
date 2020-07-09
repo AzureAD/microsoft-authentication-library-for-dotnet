4d7a12377b13cd46468ec6c81a3ee83948f71ac8
Bogdan Gavril <bogavril@microsoft.com>
2020-07-09 11:18:04 +0100

    Improve error message for embedded webview http redirect failure (#1916)
    * Improve error message for embedded webview http redirect failure
    
    * Update AuthorizationResultTests.cs

---
+++ tests/Microsoft.Identity.Test.Unit.net45/WebUITests/AuthorizationResultTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.UI;
+using Microsoft.Identity.Client.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Identity.Test.Unit.WebUITests
+{
+    [TestClass]
+    public class AuthorizationResultTests
+    {
+        [TestMethod]
+        public void UrlInErrorDescriptionTest()
+        {
+            Uri uri = new Uri("http://some_url.com?q=p");
+            UriBuilder errorUri = new UriBuilder(TestConstants.AuthorityHomeTenant)
+            {
+                Query = string.Format(
+                           CultureInfo.InvariantCulture,
+                           "error={0}&error_description={1}",
+                           MsalError.NonHttpsRedirectNotSupported,
+                           MsalErrorMessage.NonHttpsRedirectNotSupported + " - " + CoreHelpers.UrlEncode(uri.AbsoluteUri))
+            };
+
+            var result = AuthorizationResult.FromUri(errorUri.Uri.AbsoluteUri);
+            Assert.AreEqual(MsalErrorMessage.NonHttpsRedirectNotSupported + " - " + uri.AbsoluteUri, result.ErrorDescription);
+            Assert.AreEqual(MsalError.NonHttpsRedirectNotSupported, result.Error);
+        }
+    }
+}
