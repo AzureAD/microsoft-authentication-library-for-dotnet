d801d1f44cc4ae82495a30bcb0de9733a436afca
Bogdan Gavril <bogavril@microsoft.com>
2020-07-13 20:58:59 +0100

    Update TokenSource.cs

--- src/client/Microsoft.Identity.Client/TokenSource.cs
+++ src/client/Microsoft.Identity.Client/TokenSource.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 namespace Microsoft.Identity.Client
 {
     /// <summary>
-    /// Specifies what is the source of token in the authentication result.
+    /// Specifies the source of the access and Id tokens in the authentication result.
     /// </summary>
     public enum TokenSource
     {
         /// <summary>
-        /// The source of token is cache.
+        /// The source of access and Id token is MSAL's cache.
         /// </summary>
         Cache,
         /// <summary>
-        /// The source of token is Identity Provider like AAD, ADFS or B2C.
+        /// The source of the access and Id token is Identity Provider - Azure Active Directory (AAD), ADFS or AAD B2C.
         /// </summary>
         IdentityProvider,
         /// <summary>
-        /// The source of token is Broker.
+        /// The source of the access and Id token is a broker application - Authenticator or Company Portal. Brokers are supported only on Android and iOS.
         /// </summary>
         Broker
     }
