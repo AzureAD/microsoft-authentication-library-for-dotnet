298d55d9ce90c5df9a830fc12f3df39b16c888d4
Bogdan Gavril <bogavril@microsoft.com>
2020-07-23 19:11:41 +0100

    Fix typo

--- src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
+++ src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
@@ -317,7 +317,7 @@ namespace Microsoft.Identity.Client
 
         public const string CryptoNet45 = 
             "Could not use the certificate for signing. See inner exception for details. " +
-            "Possible cause: this may be a known issue with apps build againt .NET Desktop 4.6 or lower. " +
+            "Possible cause: this may be a known issue with apps build against .NET Desktop 4.6 or lower. " +
             "Either target a higher version of .NET desktop - 4.6.1 and above, " +
             "or use a different certificate type (non-CNG) or sign your own assertion " +
             "as described at https://aka.ms/msal-net-signed-assertion";
