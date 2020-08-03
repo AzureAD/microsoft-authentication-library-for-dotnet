d36d5867d1af9f969b6aed805d746c650f9106f0
Bogdan Gavril <bogavril@microsoft.com>
2020-08-03 15:10:53 +0100

    Pull id token along AT when looking in the cache for OBO

--- src/client/Microsoft.Identity.Client/Internal/Requests/OnBehalfOfRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/OnBehalfOfRequest.cs
@@ -44,11 +44,15 @@ namespace Microsoft.Identity.Client.Internal.Requests
             MsalAccessTokenCacheItem msalAccessTokenItem = await CacheManager.FindAccessTokenAsync().ConfigureAwait(false);
             if (msalAccessTokenItem != null)
             {
+                var msalIdTokenItem = await CacheManager.GetIdTokenCacheItemAsync(msalAccessTokenItem.GetIdTokenItemKey()).ConfigureAwait(false);
+                AuthenticationRequestParameters.RequestContext.Logger.Info(
+                    "OBO found a valid access token in the cache. ID token also found? " + (msalIdTokenItem != null));
+
                 AuthenticationRequestParameters.RequestContext.ApiEvent.IsAccessTokenCacheHit = true;
 
                 return new AuthenticationResult(
-                    msalAccessTokenItem, 
-                    null,
+                    msalAccessTokenItem,
+                    msalIdTokenItem,
                     AuthenticationRequestParameters.AuthenticationScheme,
                     AuthenticationRequestParameters.RequestContext.CorrelationId,
                     TokenSource.Cache);
