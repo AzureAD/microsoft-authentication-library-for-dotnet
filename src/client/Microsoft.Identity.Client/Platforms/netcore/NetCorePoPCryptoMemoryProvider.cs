afc562205ea7a7764dd5b6aafba0f6fb16988c7e
Trwalke <trwalke@microsoft.com>
2020-08-11 12:35:56 -0700

    Adding new key management strategy. adding netcore implementation

--- src/client/Microsoft.Identity.Client/Platforms/netcore/NetCorePoPCryptoMemoryProvider.cs
+++ src/client/Microsoft.Identity.Client/Platforms/netcore/NetCorePoPCryptoMemoryProvider.cs
@@ -8,7 +8,6 @@ using Microsoft.Identity.Client.Utils;
 
 namespace Microsoft.Identity.Client.Platforms.net45
 {
-
     /// <summary>
     /// This provider is based on the RSACryptoServiceProvider container concept. 
     /// It will create and store a pair of keys in the context of the current user. Subsequent calls to this provider will reuse this key.
@@ -16,56 +15,72 @@ namespace Microsoft.Identity.Client.Platforms.net45
     /// <remarks>
     /// Key creation and storage only works on Windows. See https://stackoverflow.com/questions/41986995/implement-rsa-in-net-core/42006084 for more details.
     /// </remarks>
-    internal class NetDesktopPoPCryptoProvider : IPoPCryptoProvider, IDisposable
+    internal class NetCorePoPCryptoMemoryProvider
     {
         internal /* internal for test only */ const int RsaKeySize = 2048;
         internal /* internal for test only */ const string ContainerName = "com.microsoft.msal";
-        private readonly RSACryptoServiceProvider s_InMemorySigningKey;
-        private bool s_PersistKey = false;
+        private static RSA s_InMemorySigningKey;
+        private bool s_PersistKey;
+        private DateTime s_KeyTimeValidTo;
+        private readonly uint s_DefaultKeyExpirationTime = 60 * 60 * 8; // Eight Hours
 
-        // This is a singleton because the key is the same on a device
-        private static readonly Lazy<NetDesktopPoPCryptoProvider> lazyInstance =
-            new Lazy<NetDesktopPoPCryptoProvider>(() => new NetDesktopPoPCryptoProvider());
-
-        private RSACryptoServiceProvider RsaKey
+        private RSA RsaKey
         {
             get
             {
                 if (s_PersistKey)
                 {
-                    if (!CheckKeyExpiration())
+                    if (CheckKeyExpiration())
                     {
-                        s_SigningKey = RSA.Create();
-                        s_SigningKey.KeySize = RsaKeySize;
-                        return s_SigningKey;
+                        s_InMemorySigningKey = RSA.Create();
+                        s_InMemorySigningKey.KeySize = RsaKeySize;
+                        return s_InMemorySigningKey;
                     }
 
-                    return 
+                    return s_InMemorySigningKey;
                 }
 
                 return GetOrCreatePersistededKey(ContainerName);
             }
         }
 
-        public static NetDesktopPoPCryptoProvider Instance { get { return lazyInstance.Value; } }
+        // This is a singleton because the key is the same on a device
+        private static readonly Lazy<NetCorePoPCryptoMemoryProvider> lazyInstance =
+            new Lazy<NetCorePoPCryptoMemoryProvider>(() => new NetCorePoPCryptoMemoryProvider());
 
-        private NetDesktopPoPCryptoProvider(bool persisKey = false)
-        {
-            if (persisKey)
-            {
-                s_InMemorySigningKey = GetOrCreatePersistededKey(ContainerName);
-            }
-            
-            RSAParameters publicKeyInfo = RsaKey.ExportParameters(false);
-
-            CannonicalPublicKeyJwk = ComputeCannonicalJwk(publicKeyInfo);
-        }
+        public static NetCorePoPCryptoMemoryProvider Instance { get { return lazyInstance.Value; } }
 
         public string CannonicalPublicKeyJwk { get; }
 
+        private NetCorePoPCryptoMemoryProvider(bool persistKey = false)
+        {
+            if (persistKey)
+            {
+                s_InMemorySigningKey = GetOrCreatePersistededKey(ContainerName);
+            }
+
+            RSAParameters publicKeyInfo = s_InMemorySigningKey.ExportParameters(false);
+
+            CannonicalPublicKeyJwk = ComputeCannonicalJwk(publicKeyInfo);
+            s_PersistKey = persistKey;
+        }
+
+
         public byte[] Sign(byte[] payload)
         {
-            return RsaKey.SignData(payload, CryptoConfig.MapNameToOID("SHA256"));
+            return RsaKey.SignData(payload, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+
+        private bool CheckKeyExpiration()
+        {
+            return ConvertToTimeT(s_KeyTimeValidTo) <= ConvertToTimeT(DateTime.Now);
+        }
+
+        internal static long ConvertToTimeT(DateTime time)
+        {
+            var startTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan diff = time - startTime;
+            return (long)diff.TotalSeconds;
         }
 
         private static RSACryptoServiceProvider GetOrCreatePersistededKey(string containerName)
@@ -98,23 +113,6 @@ namespace Microsoft.Identity.Client.Platforms.net45
         private static string ComputeCannonicalJwk(RSAParameters rsaPublicKey)
         {
             return $@"{{""{JsonWebKeyParameterNames.E}"":""{Base64UrlHelpers.Encode(rsaPublicKey.Exponent)}"",""{JsonWebKeyParameterNames.Kty}"":""{JsonWebAlgorithmsKeyTypes.RSA}"",""{JsonWebKeyParameterNames.N}"":""{Base64UrlHelpers.Encode(rsaPublicKey.Modulus)}""}}";
-        }
-
-        public void Dispose()
-        {
-            s_InMemorySigningKey.Dispose();
-        }
-
-        private bool CheckKeyExpiration()
-        {
-            return ConvertToTimeT(s_KeyTimeValidTo) <= ConvertToTimeT(DateTime.Now);
-        }
-
-        internal static long ConvertToTimeT(DateTime time)
-        {
-            var startTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            TimeSpan diff = time - startTime;
-            return (long)diff.TotalSeconds;
         }
     }
 }
