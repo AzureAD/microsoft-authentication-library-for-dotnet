b3ea5720a7956060bc157929126bfcf0df7b2c70
Bogdan Gavril <bogavril@microsoft.com>
2020-08-21 12:39:45 +0100

    fix a namespace

--- tests/Microsoft.Identity.Test.Unit.net45/pop/CryptoProviderTests.cs
+++ tests/Microsoft.Identity.Test.Unit.net45/pop/CryptoProviderTests.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Identity.Client.PlatformsCommon;
+using Microsoft.Identity.Client.AuthScheme.PoP;
 using Microsoft.Identity.Json.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
