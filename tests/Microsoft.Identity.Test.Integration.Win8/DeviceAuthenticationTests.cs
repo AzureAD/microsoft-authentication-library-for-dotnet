459bb0e48c7b99a871af795d47416447d9f05c65
Jenny Ferries <jeferrie@microsoft.com>
2020-11-24 09:04:25 -0800

    pr feedback

--- tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
+++ tests/Microsoft.Identity.Test.Integration.Win8/DeviceAuthenticationTests.cs
@@ -24,44 +24,37 @@ namespace Microsoft.Identity.Test.Integration.Win8
 
         public async Task PKeyAuthNonInteractiveTestAsync()
         {
-            try
-            {
-                //Arrange
-                var labResponse = await LabUserHelper.GetSpecificUserAsync(_deviceAuthuser).ConfigureAwait(false);
-                var factory = new HttpSnifferClientFactory();
-                var msalPublicClient = PublicClientApplicationBuilder
-                    .Create(labResponse.App.AppId)
-                    .WithAuthority("https://login.microsoftonline.com/organizations/")
-                    .WithHttpClientFactory(factory)
-                    .Build();
+            //Arrange
+            var labResponse = await LabUserHelper.GetSpecificUserAsync(_deviceAuthuser).ConfigureAwait(false);
+            var factory = new HttpSnifferClientFactory();
+            var msalPublicClient = PublicClientApplicationBuilder
+                .Create(labResponse.App.AppId)
+                .WithAuthority("https://login.microsoftonline.com/organizations/")
+                .WithHttpClientFactory(factory)
+                .Build();
 
-                //Act
-               var authResult = msalPublicClient.AcquireTokenByUsernamePassword(
-                    new[] { "user.read" },
-                    labResponse.User.Upn,
-                    new NetworkCredential("", labResponse.User.GetOrFetchPassword()).SecurePassword)
-                .WithClaims(JObject.Parse(_claims).ToString())
-                .ExecuteAsync(CancellationToken.None).Result;
+            //Act
+            var authResult = msalPublicClient.AcquireTokenByUsernamePassword(
+                 new[] { "user.read" },
+                 labResponse.User.Upn,
+                 new NetworkCredential("", labResponse.User.GetOrFetchPassword()).SecurePassword)
+             .WithClaims(JObject.Parse(_claims).ToString())
+             .ExecuteAsync(CancellationToken.None).Result;
 
-                //Assert
-                Assert.IsNotNull(authResult);
-                Assert.IsNotNull(authResult.AccessToken);
-                Assert.IsNotNull(authResult.IdToken);
-                Assert.IsTrue(string.Equals(_deviceAuthuser, authResult.Account.Username, StringComparison.InvariantCultureIgnoreCase));
+            //Assert
+            Assert.IsNotNull(authResult);
+            Assert.IsNotNull(authResult.AccessToken);
+            Assert.IsNotNull(authResult.IdToken);
+            Assert.IsTrue(string.Equals(_deviceAuthuser, authResult.Account.Username, StringComparison.InvariantCultureIgnoreCase));
 
-                var (req, res) = factory.RequestsAndResponses
-                    .Where(x => x.Item1.RequestUri.AbsoluteUri == labResponse.Lab.Authority + "organizations/oauth2/v2.0/token"
-                             && x.Item2.StatusCode == HttpStatusCode.OK).ElementAt(1);
+            var (req, res) = factory.RequestsAndResponses
+                .Where(x => x.Item1.RequestUri.AbsoluteUri == labResponse.Lab.Authority + "organizations/oauth2/v2.0/token"
+                         && x.Item2.StatusCode == HttpStatusCode.OK).ElementAt(1);
 
-                var AuthHeader = req.Headers.Single(h => h.Key == "Authorization").Value.FirstOrDefault();
+            var AuthHeader = req.Headers.Single(h => h.Key == "Authorization").Value.FirstOrDefault();
 
-                Assert.IsTrue(!string.IsNullOrEmpty(AuthHeader));
-                Assert.IsTrue(AuthHeader.Contains("PKeyAuth"));
-            }
-            catch(Exception ex)
-            {
-                Assert.Fail("Device Auth test failed. " + ex.Message + " Inner Exception: " + ex.InnerException.Message);
-            }
+            Assert.IsTrue(!string.IsNullOrEmpty(AuthHeader));
+            Assert.IsTrue(AuthHeader.Contains("PKeyAuth"));
         }
     }
 }
