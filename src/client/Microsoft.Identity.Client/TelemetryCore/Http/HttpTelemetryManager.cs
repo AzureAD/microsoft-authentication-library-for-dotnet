33cde04ab542fafec93cdec6bb3f7f2f58cb308b
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2020-12-02 15:50:33 -0800

    Fix telemetry, remove if block while adding telemetry logs (#2266)
    * Fix telemetry, remove if block while adding telemetry logs
    
    * Fix last telemetry header
    
    * Fix test for last header

--- src/client/Microsoft.Identity.Client/TelemetryCore/Http/HttpTelemetryManager.cs
+++ src/client/Microsoft.Identity.Client/TelemetryCore/Http/HttpTelemetryManager.cs
@@ -76,15 +76,13 @@ namespace Microsoft.Identity.Client.TelemetryCore.Http
                 failedRequests.Append(",");
                 failedRequests.Append(ev.CorrelationId);
 
-                if (ev.ContainsKey(MsalTelemetryBlobEventNames.RegionDiscovered))
-                {
-                    if (!firstFailure)
-                        platformFields.Append(",");
-
-                    platformFields.Append(ev.RegionDiscovered);
+                if (!firstFailure)
                     platformFields.Append(",");
-                    platformFields.Append(ev.RegionSource);
-                }
+
+                ev.TryGetValue(MsalTelemetryBlobEventNames.RegionSource, out string regionSource);
+                platformFields.Append(ev.RegionDiscovered);
+                platformFields.Append(",");
+                platformFields.Append(regionSource);
 
                 firstFailure = false;
             }
@@ -125,17 +123,13 @@ namespace Microsoft.Identity.Client.TelemetryCore.Http
 
             // Since regional fields will only be logged in case it is opted.
             var regionalFields = new StringBuilder();
-            if (!string.IsNullOrEmpty(regionDiscovered))
-            {
-                regionalFields.Append(regionDiscovered);
-                regionalFields.Append(",");
-                regionalFields.Append((regionSource));
-                regionalFields.Append(",");
-            }
+            regionalFields.Append(regionDiscovered);
+            regionalFields.Append(",");
+            regionalFields.Append((regionSource));
 
             return $"{TelemetryConstants.HttpTelemetrySchemaVersion2}" +
                 $"|{apiId},{ConvertFromStringToBitwise(forceRefresh)}" +
-                $"|{regionalFields}{ConvertFromStringToBitwise(isTokenCacheSerialized)}";
+                $"|{regionalFields},{ConvertFromStringToBitwise(isTokenCacheSerialized)}";
         }
 
         private string ConvertFromStringToBitwise(string value)
