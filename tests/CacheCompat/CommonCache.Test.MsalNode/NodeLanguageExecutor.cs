8a8bd694fdd0e3de83dc0bfb216794df4668082a
Bogdan Gavril <bogavril@microsoft.com>
2020-12-03 14:00:39 +0000

    Cache compat with MSAL.node tests (#2262)
    * Cache compat node tests
    
    * latest
    
    * Update index.js

---
+++ tests/CacheCompat/CommonCache.Test.MsalNode/NodeLanguageExecutor.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommonCache.Test.Common;
+
+namespace CommonCache.Test.MsalPython
+{
+    public class NodeLanguageExecutor : ILanguageExecutor
+    {
+        public async Task<ProcessRunResults> ExecuteAsync(
+            string arguments,
+            CancellationToken cancellationToken)
+        {
+            var processUtils = new ProcessUtils();
+
+            const string npm = "npm.cmd";
+            string npmPath = await processUtils.FindProgramAsync(npm, cancellationToken).ConfigureAwait(false);
+
+            string directoryWithScript = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "NodeScript");
+
+            try
+            {
+                var result = await processUtils.RunProcessAsync(npmPath, $"install {directoryWithScript}").ConfigureAwait(false);
+            }            
+            catch (ProcessRunException ex)
+            {
+                Console.WriteLine(ex.ProcessStandardOutput);
+                throw;
+            }
+
+            string executablePath = "node.exe";
+
+            executablePath = await processUtils.FindProgramAsync(executablePath, cancellationToken).ConfigureAwait(false);
+            string executeArgs = $"{directoryWithScript}\\index.js {arguments}";
+
+            Console.WriteLine($"Calling: {executablePath} {executeArgs}");
+            ProcessRunResults processRunResults = await processUtils.RunProcessAsync(executablePath, executeArgs, cancellationToken).ConfigureAwait(false);
+            return processRunResults;
+        }
+    }
+}
--- tests/CacheCompat/CommonCache.Test.Unit/Utils/CacheProgramFactory.cs
+++ tests/CacheCompat/CommonCache.Test.Unit/Utils/CacheProgramFactory.cs
@@ -19,36 +19,40 @@ namespace CommonCache.Test.Unit.Utils
 
             switch (cacheProgramType)
             {
-            case CacheProgramType.AdalV3:
-                executablePath = Path.Combine(BaseExecutablePath, "AdalV3", "CommonCache.Test.AdalV3.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv3results.json");
-                break;
-            case CacheProgramType.AdalV4:
-                executablePath = Path.Combine(BaseExecutablePath, "AdalV4", "CommonCache.Test.AdalV4.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv4results.json");
-                break;
-            case CacheProgramType.AdalV5:
-                executablePath = Path.Combine(BaseExecutablePath, "AdalV5", "CommonCache.Test.AdalV5.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv5results.json");
-                break;
-            case CacheProgramType.MsalV2:
-                executablePath = Path.Combine(BaseExecutablePath, "MsalV2", "CommonCache.Test.MsalV2.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msalv2results.json");
-                break;
-            case CacheProgramType.MsalV3:
-                executablePath = Path.Combine(BaseExecutablePath, "MsalV3", "CommonCache.Test.MsalV3.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msalv3results.json");
-                break;
-            case CacheProgramType.MsalPython:
-                executablePath = Path.Combine(BaseExecutablePath, "MsalPython", "CommonCache.Test.MsalPython.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msal_python_results.json");
-                break;
-            case CacheProgramType.MsalJava:
-                executablePath = Path.Combine(BaseExecutablePath, "MsalJava", "CommonCache.Test.MsalJava.exe");
-                resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msal_java_results.json");
-                break;
-            default:
-                throw new ArgumentException("Unknown cacheProgramType", nameof(cacheProgramType));
+                case CacheProgramType.AdalV3:
+                    executablePath = Path.Combine(BaseExecutablePath, "AdalV3", "CommonCache.Test.AdalV3.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv3results.json");
+                    break;
+                case CacheProgramType.AdalV4:
+                    executablePath = Path.Combine(BaseExecutablePath, "AdalV4", "CommonCache.Test.AdalV4.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv4results.json");
+                    break;
+                case CacheProgramType.AdalV5:
+                    executablePath = Path.Combine(BaseExecutablePath, "AdalV5", "CommonCache.Test.AdalV5.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "adalv5results.json");
+                    break;
+                case CacheProgramType.MsalV2:
+                    executablePath = Path.Combine(BaseExecutablePath, "MsalV2", "CommonCache.Test.MsalV2.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msalv2results.json");
+                    break;
+                case CacheProgramType.MsalV3:
+                    executablePath = Path.Combine(BaseExecutablePath, "MsalV3", "CommonCache.Test.MsalV3.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msalv3results.json");
+                    break;
+                case CacheProgramType.MsalPython:
+                    executablePath = Path.Combine(BaseExecutablePath, "MsalPython", "CommonCache.Test.MsalPython.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msal_python_results.json");
+                    break;
+                case CacheProgramType.MsalJava:
+                    executablePath = Path.Combine(BaseExecutablePath, "MsalJava", "CommonCache.Test.MsalJava.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msal_java_results.json");
+                    break;
+                case CacheProgramType.MsalNode:
+                    executablePath = Path.Combine(BaseExecutablePath, "MsalNode", "CommonCache.Test.MsalNode.exe");
+                    resultsFilePath = Path.Combine(CommonCacheTestUtils.CacheFileDirectory, "msal_node_results.json");
+                    break;
+                default:
+                    throw new ArgumentException("Unknown cacheProgramType", nameof(cacheProgramType));
             }
 
             return new CacheProgram(executablePath, resultsFilePath, cacheStorageType);
