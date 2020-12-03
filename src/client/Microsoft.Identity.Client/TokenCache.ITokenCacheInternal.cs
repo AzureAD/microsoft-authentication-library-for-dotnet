f3d3031ad42616ef82e45586c34dca9ceba622fd
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-12-03 13:27:27 -0800

    Add a comment with wiki link.

--- src/client/Microsoft.Identity.Client/TokenCache.ITokenCacheInternal.cs
+++ src/client/Microsoft.Identity.Client/TokenCache.ITokenCacheInternal.cs
@@ -20,7 +20,8 @@ using Microsoft.Identity.Client.Utils;
 namespace Microsoft.Identity.Client
 {
     /// <summary>
-    /// IMPORTANT: this class is perf critical; any changes must be benchmarked using Microsoft.Identity.Test.Performace
+    /// IMPORTANT: this class is perf critical; any changes must be benchmarked using Microsoft.Identity.Test.Performace.
+    /// More information about how to test and what data to look for is in https://aka.ms/msal-net-performance-testing.
     /// </summary>
     public sealed partial class TokenCache : ITokenCacheInternal
     {
@@ -283,7 +284,8 @@ namespace Microsoft.Identity.Client
         }
 
         /// <summary>
-        /// IMPORTANT: this class is perf critical; any changes must be benchmarked using Microsoft.Identity.Test.Performace
+        /// IMPORTANT: this class is perf critical; any changes must be benchmarked using Microsoft.Identity.Test.Performace.
+        /// More information about how to test and what data to look for is in https://aka.ms/msal-net-performance-testing.
         /// 
         /// Scenario: client_creds with default in-memory cache can get to ~500k tokens
         /// </summary>
