89a115479a52f8b55cf55140bff6b09fdeddd503
pmaytak <34331512+pmaytak@users.noreply.github.com>
2020-12-18 02:32:32 -0800

    Add GetRefreshToken test.

--- tests/Microsoft.Identity.Client.Performance/AdalCacheOperationsTests.cs
+++ tests/Microsoft.Identity.Client.Performance/AdalCacheOperationsTests.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Client.Cache.Items;
 using Microsoft.Identity.Client.Instance;
 using Microsoft.Identity.Client.Instance.Discovery;
 using Microsoft.Identity.Client.Internal;
 using Microsoft.Identity.Client.Internal.Requests;
 using Microsoft.Identity.Client.OAuth2;
 using Microsoft.Identity.Test.Common;
-using Microsoft.Identity.Test.Common.Core.Helpers;
 using Microsoft.Identity.Test.Common.Core.Mocks;
 using Microsoft.Identity.Test.Unit;
 
 namespace Microsoft.Identity.Test.Performance
 {
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
     public class AdalCacheOperationsTests
     {
         private ITokenCacheInternal _cache;
@@ -27,13 +26,17 @@ namespace Microsoft.Identity.Test.Performance
         [Params(1, 100, 1000)]
         public int TokenCacheSize { get; set; }
 
-        [GlobalSetup(Target = nameof(SaveTokenResponse_EnabledAdalCache_TestAsync))]
+        [GlobalSetup(Targets = new[] { 
+            nameof(SaveTokenResponse_EnabledAdalCache_TestAsync), 
+            nameof(FindRefreshToken_EnabledAdalCache_TestAsync) })]
         public void GlobalSetup_EnabledAdalCache()
         {
             GlobalSetup(true);
         }
 
-        [GlobalSetup(Target = nameof(SaveTokenResponse_DisabledAdalCache_TestAsync))]
+        [GlobalSetup(Targets = new[] { 
+            nameof(SaveTokenResponse_DisabledAdalCache_TestAsync), 
+            nameof(FindRefreshToken_DisabledAdalCache_TestAsync) })]
         public void GlobalSetup_DisabledAdalCache()
         {
             GlobalSetup(false);
@@ -56,13 +59,14 @@ namespace Microsoft.Identity.Test.Performance
             TokenCacheHelper.AddRefreshTokensToCache(_cache.Accessor, TokenCacheSize);
         }
 
-        [Benchmark(Baseline = true)]
+        #region SaveToken
+        [BenchmarkCategory("SaveTokenResponse"), Benchmark(Baseline = true)]
         public async Task<string> SaveTokenResponse_EnabledAdalCache_TestAsync()
         {
             return await SaveTokenResponse_TestAsync().ConfigureAwait(true);
         }
 
-        [Benchmark]
+        [BenchmarkCategory("SaveTokenResponse"), Benchmark]
         public async Task<string> SaveTokenResponse_DisabledAdalCache_TestAsync()
         {
             return await SaveTokenResponse_TestAsync().ConfigureAwait(true);
@@ -73,6 +77,27 @@ namespace Microsoft.Identity.Test.Performance
             var result = await _cache.SaveTokenResponseAsync(_requestParams, _response).ConfigureAwait(true);
             return result.Item1.ClientId;
         }
+        #endregion
+
+        #region GetRefreshToken
+        [BenchmarkCategory("GetRefreshToken"), Benchmark(Baseline = true)]
+        public async Task<string> FindRefreshToken_EnabledAdalCache_TestAsync()
+        {
+            return await FindRefreshToken_TestAsync().ConfigureAwait(true);
+        }
+
+        [BenchmarkCategory("GetRefreshToken"), Benchmark]
+        public async Task<string> FindRefreshToken_DisabledAdalCache_TestAsync()
+        {
+            return await FindRefreshToken_TestAsync().ConfigureAwait(true);
+        }
+
+        private async Task<string> FindRefreshToken_TestAsync()
+        {
+            var result = await _cache.FindRefreshTokenAsync(_requestParams).ConfigureAwait(true);
+            return result.ClientId;
+        }
+        #endregion
 
         private void AddHostToInstanceCache(IServiceBundle serviceBundle, string host)
         {
