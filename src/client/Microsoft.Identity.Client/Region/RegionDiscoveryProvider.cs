29cb85e6c06b4ac8c2250e2cf796791a5656ad2a
Travis Walker <travis.walker@microsoft.com>
2020-12-29 14:13:16 -0800

    Update src/client/Microsoft.Identity.Client/Region/RegionDiscoveryProvider.cs
    Co-authored-by: Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>

--- src/client/Microsoft.Identity.Client/Region/RegionDiscoveryProvider.cs
+++ src/client/Microsoft.Identity.Client/Region/RegionDiscoveryProvider.cs
@@ -229,7 +229,7 @@ namespace Microsoft.Identity.Client.Region
                 {
                     if (requestContext.ServiceBundle.Config.AuthorityInfo.FallbackToGlobal)
                     {
-                        requestContext.Logger.Info($"Attempting to fall back to global region");
+                        requestContext.Logger.Info($"Attempting to fall back to global endpoint");
                         requestContext.ApiEvent.FallBackToGlobal = "1";
                         return null;
                     }
