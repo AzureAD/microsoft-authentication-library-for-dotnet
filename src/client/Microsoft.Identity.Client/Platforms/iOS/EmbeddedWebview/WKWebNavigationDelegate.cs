7ac824c52cdc07bf2b8692bf50766078d35e8e5b
Bogdan Gavril <bogavril@microsoft.com>
2021-02-03 13:16:35 +0000

    Fix for #2367 (#2374)
    * Fix for #2367
    
    * Fix build
    
    * fix
    
    * fix

--- src/client/Microsoft.Identity.Client/Platforms/iOS/EmbeddedWebview/WKWebNavigationDelegate.cs
+++ src/client/Microsoft.Identity.Client/Platforms/iOS/EmbeddedWebview/WKWebNavigationDelegate.cs
@@ -7,6 +7,7 @@ using CoreFoundation;
 using Foundation;
 using Microsoft.Identity.Client.Platforms.Shared.Apple;
 using Microsoft.Identity.Client.PlatformsCommon;
+using Microsoft.Identity.Client.PlatformsCommon.Shared;
 using Microsoft.Identity.Client.UI;
 using Microsoft.Identity.Client.Utils;
 using UIKit;
