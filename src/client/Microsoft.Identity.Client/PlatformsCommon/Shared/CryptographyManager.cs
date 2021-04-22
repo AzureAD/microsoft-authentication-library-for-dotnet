711a1198c072c89f74a62ec0f43edfa84f6b9060
Richard Fuoco <richf@ossiaco.com>
2021-04-22 14:40:29 -0400

    Update to multi-platform

--- src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/CryptographyManager.cs
+++ src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/CryptographyManager.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -13,12 +12,12 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
 {
     internal static class CryptographyManager
     {
-        private static readonly ConcurrentDictionary<string, RSA> s_certificateToRsaMap = new ConcurrentDictionary<string, RSA>();
+        private static readonly ConcurrentDictionary<string, AsymmetricAlgorithm> s_certificateToRsaMap = new ConcurrentDictionary<string, AsymmetricAlgorithm>();
         private static readonly int s_maximumMapSize = 1000;
 
         public static byte[] SignWithCertificate(string message, X509Certificate2 certificate)
         {
-            using var chain = new X509Chain
+            var chain = new X509Chain
             {
                 ChainPolicy = BuildChainPolicyChained(),
             };
@@ -30,7 +29,9 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
                 throw new InvalidOperationException($"An invalid certificate was supplied. {chainErrors}");
             }
 
-            if (!s_certificateToRsaMap.TryGetValue(certificate.Thumbprint, out var alg))
+            chain.Dispose();
+
+            if (!s_certificateToRsaMap.TryGetValue(certificate.Thumbprint, out AsymmetricAlgorithm alg))
             {
                 if (s_certificateToRsaMap.Count >= s_maximumMapSize)
                 {
@@ -40,19 +41,30 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
 #if NET5_0_OR_GREATER
                 alg = certificate.GetRSAPrivateKey() ?? certificate.GetECDsaPrivateKey() ?? (AsymmetricAlgorithm?)certificate.GetDSAPrivateKey() ?? throw new NotSupportedException("The certificate key algorithm is not supported.");
 #else
-                alg = certificate.GetRSAPrivateKey() ?? (AsymmetricAlgorithm?)certificate.GetECDsaPrivateKey() ?? throw new NotSupportedException("The certificate key algorithm is not supported.");
+                alg = certificate.GetRSAPrivateKey() ?? (AsymmetricAlgorithm)certificate.GetECDsaPrivateKey() ?? throw new NotSupportedException("The certificate key algorithm is not supported.");
 #endif
             }
 
-            var signedData = alg switch
+            byte[] signedData;
+            if (alg is RSA rsa)
             {
-                RSA rsa => rsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1),
-                ECDsa eCDsa => eCDsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256),
+                signedData = rsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+            else if (alg is ECDsa eCDsa)
+            {
+                signedData = eCDsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256);
+
+            }
 #if NET5_0_OR_GREATER
-                DSA dsa => dsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256),
+            else if (alg is DSA dsa)
+            {
+                signedData = dsa.SignData(Encoding.UTF8.GetBytes(message), HashAlgorithmName.SHA256);
+            }
 #endif
-                _ => throw new NotSupportedException("The certificate key algorithm is not supported."),
-            };
+            else
+            {
+                throw new NotSupportedException("The certificate key algorithm is not supported.");
+            }
 
             // Cache only valid crypto providers, which are able to sign data successfully
             s_certificateToRsaMap[certificate.Thumbprint] = alg;
@@ -71,7 +83,8 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
                 RevocationMode = revocationMode,
             };
 
-            _ = chainPolicy.ApplicationPolicy.Add(OidLookup.DocumentSigning);
+            // Digital Signature
+            _ = chainPolicy.ApplicationPolicy.Add(new Oid("1.3.6.1.4.1.311.10.3.12"));
             return chainPolicy;
         }
     }
