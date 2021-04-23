82d1ea6b1c67435d36e31f79d37744ab8fdc178d
Richard Fuoco <richf@ossiaco.com>
2021-04-23 14:34:32 -0400

    Removed Certificate Chain validation

--- src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/CryptographyManager.cs
+++ src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/CryptographyManager.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Concurrent;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -17,20 +16,6 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
 
         public static byte[] SignWithCertificate(string message, X509Certificate2 certificate)
         {
-            var chain = new X509Chain
-            {
-                ChainPolicy = BuildChainPolicyChained(),
-            };
-
-            var certificateIsValid = chain.Build(certificate);
-            if (!certificateIsValid)
-            {
-                var chainErrors = string.Join(", ", chain.ChainStatus.Select(e => e.Status.ToString()));
-                throw new InvalidOperationException($"An invalid certificate was supplied. {chainErrors}");
-            }
-
-            chain.Dispose();
-
             if (!s_certificateToRsaMap.TryGetValue(certificate.Thumbprint, out AsymmetricAlgorithm alg))
             {
                 if (s_certificateToRsaMap.Count >= s_maximumMapSize)
@@ -73,23 +58,6 @@ namespace Microsoft.Identity.Client.PlatformsCommon.Shared
             // Cache only valid crypto providers, which are able to sign data successfully
             s_certificateToRsaMap[certificate.Thumbprint] = alg;
             return signedData;
-        }
-
-        private static X509ChainPolicy BuildChainPolicyChained(
-            X509VerificationFlags verificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority,
-            X509RevocationFlag revocationFlag = X509RevocationFlag.EndCertificateOnly,
-            X509RevocationMode revocationMode = X509RevocationMode.NoCheck)
-        {
-            var chainPolicy = new X509ChainPolicy
-            {
-                VerificationFlags = verificationFlags,
-                RevocationFlag = revocationFlag,
-                RevocationMode = revocationMode,
-            };
-
-            // Digital Signature
-            _ = chainPolicy.ApplicationPolicy.Add(new Oid("1.3.6.1.4.1.311.10.3.12"));
-            return chainPolicy;
         }
     }
 }
