6056ccdb09f62970696af676a9dcfedc63887de6
Bogdan Gavril <bogavril@microsoft.com>
2021-04-27 19:10:50 +0100

    Update comments

--- src/client/Microsoft.Identity.Client/AuthenticationResultMetadata.cs
+++ src/client/Microsoft.Identity.Client/AuthenticationResultMetadata.cs
@@ -24,17 +24,19 @@ namespace Microsoft.Identity.Client
         public TokenSource TokenSource { get; }
 
         /// <summary>
-        /// Total time spent to service this request. Includes TimeInHttp and TimeInCache. All times in ms.
+        /// Total time (in ms) spent to service this request, in ms. Includes time spent making Http Requests <see cref="TimeSpentInHttp"/>, time spent
+        /// in token cache callbacks <see cref="TimeSpentInCache"/>, time spent in MSAL and context switching.
         /// </summary>
         public long TimeSpentInTotal { get; set; }
 
         /// <summary>
-        /// Time spent in the cache to service the request
+        /// Time (in ms) MSAL spent in reading and writing to the token cache, i.e. in the OnBeforeAccess, OnAfterAccess etc. callbacks. 
+        /// Does not include internal MSAL logic for searching through the cache once loaded.
         /// </summary>
         public long TimeSpentInCache { get; set; }
 
         /// <summary>
-        /// Time spent for HTTP communication
+        /// Time (in ms) MSAL spent for HTTP communication.
         /// </summary>
         public long TimeSpentInHttp { get; set; }
     }
