861a907d106cab1920a229e8cac0f3296c9e21c5
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-05-20 14:04:39 -0700

    nit.

--- tests/Microsoft.Identity.Test.Unit/TokenClientTests.cs
+++ tests/Microsoft.Identity.Test.Unit/TokenClientTests.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.OAuth2;
 using Microsoft.Identity.Client.Utils;
-using Microsoft.Identity.Test.Common.Core.Helpers;
 using Microsoft.Identity.Test.Common.Core.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium.Remote;
 
 namespace Microsoft.Identity.Test.Unit
 {
