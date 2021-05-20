03bfe760413c0f229e6feff790caf5f6015c3585
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-05-20 14:02:00 -0700

    Add unit test to test for missing token type in token response response.

---
+++ tests/Microsoft.Identity.Test.Unit/TokenClientTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.OAuth2;
+using Microsoft.Identity.Client.Utils;
+using Microsoft.Identity.Test.Common.Core.Helpers;
+using Microsoft.Identity.Test.Common.Core.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+
+namespace Microsoft.Identity.Test.Unit
+{
+    [TestClass]
+    public class TokenClientTests : TestBase
+    {
+        [TestMethod]
+        public async Task SendTokenRequest_MissingAccessTokenTypeInResponse_Throws_Async()
+        {
+            using (MockHttpAndServiceBundle harness = CreateTestHarness())
+            {
+                var requestParams = harness.CreateAuthenticationRequestParameters(TestConstants.AuthorityCommonTenant);
+                var tokenClient = new TokenClient(requestParams);
+
+                harness.HttpManager.AddInstanceDiscoveryMockHandler();
+                var fakeResponse = TestConstants.CreateMsalTokenResponse();
+                fakeResponse.TokenType = null;
+                harness.HttpManager.AddResponseMockHandlerForPost(MockHelpers.CreateSuccessResponseMessage(JsonHelper.SerializeToJson(fakeResponse)));
+                await requestParams.AuthorityManager.RunInstanceDiscoveryAndValidationAsync().ConfigureAwait(false);
+
+                var ex = await Assert.ThrowsExceptionAsync<MsalClientException>(
+                    () => tokenClient.SendTokenRequestAsync(new Dictionary<string, string>())).ConfigureAwait(false);
+                Assert.AreEqual(MsalError.AccessTokenTypeMissing, ex.ErrorCode);
+            }
+        }
+    }
+}
