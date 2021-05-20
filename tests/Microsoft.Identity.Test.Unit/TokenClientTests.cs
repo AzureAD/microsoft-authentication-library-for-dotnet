5eb456a72508656f2f6ec4f40b22d5460a884749
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-05-20 14:05:21 -0700

    nit.

--- tests/Microsoft.Identity.Test.Unit/TokenClientTests.cs
+++ tests/Microsoft.Identity.Test.Unit/TokenClientTests.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.OAuth2;
