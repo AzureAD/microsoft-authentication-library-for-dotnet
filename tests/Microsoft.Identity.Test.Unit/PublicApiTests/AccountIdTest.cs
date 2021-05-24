95a8137129cb77a2d9da2458599e5f76091e9f47
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2021-05-24 10:12:03 -0700

    Handle case when b2c profile contains a '.' (#2648)
    * Handle case when b2c profile contains a '.'
    
    * Add test policy to desktop app
    
    * Add tests for ParseFromString method
    
    * Update tests with DataTestMethod

--- tests/Microsoft.Identity.Test.Unit/PublicApiTests/AccountIdTest.cs
+++ tests/Microsoft.Identity.Test.Unit/PublicApiTests/AccountIdTest.cs
@@ -34,5 +34,21 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
            new AccountId("a.b", "a", "b");
            new AccountId("adfs");
         }
+
+        [DataTestMethod]
+        [DataRow("uid", "uid", null, DisplayName = "Parse from string with no dot")]
+        [DataRow("uid.utid", "uid", "utid", DisplayName = "Parse from string with one dot")]
+        [DataRow("uid.1.utid", "uid.1", "utid", DisplayName = "Parse from string with 2 dots")]
+        [DataRow("uid.1.2.3.utid", "uid.1.2.3", "utid", DisplayName = "Parse from string with more than 2 dots")]
+        [DataRow(".utid", "", "utid", DisplayName = "Parse from string with dot as first character")]
+        [DataRow("uid.", "uid", "", DisplayName = "Parse from string with dot as last character")]
+        [DataRow(".", "", "", DisplayName = "Parse from string with dot as only character")]
+        public void ParseFromStringNoDotTest(string homeAccountId, string objectId, string tenantId)
+        {
+            AccountId accountId = AccountId.ParseFromString(homeAccountId);
+
+            Assert.AreEqual(objectId, accountId.ObjectId);
+            Assert.AreEqual(tenantId, accountId.TenantId);
+        }
     }
 }
