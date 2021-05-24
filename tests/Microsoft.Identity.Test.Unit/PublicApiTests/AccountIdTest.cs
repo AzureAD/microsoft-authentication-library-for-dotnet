9542029964bafd5e7695625ef572805a0180bc4f
neha-bhargava <nebharg@microsoft.com>
2021-05-24 09:18:52 -0700

    Update tests with DataTestMethod

--- tests/Microsoft.Identity.Test.Unit/PublicApiTests/AccountIdTest.cs
+++ tests/Microsoft.Identity.Test.Unit/PublicApiTests/AccountIdTest.cs
@@ -35,74 +35,20 @@ namespace Microsoft.Identity.Test.Unit.PublicApiTests
            new AccountId("adfs");
         }
 
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with no dot")]
-        public void ParseFromStringNoDotTest()
+        [DataTestMethod]
+        [DataRow("uid", "uid", null, DisplayName = "Parse from string with no dot")]
+        [DataRow("uid.utid", "uid", "utid", DisplayName = "Parse from string with one dot")]
+        [DataRow("uid.1.utid", "uid.1", "utid", DisplayName = "Parse from string with 2 dots")]
+        [DataRow("uid.1.2.3.utid", "uid.1.2.3", "utid", DisplayName = "Parse from string with more than 2 dots")]
+        [DataRow(".utid", "", "utid", DisplayName = "Parse from string with dot as first character")]
+        [DataRow("uid.", "uid", "", DisplayName = "Parse from string with dot as last character")]
+        [DataRow(".", "", "", DisplayName = "Parse from string with dot as only character")]
+        public void ParseFromStringNoDotTest(string homeAccountId, string objectId, string tenantId)
         {
-            AccountId accountId = AccountId.ParseFromString("uid");
+            AccountId accountId = AccountId.ParseFromString(homeAccountId);
 
-            Assert.AreEqual("uid", accountId.ObjectId);
-            Assert.AreEqual(null, accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with 1 dot")]
-        public void ParseFromStringOneDotTest()
-        {
-            AccountId accountId = AccountId.ParseFromString("uid.utid");
-
-            Assert.AreEqual("uid", accountId.ObjectId);
-            Assert.AreEqual("utid", accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with 2 dots")]
-        public void ParseFromStringWith2DotsTest()
-        {
-            AccountId accountId = AccountId.ParseFromString("uid.1.utid");
-
-            Assert.AreEqual("uid.1", accountId.ObjectId);
-            Assert.AreEqual("utid", accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with more than 2 dots")]
-        public void ParseFromStringWithMoreThan2DotsTest()
-        {
-            AccountId accountId = AccountId.ParseFromString("uid.1.2.3.utid");
-
-            Assert.AreEqual("uid.1.2.3", accountId.ObjectId);
-            Assert.AreEqual("utid", accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with dot as first character")]
-        public void ParseFromStringWithDotAsFirstCharacterTest()
-        {
-            AccountId accountId = AccountId.ParseFromString(".utid");
-
-            Assert.AreEqual("", accountId.ObjectId);
-            Assert.AreEqual("utid", accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with dot as last character")]
-        public void ParseFromStringWithDotAsLastCharacterTest()
-        {
-            AccountId accountId = AccountId.ParseFromString("uid.");
-
-            Assert.AreEqual("uid", accountId.ObjectId);
-            Assert.AreEqual("", accountId.TenantId);
-        }
-
-        [TestMethod]
-        [Description("Test the parsing of HomeAccountId with dot as only character")]
-        public void ParseFromStringWithDotAsOnlyCharacterTest()
-        {
-            AccountId accountId = AccountId.ParseFromString(".");
-
-            Assert.AreEqual("", accountId.ObjectId);
-            Assert.AreEqual("", accountId.TenantId);
+            Assert.AreEqual(objectId, accountId.ObjectId);
+            Assert.AreEqual(tenantId, accountId.TenantId);
         }
     }
 }
