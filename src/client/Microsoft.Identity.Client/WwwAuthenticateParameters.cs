db15fbe941d0308e5dbca332f2372a201b7b9960
Jennyf19 <jeferrie@microsoft.com>
2021-07-05 08:34:26 -0700

    initial commit of www-authenticate headers

---
+++ src/client/Microsoft.Identity.Client/WwwAuthenticateParameters.cs
@@ -0,0 +1,307 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Identity.Client
+{
+    /// <summary>
+    /// Parameters returned by the WWW-Authenticate header. This allows for dynamic
+    /// scenarios such as claim challenge, CAE, CA auth context.
+    /// See https://aka.ms/msal-net/wwwAuthenticate.
+    /// </summary>
+    public class WwwAuthenticateParameters
+    {
+        /// <summary>
+        /// Resource for which to request scopes.
+        /// </summary>
+        public string Resource { get; set; }
+
+        /// <summary>
+        /// Scopes to request.
+        /// </summary>
+        public string[] Scopes
+        {
+            get
+            {
+                if (scopes != null)
+                {
+                    return scopes;
+                }
+                else if (!string.IsNullOrEmpty(Resource))
+                {
+                    return new[] { $"{Resource}/.default" };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                scopes = value;
+            }
+        }
+
+        private string[] scopes;
+
+        /// <summary>
+        /// Authority from which to request an access token.
+        /// </summary>
+        public string Authority { get; set; }
+
+        /// <summary>
+        /// Requested claims.
+        /// </summary>
+        public string Claims { get; set; }
+
+        /// <summary>
+        /// Error.
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Dictionary of raw parameters in the WWW-Authenticate header (extracted from the WWW-Authenticate header
+        /// string value, without any processing. This allows support for APIs which are not mappable easily to the standard
+        /// or framework specific (SAL, Microsoft.Identity.Web).
+        /// </summary>
+        public IDictionary<string, string> RawParameters { get; private set; }
+
+        /// <summary>
+        /// Create WWW-Authenticate parameters from the HttpResponseHeaders.
+        /// </summary>
+        /// <param name="httpResponseHeaders">HttpResponseHeaders.</param>
+        /// <param name="scheme">Authentication scheme. Default is "Bearer".</param>
+        /// <returns></returns>
+        public static WwwAuthenticateParameters ExtractWwwAuthenticateParametersFromHeaders(
+            HttpResponseHeaders httpResponseHeaders,
+            string scheme = "Bearer")
+        {
+            if (httpResponseHeaders.WwwAuthenticate.Any())
+            {
+                // TODO: could it be Pop too?
+                AuthenticationHeaderValue bearer = httpResponseHeaders.WwwAuthenticate.First(v => string.Equals(v.Scheme, scheme, StringComparison.OrdinalIgnoreCase));
+                string wwwAuthenticateValue = bearer.Parameter;
+                return ExtractParametersFromWwwAuthenticateHeaderValue(wwwAuthenticateValue);
+            }
+
+            return CreateWwwAuthenticateParameters(new Dictionary<string, string>());
+        }
+
+        /// <summary>
+        /// Extract parameters from the WWW-Authenticate string.
+        /// </summary>
+        /// <param name="wwwAuthenticateValue">String contained in a WWW-Authenticate header.</param>
+        /// <returns>Dictionary of parameters (name/value).</returns>
+        public static WwwAuthenticateParameters ExtractParametersFromWwwAuthenticateHeaderValue(string wwwAuthenticateValue)
+        {
+            if (string.IsNullOrWhiteSpace(wwwAuthenticateValue))
+            {
+                throw new ArgumentException($"'{nameof(wwwAuthenticateValue)}' cannot be null or whitespace.", nameof(wwwAuthenticateValue));
+            }
+
+            IDictionary<string, string> parameters = SplitWithQuotes(wwwAuthenticateValue, ',')
+                .Select(v => ExtractKeyValuePair(v.Trim()))
+                .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            return CreateWwwAuthenticateParameters(parameters);
+        }
+
+        /// <summary>
+        /// Create the authenticate parameters by attempting to call the resource unauthenticated, and analysis the response
+        /// </summary>
+        /// <param name="resourceUri">Uri of the resouce</param>
+        /// <returns>WwwAuthenticateParameters extracted from response to the un-authenticated call.</returns>
+        public static async Task<WwwAuthenticateParameters> CreateFromResourceResponseAsync(string resourceUri)
+        {
+            // call this endpoint and see what the header says and return that
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, resourceUri);
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage).ConfigureAwait(false);
+            var wwwAuthParam = ExtractWwwAuthenticateParametersFromHeaders(httpResponseMessage.Headers);
+            return wwwAuthParam;
+        }
+
+        /// <summary>
+        /// Extracts the claim challenge from HTTP header.
+        /// </summary>
+        /// <param name="httpResponseHeaders">The HTTP response headers.</param>
+        /// <param name="scheme">Authentication scheme. Default is Bearer.</param>
+        /// <returns></returns>
+        public static string ExtractClaimChallengeFromHttpHeader(
+            HttpResponseHeaders httpResponseHeaders,
+            string scheme = "Bearer")
+        {
+            WwwAuthenticateParameters parameters = ExtractWwwAuthenticateParametersFromHeaders(
+                httpResponseHeaders,
+                scheme);
+
+            try
+            {
+                // read the header and checks if it conatins error with insufficient_claims value.
+                if (null != parameters.Error && "insufficient_claims" == parameters.Error)
+                {
+                    if (null != parameters.Claims)
+                    {
+                        return parameters.Claims;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return null;
+        }
+
+        internal static WwwAuthenticateParameters CreateWwwAuthenticateParameters(IDictionary<string, string> values)
+        {
+            WwwAuthenticateParameters wwwAuthenticateParameters = new WwwAuthenticateParameters();
+            wwwAuthenticateParameters.RawParameters = values;
+            string value;
+
+            if (values.TryGetValue("authorization_uri", out value))
+            {
+                wwwAuthenticateParameters.Authority = value.Replace("/oauth2/authorize", string.Empty);
+            }
+
+            if (string.IsNullOrEmpty(wwwAuthenticateParameters.Authority))
+            {
+                if (values.TryGetValue("authorization", out value))
+                {
+                    wwwAuthenticateParameters.Authority = value.Replace("/oauth2/authorize", string.Empty);
+                }
+            }
+
+            if (string.IsNullOrEmpty(wwwAuthenticateParameters.Authority))
+            {
+                if (values.TryGetValue("authority", out value))
+                {
+                    wwwAuthenticateParameters.Authority = value.TrimEnd('/');
+                }
+            }
+
+            if (values.TryGetValue("resource_id", out value))
+            {
+                wwwAuthenticateParameters.Resource = value;
+            }
+
+            if (string.IsNullOrEmpty(wwwAuthenticateParameters.Resource))
+            {
+                if (values.TryGetValue("resource", out value))
+                {
+                    wwwAuthenticateParameters.Resource = value;
+                }
+            }
+
+            if (string.IsNullOrEmpty(wwwAuthenticateParameters.Resource))
+            {
+                if (values.TryGetValue("client_id", out value))
+                {
+                    wwwAuthenticateParameters.Resource = value;
+                }
+            }
+
+            if (values.TryGetValue("claims", out value))
+            {
+                wwwAuthenticateParameters.Claims = GetJsonFragment(value);
+            }
+
+            if (values.TryGetValue("error", out value))
+            {
+                wwwAuthenticateParameters.Error = value;
+            }
+
+            return wwwAuthenticateParameters;
+        }
+
+        internal static List<string> SplitWithQuotes(string input, char delimiter)
+        {
+            List<string> items = new List<string>();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return items;
+            }
+
+            int startIndex = 0;
+            bool insideString = false;
+            string item;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == delimiter && !insideString)
+                {
+                    item = input.Substring(startIndex, i - startIndex);
+                    if (!string.IsNullOrWhiteSpace(item.Trim()))
+                    {
+                        items.Add(item);
+                    }
+
+                    startIndex = i + 1;
+                }
+                else if (input[i] == '"')
+                {
+                    insideString = !insideString;
+                }
+            }
+
+            item = input.Substring(startIndex);
+            if (!string.IsNullOrWhiteSpace(item.Trim()))
+            {
+                items.Add(item);
+            }
+
+            return items;
+        }
+
+        /// <summary>
+        /// Extracts a key value pair from an expression of the form a=b
+        /// </summary>
+        /// <param name="assignment">assignment</param>
+        /// <returns>Key Value pair</returns>
+        private static KeyValuePair<string, string> ExtractKeyValuePair(string assignment)
+        {
+            string[] segments = SplitWithQuotes(assignment, '=')
+                .Select(s => s.Trim().Trim('"'))
+                .ToArray();
+            if (segments.Length != 2)
+            {
+                throw new ArgumentException(nameof(assignment), $"{assignment} isn't of the form a=b");
+            }
+
+            return new KeyValuePair<string, string>(segments[0].ToLowerInvariant(), segments[1]);
+        }
+
+        /// <summary>
+        /// Checks if input is a base-64 encoded string.
+        /// If it is one, decodes it to get a json fragment.
+        /// </summary>
+        /// <param name="inputString">Input string</param>
+        /// <returns>a json fragment (original input string or decoded from base64 encoded).</returns>
+        private static string GetJsonFragment(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString) || inputString.Length % 4 != 0 || inputString.Any(c => char.IsWhiteSpace(c)))
+            {
+                return inputString;
+            }
+
+            try
+            {
+                var decodedBase64Bytes = Convert.FromBase64String(inputString);
+                var decoded = Encoding.UTF8.GetString(decodedBase64Bytes);
+                return decoded;
+            }
+            catch (Exception)
+            {
+                return inputString;
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Identity.Test.Unit/WwwAuthenticateParametersTests.cs
@@ -0,0 +1,158 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Identity.Client;
+
+namespace Microsoft.Identity.Test.Unit
+{
+    [TestClass]
+    public class WwwAuthenticateParametersTests
+    {
+        const string ClientIdKey = "client_id";
+        const string ResourceIdKey = "resource_id";
+        const string ResourceKey = "resource";
+        const string GraphGuid = "00000003-0000-0000-c000-000000000000";
+        const string AuthorizationUriKey = "authorization_uri";
+        const string AuthorizationKey = "authorization";
+        const string AuthorityKey = "authority";
+        const string AuthorizationValue = "https://login.microsoftonline.com/common/oauth2/authorize";
+        const string Realm = "realm";
+        const string EncodedClaims = "eyJpZF90b2tlbiI6eyJhdXRoX3RpbWUiOnsiZXNzZW50aWFsIjp0cnVlfSwiYWNyIjp7InZhbHVlcyI6WyJ1cm46bWFjZTppbmNvbW1vbjppYXA6c2lsdmVyIl19fX0=";
+        const string DecodedClaims = "{\"id_token\":{\"auth_time\":{\"essential\":true},\"acr\":{\"values\":[\"urn:mace:incommon:iap:silver\"]}}}";
+        const string SomeClaims = "some_claims";
+
+        [TestMethod]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authorization_uri=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authority=\"https://login.microsoftonline.com/common/\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authority=\"https://login.microsoftonline.com/common\"")]
+        [DataRow("resource_id=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("resource=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        public void CreateWwwAuthenticateResponse(string resource, string authorizationUri)
+        {
+            HttpResponseMessage httpResponse = new HttpResponseMessage((HttpStatusCode)401)
+            {
+            };
+            httpResponse.Headers.Add("WWW-Authenticate", $"Bearer realm=\"\", {resource}, {authorizationUri}");
+
+            var authParams = WwwAuthenticateParameters.ExtractWwwAuthenticateParametersFromHeaders(httpResponse.Headers);
+
+            Assert.AreEqual(GraphGuid, authParams.Resource);
+            Assert.AreEqual(TestConstants.AuthorityCommonTenant.TrimEnd('/'), authParams.Authority);
+            Assert.AreEqual($"{GraphGuid}/.default", authParams.Scopes.FirstOrDefault());
+            Assert.AreEqual(3, authParams.RawParameters.Count);
+            Assert.IsNull(authParams.Claims);
+            Assert.IsNull(authParams.Error);
+        }
+
+        [TestMethod]
+        [DataRow(ClientIdKey, AuthorizationUriKey)]
+        [DataRow(ClientIdKey, AuthorizationKey)]
+        [DataRow(ClientIdKey, AuthorityKey)]
+        [DataRow(ResourceIdKey, AuthorizationUriKey)]
+        [DataRow(ResourceIdKey, AuthorizationKey)]
+        [DataRow(ResourceIdKey, AuthorityKey)]
+        [DataRow(ResourceKey, AuthorizationUriKey)]
+        [DataRow(ResourceKey, AuthorizationKey)]
+        [DataRow(ResourceKey, AuthorityKey)]
+        public void CreateRawParameters(string resourceHeaderKey, string authorizationUriHeaderKey)
+        {
+            HttpResponseMessage httpResponse = CreateHttpResponseHeaders(resourceHeaderKey, authorizationUriHeaderKey);
+
+            var authParams = WwwAuthenticateParameters.ExtractWwwAuthenticateParametersFromHeaders(httpResponse.Headers);
+
+            Assert.IsTrue(authParams.RawParameters.ContainsKey(resourceHeaderKey));
+            Assert.IsTrue(authParams.RawParameters.ContainsKey(authorizationUriHeaderKey));
+            Assert.IsTrue(authParams.RawParameters.ContainsKey(Realm));
+        }
+
+        [TestMethod]
+        [DataRow(SomeClaims)]
+        [DataRow(EncodedClaims)]
+        public void CreateRawParameters_ClaimsAndErrorReturned(string claims)
+        {
+            HttpResponseMessage httpResponse = CreateClaimsHttpResponse(claims);
+            var authParams = WwwAuthenticateParameters.ExtractWwwAuthenticateParametersFromHeaders(httpResponse.Headers);
+
+            string claimsKey = "claims";
+            string errorKey = "error";
+            Assert.IsTrue(authParams.RawParameters.TryGetValue(AuthorizationUriKey, out string authorizationUri));
+            Assert.AreEqual(AuthorizationValue, authorizationUri);
+            Assert.IsTrue(authParams.RawParameters.ContainsKey(Realm));
+            Assert.IsTrue(authParams.RawParameters.TryGetValue(Realm, out string realmValue));
+            Assert.AreEqual(string.Empty, realmValue);
+            Assert.IsTrue(authParams.RawParameters.TryGetValue(claimsKey, out string claimsValue));
+            Assert.AreEqual(claims, claimsValue);
+            Assert.IsTrue(authParams.RawParameters.TryGetValue(errorKey, out string errorValue));
+            Assert.AreEqual("insufficient_claims", errorValue);
+        }
+
+        [TestMethod]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authorization_uri=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authority=\"https://login.microsoftonline.com/common/\"")]
+        [DataRow("client_id=00000003-0000-0000-c000-000000000000", "authority=\"https://login.microsoftonline.com/common\"")]
+        [DataRow("resource_id=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        [DataRow("resource=00000003-0000-0000-c000-000000000000", "authorization=\"https://login.microsoftonline.com/common/oauth2/authorize\"")]
+        public void CreateWwwAuthenticateParamsFromWwwAuthenticateHeader(string clientId, string authorizationUri)
+        {
+            HttpResponseMessage httpResponse = new HttpResponseMessage((HttpStatusCode)401)
+            {
+            };
+            httpResponse.Headers.Add("WWW-Authenticate", $"Bearer realm=\"\", {clientId}, {authorizationUri}");
+
+            var wwwAuthenticateResponse = httpResponse.Headers.WwwAuthenticate.FirstOrDefault().Parameter;
+
+            var authParams = WwwAuthenticateParameters.ExtractParametersFromWwwAuthenticateHeaderValue(wwwAuthenticateResponse);
+            
+            Assert.AreEqual(GraphGuid, authParams.Resource);
+            Assert.AreEqual(TestConstants.AuthorityCommonTenant.TrimEnd('/'), authParams.Authority);
+            Assert.AreEqual($"{GraphGuid}/.default", authParams.Scopes.FirstOrDefault());
+            Assert.AreEqual(3, authParams.RawParameters.Count);
+            Assert.IsNull(authParams.Claims);
+            Assert.IsNull(authParams.Error);
+        }
+
+        [TestMethod]
+        public void ExtractClaimChallengeFromHeader()
+        {
+            HttpResponseMessage httpResponse = CreateClaimsHttpResponse(SomeClaims);
+
+            string extractedClaims = WwwAuthenticateParameters.ExtractClaimChallengeFromHttpHeader(httpResponse.Headers);
+
+            Assert.AreEqual(SomeClaims, extractedClaims);
+        }
+
+        [TestMethod]
+        public void ExtractEncodedClaimChallengeFromHeader()
+        {
+            HttpResponseMessage httpResponse = CreateClaimsHttpResponse(EncodedClaims);
+
+            string extractedClaims = WwwAuthenticateParameters.ExtractClaimChallengeFromHttpHeader(httpResponse.Headers);
+
+            Assert.AreEqual(DecodedClaims, extractedClaims);
+        }
+
+        private static HttpResponseMessage CreateClaimsHttpResponse(string claims)
+        {
+            HttpResponseMessage httpResponse = new HttpResponseMessage((HttpStatusCode)401)
+            {
+            };
+            httpResponse.Headers.Add("WWW-Authenticate", $"Bearer realm=\"\", client_id=\"00000003-0000-0000-c000-000000000000\", authorization_uri=\"https://login.microsoftonline.com/common/oauth2/authorize\", error=\"insufficient_claims\", claims=\"{claims}\"");
+            return httpResponse;
+        }
+
+        private static HttpResponseMessage CreateHttpResponseHeaders(string resourceHeaderKey, string authorizationUriHeaderKey)
+        {
+            HttpResponseMessage httpResponse = new HttpResponseMessage((HttpStatusCode)401)
+            {
+            };
+            httpResponse.Headers.Add("WWW-Authenticate", $"Bearer realm=\"\", {resourceHeaderKey}=\"{GraphGuid}\", {authorizationUriHeaderKey}=\"{AuthorizationValue}\"");
+            return httpResponse;
+        }
+    }
+}
