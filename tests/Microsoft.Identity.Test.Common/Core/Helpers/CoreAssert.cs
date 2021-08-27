73227365eab086f8e5051e9952bc5c67569d804d
trwalke <trwalke@microsoft.com>
2021-08-27 14:11:19 -0700

    Test Update

--- tests/Microsoft.Identity.Test.Common/Core/Helpers/CoreAssert.cs
+++ tests/Microsoft.Identity.Test.Common/Core/Helpers/CoreAssert.cs
@@ -39,6 +39,13 @@ namespace Microsoft.Identity.Test.Common.Core.Helpers
                 $"The dates are off by {t.TotalMilliseconds}ms, which is more than the expected {delta.TotalMilliseconds}ms");
         }
 
+        public static void IsInRange(DateTimeOffset expected, DateTimeOffset actual, TimeSpan range)
+        {
+            TimeSpan t = expected - actual;
+            Assert.IsTrue(t >= -range && t <= range,
+                $"The dates are off by {t.TotalMilliseconds}ms, which is more than the expected {range.TotalMilliseconds}ms");
+        }
+
         public static void AssertDictionariesAreEqual<TKey, TValue>(
           IDictionary<TKey, TValue> dict1,
           IDictionary<TKey, TValue> dict2,
