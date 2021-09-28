7081af95d7a97e43a917960010c9a0376c79455a
Alexander Batishchev <abatishchev@gmail.com>
2021-09-28 10:09:10 -0700

    Update WwwAuthenticateParameters.cs

--- src/client/Microsoft.Identity.Client/WwwAuthenticateParameters.cs
+++ src/client/Microsoft.Identity.Client/WwwAuthenticateParameters.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Identity.Client
