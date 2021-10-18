a54a2e1dc16baf885442cf1153c37b0550b442fa
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-10-18 16:12:17 -0700

    Fix iOS devapp.

--- tests/devapps/XForms/XForms.iOS/Main.cs
+++ tests/devapps/XForms/XForms.iOS/Main.cs
@@ -17,7 +17,7 @@ namespace XForms.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
