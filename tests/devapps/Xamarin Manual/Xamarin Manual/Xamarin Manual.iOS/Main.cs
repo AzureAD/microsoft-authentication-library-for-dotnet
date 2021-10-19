67d59b1f185579e0e04eaac4983773157d3a9f7e
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-10-18 23:26:20 -0700

    Fix more dev apps.

--- tests/devapps/Xamarin Manual/Xamarin Manual/Xamarin Manual.iOS/Main.cs
+++ tests/devapps/Xamarin Manual/Xamarin Manual/Xamarin Manual.iOS/Main.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
+﻿using UIKit;
 
 namespace Xamarin_Manual.iOS
 {
@@ -14,7 +9,7 @@ namespace Xamarin_Manual.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
 
         }
     }
