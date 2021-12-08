5db3247fd6dd234a2ce55a2ffef19fbed9eb9ecf
pmaytak <34331512+pmaytak@users.noreply.github.com>
2021-12-07 22:20:02 -0800

    Update dev apps to use app config.

--- tests/devapps/RegionalTestApp/Program.cs
+++ tests/devapps/RegionalTestApp/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Identity.Client;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Security.Cryptography.X509Certificates;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Test.Integration.NetFx.Infrastructure;
 
 /**
  * Note: Restart the application between the test cases to clear the statics.
@@ -82,10 +81,13 @@ namespace TestApp
     {
         private const string s_environmentVarName = "REGION_NAME";
         private const string s_region = "centralus";
+        private static IConfidentialAppSettings s_appSettings;
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("AsyncUsage.CSharp.Naming", "UseAsyncSuffix:Use Async suffix", Justification = "Main method")]
         static async Task Main(string[] args)
         {
+            s_appSettings = ConfidentialAppSettings.GetSettings(Cloud.Public);
+
             while (true)
             {
                 Console.Clear();
@@ -105,44 +107,42 @@ namespace TestApp
 
                 try
                 {
-                    X509Certificate2 certificate = ReadCertificate();
-
                     switch (menuSelection)
                     {
                         case 1: // Regional with a valid user-specified region
-                            await AcquireTokenAsync(certificate, region: "westus").ConfigureAwait(false);
+                            await AcquireTokenAsync(region: "westus").ConfigureAwait(false);
                             break;
 
                         case 2: // Regional auto-detect with environment variable
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
                             break;
 
                         case 3: // Regional auto-detect with IMDS call (without environment variable, test in Azure VM)
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery, setEnvVariable: false).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery, setEnvVariable: false).ConfigureAwait(false);
                             break;
 
                         case 4: // Acquire token with global, then regional
-                            await AcquireTokenAsync(certificate, region: string.Empty).ConfigureAwait(false);
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: string.Empty).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
                             break;
 
                         case 5: // Acquire token with regional, then global
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
-                            await AcquireTokenAsync(certificate, region: string.Empty).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: string.Empty).ConfigureAwait(false);
                             break;
 
                         case 6: // Acquire token twice in a row with regional (tests for double region appending regression)
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
-                            await AcquireTokenAsync(certificate, region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: ConfidentialClientApplication.AttemptRegionDiscovery).ConfigureAwait(false);
                             break;
 
                         case 7: // Acquire token twice in a row with global
-                            await AcquireTokenAsync(certificate, region: string.Empty).ConfigureAwait(false);
-                            await AcquireTokenAsync(certificate, region: string.Empty).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: string.Empty).ConfigureAwait(false);
+                            await AcquireTokenAsync(region: string.Empty).ConfigureAwait(false);
                             break;
 
                         case 8: // Regional with an invalid user-specified region
-                            await AcquireTokenAsync(certificate, region: "invalidRegion").ConfigureAwait(false);
+                            await AcquireTokenAsync(region: "invalidRegion").ConfigureAwait(false);
                             break;
 
                         case 0:
@@ -163,24 +163,22 @@ namespace TestApp
             }
         }
 
-        private static async Task AcquireTokenAsync(X509Certificate2 certificate, string region, bool setEnvVariable = true)
+        private static async Task AcquireTokenAsync(string region, bool setEnvVariable = true)
         {
-            string clientId = "16dab2ba-145d-4b1b-8569-bf4b9aed4dc8";
-
             if (!string.IsNullOrEmpty(region) && setEnvVariable)
             {
                 Environment.SetEnvironmentVariable(s_environmentVarName, s_region);
             }
 
-            string[] scopes = new string[] { $"{clientId}/.default", };
+            string[] scopes = new string[] { $"{s_appSettings.ClientId}/.default", };
             Dictionary<string, string> dict = new Dictionary<string, string>
             {
                 ["allowestsrnonmsi"] = "true"
             };
 
-            var builder = ConfidentialClientApplicationBuilder.Create(clientId)
-                .WithAuthority("https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47", false)
-                .WithCertificate(certificate)
+            var builder = ConfidentialClientApplicationBuilder.Create(s_appSettings.ClientId)
+                .WithAuthority(s_appSettings.Authority, false)
+                .WithCertificate(s_appSettings.GetCertificate())
                 .WithLogging(Log, LogLevel.Verbose, true);
 
             // Regional if region specified, global otherwise
@@ -214,29 +212,6 @@ namespace TestApp
             }
             Console.WriteLine($"{level} {message}");
             Console.ResetColor();
-        }
-
-        private static X509Certificate2 ReadCertificate()
-        {
-            string thumbprint = "378938210C976692D7F523B8C4FFBB645D17CE92";
-
-            X509Certificate2 cert = null;
-
-            using (X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
-            {
-                store.Open(OpenFlags.ReadOnly);
-                X509Certificate2Collection certCollection = store.Certificates;
-
-                // Find unexpired certificates.
-                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
-
-                // From the collection of unexpired certificates, find the ones with the correct name.
-                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindByThumbprint, thumbprint, false);
-
-                // Return the first certificate in the collection, has the right name and is current.
-                cert = signingCert.OfType<X509Certificate2>().OrderByDescending(c => c.NotBefore).FirstOrDefault();
-            }
-            return cert;
         }
     }
 }
