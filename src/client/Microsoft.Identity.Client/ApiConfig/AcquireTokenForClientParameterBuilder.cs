a5c5115c38a0976027d1031a952a5fc8e92fb98d
pmaytak <34331512+pmaytak@users.noreply.github.com>
2022-01-04 23:20:55 -0800

    Update comments.

--- src/client/Microsoft.Identity.Client/ApiConfig/AcquireTokenForClientParameterBuilder.cs
+++ src/client/Microsoft.Identity.Client/ApiConfig/AcquireTokenForClientParameterBuilder.cs
@@ -52,9 +52,10 @@ namespace Microsoft.Identity.Client
         }
 
         /// <summary>
-        /// Specifies if the x5c claim (public key of the certificate) should be sent to the STS.
+        /// Applicable to first-party applications only, this method also allows to specify 
+        /// if the <see href="https://datatracker.ietf.org/doc/html/rfc7517#section-4.7">x5c claim</see> should be sent to Azure AD.
         /// Sending the x5c enables application developers to achieve easy certificate roll-over in Azure AD:
-        /// this method will send the public certificate to Azure AD along with the token request,
+        /// this method will send the certificate chain to Azure AD along with the token request,
         /// so that Azure AD can use it to validate the subject name based on a trusted issuer policy.
         /// This saves the application admin from the need to explicitly manage the certificate rollover
         /// (either via portal or PowerShell/CLI operation). For details see https://aka.ms/msal-net-sni
