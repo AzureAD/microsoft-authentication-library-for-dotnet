d51b5b6a78dc5d37ae32f4f95431c036e3eccd27
trwalke <trwalke@microsoft.com>
2022-01-24 10:30:36 -0800

    Adding Android install

---
+++ build/InstallAndroid.ps1
@@ -0,0 +1,28 @@
+$url = "https://dl.google.com/android/repository/commandlinetools-win-7583922_latest.zip"
+$fileName = "AndroidTools.zip"
+$source = "C:\Downloads\$fileName"
+$destination = '$(Build.SourcesDirectory)\AndroidSdkTools'
+$androidSdk = "C:\Program Files (x86)\Android\android-sdk\"
+
+$ErrorActionPreference = "Stop"
+
+if (Test-Path "$PSScriptRoot\win-installer-helper.psm1") 
+{
+    Import-Module "$PSScriptRoot\win-installer-helper.psm1" -DisableNameChecking
+} elseif (Test-Path "$PSScriptRoot\..\..\Helpers\win-installer-helper.psm1") 
+{
+    Import-Module "$PSScriptRoot\..\..\Helpers\win-installer-helper.psm1" -DisableNameChecking
+}
+
+try 
+{
+    Get-File -Url $url -FileName $fileName
+    Expand-ArchiveWith7Zip -Source $source -Destination $destination
+} 
+finally 
+{
+    Stop-Setup
+}
+
+echo "installing android"
+$destination\cmdline-tools\bin\.\sdkmanager "platforms;android-29" --sdk_root=$androidSdk
