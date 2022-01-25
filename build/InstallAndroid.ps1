712f0336b5f098f6bb36fd81f9e18068b9bec57b
Travis Walker <travis.walker@microsoft.com>
2022-01-24 17:28:23 -0800

    Test22

--- build/InstallAndroid.ps1
+++ build/InstallAndroid.ps1
@@ -19,10 +19,18 @@ mkdir -Path C:\Downloads\ -Force
 Get-File -Url $url -FileName $fileName
 #Expand-ArchiveWith7Zip -Source $source -Destination $destination
 
+Start-Setup
+$PathNodes=@()
+try 
+{
+    echo "Expanding"
+    Expand-Archive -LiteralPath "$source" -DestinationPath "C:\Downloads\AndroidSdkTools" -Force
 
-echo "Expanding"
-Expand-Archive -LiteralPath "$source" -DestinationPath "C:\Downloads\AndroidSdkTools" -Force
-
-echo "installing android"
-echo y y y y y y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager --licenses --sdk_root="C:\Program Files (x86)\Android\android-sdk"
-echo y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager "platforms;android-29" --sdk_root="C:\Program Files (x86)\Android\android-sdk"
+    echo "installing android"
+    echo y y y y y y y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager --licenses --sdk_root="C:\Program Files (x86)\Android\android-sdk"
+    echo y y y y y y y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager "platforms;android-29" --sdk_root="C:\Program Files (x86)\Android\android-sdk"
+} 
+finally 
+{
+    Stop-Setup
+}
