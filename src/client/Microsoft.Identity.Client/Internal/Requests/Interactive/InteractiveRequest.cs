b912bf18ff75da4e902f4f1fa5b6caf0667acc37
neha-bhargava <nebharg@microsoft.com>
2022-02-03 15:26:29 -0800

    Update authority for interactive requests

--- src/client/Microsoft.Identity.Client/Internal/Requests/Interactive/InteractiveRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/Interactive/InteractiveRequest.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client.ApiConfig.Parameters;
 using Microsoft.Identity.Client.Core;
+using Microsoft.Identity.Client.Instance;
 using Microsoft.Identity.Client.Internal.Broker;
 using Microsoft.Identity.Client.OAuth2;
 using Microsoft.Identity.Client.UI;
@@ -128,6 +129,16 @@ namespace Microsoft.Identity.Client.Internal.Requests
             if (BrokerInteractiveRequestComponent.IsBrokerRequiredAuthCode(authCode, out string brokerInstallUri))
             {
                 return await RunBrokerWithInstallUriAsync(brokerInstallUri, cancellationToken).ConfigureAwait(false);
+            }
+
+            if (!_requestParams.AppConfig.IsBrokerEnabled && !string.IsNullOrEmpty(authResult.CloudInstanceHost))
+            {
+                _logger.Info("Updating the authority to the cloud specific authority.");
+                _requestParams.AuthorityManager = new AuthorityManager(
+                    _requestParams.RequestContext,
+                    Authority.CreateAuthorityWithEnvironment(_requestParams.Authority.AuthorityInfo, authResult.CloudInstanceHost));
+                
+                await ResolveAuthorityAsync().ConfigureAwait(false);
             }
 
             _logger.Info("Exchanging the auth code for tokens. ");
