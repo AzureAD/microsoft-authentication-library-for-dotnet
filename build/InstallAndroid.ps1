ab0820527b48b78ed0886b84a313dc6f00010bec
trwalke <trwalke@microsoft.com>
2022-02-16 09:57:08 -0800

    Refactoring

--- build/InstallAndroid.ps1
+++ build/InstallAndroid.ps1
@@ -1,8 +1,9 @@
-$url = "https://dl.google.com/android/repository/commandlinetools-win-7583922_latest.zip"
+$url = "https://dl.google.com/android/repository/commandlinetools-win-7583922_latest.zip" #Android SDK Tools from https://developer.android.com/studio#downloads
 $fileName = "AndroidTools.zip"
 $source = "C:\Downloads\$fileName"
 $destination = "C:\Downloads\AndroidSdkTools"
-$androidSdk = "C:\Program Files (x86)\Android\android-sdk\"
+$androidSdk = "C:\%PROGRAMFILES(x86)%\Android\android-sdk"
+$androidSdkVersion = "platforms;android-29"
 
 #$ErrorActionPreference = "Stop"
 
@@ -15,20 +16,20 @@ if (Test-Path "$PSScriptRoot\win-installer-helper.psm1")
 }
 
 mkdir -Path C:\Downloads\ -Force
-mkdir -Path "C:\Program Files (x86)\Android\android-sdk\licenses" -Force
+mkdir -Path "$androidSdk\licenses" -Force
 
 Get-File -Url $url -FileName $fileName
 
 echo "Expanding"
 Expand-Archive -LiteralPath "$source" -DestinationPath "C:\Downloads\AndroidSdkTools" -Force
 
-dir "C:\Program Files (x86)\Android\android-sdk\licenses"
+dir "$androidSdk\licenses"
 
 echo "Installing licenses" #This is a workaround as it is not possible to accept licences during the build run. If there is an issue, simply accept the licenses locally and reupload them.
-Copy-Item -Path microsoft-authentication-library-for-dotnet\build\AndroidSdkLicenses\* -Destination "C:\Program Files (x86)\Android\android-sdk\licenses" -Force
+Copy-Item -Path microsoft-authentication-library-for-dotnet\build\AndroidSdkLicenses\* -Destination "$androidSdk\licenses" -Force
 
-dir "C:\Program Files (x86)\Android\android-sdk\licenses"
+dir "$androidSdk\licenses"
 
 echo "installing android"
-C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager --licenses --sdk_root="C:\Program Files (x86)\Android\android-sdk"
-echo y y y y y y y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager "platforms;android-29" --sdk_root="C:\Program Files (x86)\Android\android-sdk"
+C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager --licenses --sdk_root=$androidSdk
+echo y y y y y y y |C:\Downloads\AndroidSdkTools\cmdline-tools\bin\.\sdkmanager $androidSdkVersion --sdk_root=$androidSdk
