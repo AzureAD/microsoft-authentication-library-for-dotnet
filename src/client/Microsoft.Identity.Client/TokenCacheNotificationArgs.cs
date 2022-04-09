edbb4d2608f4a86e4d3bf1312b044812b0da99fd
pmaytak <34331512+pmaytak@users.noreply.github.com>
2022-04-08 19:56:36 -0700

    Update comment on TokenCacheNotificationArgs.SuggestedCacheKey.

--- src/client/Microsoft.Identity.Client/TokenCacheNotificationArgs.cs
+++ src/client/Microsoft.Identity.Client/TokenCacheNotificationArgs.cs
@@ -37,19 +37,19 @@ namespace Microsoft.Identity.Client
                    suggestedCacheExpiry,
                    cancellationToken,
                    default)
-            {
-            }
+        {
+        }
 
         /// <summary>
         /// This constructor is for test purposes only. It allows apps to unit test their MSAL token cache implementation code.
         /// </summary>
         public TokenCacheNotificationArgs(
-            ITokenCacheSerializer tokenCache, 
-            string clientId, 
-            IAccount account, 
-            bool hasStateChanged, 
-            bool isApplicationCache, 
-            string suggestedCacheKey, 
+            ITokenCacheSerializer tokenCache,
+            string clientId,
+            IAccount account,
+            bool hasStateChanged,
+            bool isApplicationCache,
+            string suggestedCacheKey,
             bool hasTokens,
             DateTimeOffset? suggestedCacheExpiry,
             CancellationToken cancellationToken,
@@ -66,7 +66,7 @@ namespace Microsoft.Identity.Client
             CorrelationId = correlationId;
             SuggestedCacheExpiry = suggestedCacheExpiry;
         }
-        
+
         /// <summary>
         /// Gets the <see cref="ITokenCacheSerializer"/> involved in the transaction
         /// </summary>
@@ -100,16 +100,16 @@ namespace Microsoft.Identity.Client
 
         /// <summary>
         /// A suggested token cache key, which can be used with general purpose storage mechanisms that allow 
-        /// storing key-value pairs and key based retrieval. Useful in applications that store 1 token cache per user, 
+        /// storing key-value pairs and key based retrieval. Useful in applications that store one token cache per user, 
         /// the recommended pattern for web apps.
         /// 
         /// The value is: 
         /// 
         /// <list type="bullet">
-        /// <item><description>the homeAccountId for AcquireTokenSilent, GetAccount(homeAccountId), RemoveAccount and when writing tokens on confidential client calls</description></item>
-        /// <item><description>clientID + "_AppTokenCache" for AcquireTokenForClient</description></item>
-        /// <item><description>clientID_tenantID + "_AppTokenCache" for AcquireTokenForClient when tenant specific authority</description></item>
-        /// <item><description>the hash of the original token for AcquireTokenOnBehalfOf</description></item>
+        /// <item><description><c>homeAccountId</c> for <c>AcquireTokenSilent</c>, <c>GetAccount(homeAccountId)</c>, <c>RemoveAccount</c> and when writing tokens on confidential client calls</description></item>
+        /// <item><description><c>"{clientId}__AppTokenCache"</c> for <c>AcquireTokenForClient</c></description></item>
+        /// <item><description><c>"{clientId}_{tenantId}_AppTokenCache"</c> for <c>AcquireTokenForClient</c> when using a tenant specific authority</description></item>
+        /// <item><description>the hash of the original token for <c>AcquireTokenOnBehalfOf</c></description></item>
         /// </list>
         /// </summary>
         public string SuggestedCacheKey { get; }
