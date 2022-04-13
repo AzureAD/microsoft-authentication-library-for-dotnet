379c9d61718e6bf838e9d59795a8c553c1be7a74
Gladwin Johnson <gljohns@microsoft.com>
2022-04-13 08:58:25 -0700

    suppressing some warnings

--- src/client/Microsoft.Identity.Client.Brokers/BrokerExtension.cs
+++ src/client/Microsoft.Identity.Client.Brokers/BrokerExtension.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,9 @@ using Microsoft.Identity.Client.PlatformsCommon.Shared;
 
 namespace Microsoft.Identity.Client.Broker
 {
+    /// <summary>
+    /// MSAL Broker Extension for WAM support
+    /// </summary>
     public static class BrokerExtension
     {
         /// <summary>
