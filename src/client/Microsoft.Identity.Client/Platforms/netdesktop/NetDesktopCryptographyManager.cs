9ab668d01bb5d4c740167c8886508a1e5fee11b3
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2022-05-27 14:00:00 -0700

    Updated for net45

--- src/client/Microsoft.Identity.Client/Platforms/netdesktop/NetDesktopCryptographyManager.cs
+++ src/client/Microsoft.Identity.Client/Platforms/netdesktop/NetDesktopCryptographyManager.cs
@@ -19,12 +19,50 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Microsoft.Identity.Client.Platforms.net45
 {
+#if NET45
+    internal class NetDesktopCryptographyManager : ICryptographyManager
+#else
     internal class NetDesktopCryptographyManager : CommonCryptographyManager
+#endif
     {
         private static readonly ConcurrentDictionary<string, RSACryptoServiceProvider> s_certificateToRsaCspMap = new ConcurrentDictionary<string, RSACryptoServiceProvider>();
         private static readonly int s_maximumMapSize = 1000;
-      
+#if NET45
+        public string CreateBase64UrlEncodedSha256Hash(string input)
+        {
+            return string.IsNullOrEmpty(input) ? null : Base64UrlHelpers.Encode(CreateSha256HashBytes(input));
+        }
+
+        public string GenerateCodeVerifier()
+        {
+            byte[] buffer = new byte[Constants.CodeVerifierByteSize];
+            using (var randomSource = RandomNumberGenerator.Create())
+            {
+                randomSource.GetBytes(buffer);
+            }
+
+            return Base64UrlHelpers.Encode(buffer);
+        }
+
+        public string CreateSha256Hash(string input)
+        {
+            return string.IsNullOrEmpty(input) ? null : Convert.ToBase64String(CreateSha256HashBytes(input));
+        }
+
+        public byte[] CreateSha256HashBytes(string input)
+        {
+            using (var sha = SHA256.Create())
+            {
+                return sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+            }
+        }
+#endif
+
+#if NET45
+        public byte[] SignWithCertificate(string message, X509Certificate2 certificate)
+#else
         public override byte[] SignWithCertificate(string message, X509Certificate2 certificate)
+#endif
         {
             if (certificate.PublicKey.Key.KeySize < CertificateClientCredential.MinKeySizeInBits)
             {
