262af0b42739a2790676960a60aeeaed6fdcbc93
Gladwin Johnson <gljohns@microsoft.com>
2022-06-02 12:16:03 -0700

    updates per comments

--- src/client/Microsoft.Identity.Client.Broker/WamAdapters.cs
+++ src/client/Microsoft.Identity.Client.Broker/WamAdapters.cs
@@ -213,7 +213,7 @@ namespace Microsoft.Identity.Client.Broker
                     IdToken = authResult.RawIdToken,
                     CorrelationId = correlationId,
                     Scope = authResult.GrantedScopes,
-                    ExpiresIn = (long)(authResult.ExpiresOn - DateTime.UtcNow).TotalSeconds,
+                    ExpiresIn = (long)(DateTime.SpecifyKind(authResult.ExpiresOn, DateTimeKind.Utc) - DateTimeOffset.UtcNow).TotalSeconds,
                     ClientInfo = authResult.Account.ClientInfo.ToString(),
                     TokenType = authResult.IsPopAuthorization ? Constants.PoPAuthHeaderPrefix : BrokerResponseConst.Bearer,
                     WamAccountId = authResult.Account.Id,
