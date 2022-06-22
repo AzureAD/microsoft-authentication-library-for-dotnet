432e747e923589ce3665469e18a1418348c239ea
neha-bhargava <nebharg@microsoft.com>
2022-06-22 10:21:44 -0700

    Add pop for username password flow

--- tests/devapps/WAM/NetCoreWinFormsWam/Form1.cs
+++ tests/devapps/WAM/NetCoreWinFormsWam/Form1.cs
@@ -545,6 +545,11 @@ namespace NetDesktopWinForms
             var guid = Guid.NewGuid();
             var builder = pca.AcquireTokenByUsernamePassword(scopes, username, new NetworkCredential("", password).SecurePassword);
 
+            if (cbxPOP.Checked)
+            {
+                builder = builder.WithProofOfPossession(_popNonce, System.Net.Http.HttpMethod.Get, new Uri(pca.Authority));
+            }
+
             if (cbxBackgroundThread.Checked)
             {
                 await Task.Delay(500).ConfigureAwait(false);
