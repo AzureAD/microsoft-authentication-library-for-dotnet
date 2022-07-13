9cc279f600bf13b44043b5ec0079474610a4c47e
Bogdan Gavril <bogavril@microsoft.com>
2022-07-13 13:03:23 +0300

    Move app token provider to extensibility namespace and clarify use

--- src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Identity.Client.ApiConfig.Parameters;
 using Microsoft.Identity.Client.Cache.Items;
 using Microsoft.Identity.Client.Core;
+using Microsoft.Identity.Client.Extensibility;
 using Microsoft.Identity.Client.Instance;
 using Microsoft.Identity.Client.OAuth2;
 using Microsoft.Identity.Client.Utils;
@@ -143,7 +144,7 @@ namespace Microsoft.Identity.Client.Internal.Requests
                 CancellationToken = cancellationToken,
             };
 
-            TokenProviderResult externalToken = await ServiceBundle.Config.AppTokenProvider(appTokenProviderParameters).ConfigureAwait(false);
+            AppTokenProviderResult externalToken = await ServiceBundle.Config.AppTokenProvider(appTokenProviderParameters).ConfigureAwait(false);
 
             var tokenResponse =  MsalTokenResponse.CreateFromAppProviderResponse(externalToken);
             tokenResponse.Scope = appTokenProviderParameters.Scopes.AsSingleString();
