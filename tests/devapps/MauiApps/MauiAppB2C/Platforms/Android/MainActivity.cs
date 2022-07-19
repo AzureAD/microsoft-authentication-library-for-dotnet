702907667e7db5e20326e3597f7b2cbf2a8e2230
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2022-07-19 13:48:41 -0700

    Add Maui B2C app

--- tests/devapps/MauiApps/MauiAppB2C/Platforms/Android/MainActivity.cs
+++ tests/devapps/MauiApps/MauiAppB2C/Platforms/Android/MainActivity.cs
@@ -1,10 +1,27 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using MauiB2C.Platforms.Android;
+using Microsoft.Identity.Client;
+using UserDetailsClient.Core.Features.LogOn;
 
 namespace MauiB2C;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        AndroidParentWindowLocatorService parentWindowLocatorSvc = new AndroidParentWindowLocatorService();
+        parentWindowLocatorSvc.ParentWindow = this;
+        DependencyService.RegisterSingleton<IParentWindowLocatorService>(parentWindowLocatorSvc);
+    }
+
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+    }
 }
