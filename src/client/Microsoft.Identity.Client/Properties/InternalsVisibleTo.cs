54753e8d971405fcd4b9eb8557d8fe1566201de9
Michel Zehnder <MichelZ@users.noreply.github.com>
2022-08-04 12:11:13 +0200

    Move KeyTokens to it's own file (#3556)

--- src/client/Microsoft.Identity.Client/Properties/InternalsVisibleTo.cs
+++ src/client/Microsoft.Identity.Client/Properties/InternalsVisibleTo.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System.Runtime.CompilerServices;
+using Microsoft.Identity.Client;
 
 [assembly: InternalsVisibleTo("Microsoft.Identity.Client.Desktop" + KeyTokens.MSAL)]
 [assembly: InternalsVisibleTo("Microsoft.Identity.Client.Broker" + KeyTokens.MSAL)]
@@ -31,8 +32,3 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("DesktopTestApp" + KeyTokens.MSAL)]
 [assembly: InternalsVisibleTo("UWP standalone" + KeyTokens.MSAL)]
 [assembly: InternalsVisibleTo("XamarinDev" + KeyTokens.MSAL)]
-
-internal static class KeyTokens
-{
-    public const string MSAL = ", PUblicKey=00240000048000009400000006020000002400005253413100040000010001002d96616729b54f6d013d71559a017f50aa4861487226c523959d1579b93f3fdf71c08b980fd3130062b03d3de115c4b84e7ac46aef5e192a40e7457d5f3a08f66ceab71143807f2c3cb0da5e23b38f0559769978406f6e5d30ceadd7985fc73a5a609a8b74a1df0a29399074a003a226c943d480fec96dbec7106a87896539ad";
-}
