7c8fd0e4a2b2787005f1f2770a4761d15bc94563
Travis Walker <travis.walker@microsoft.com>
2022-08-15 17:08:54 -0700

    Update src/client/Microsoft.Identity.Client/IPublicClientApplication.cs
    Co-authored-by: Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>

--- src/client/Microsoft.Identity.Client/IPublicClientApplication.cs
+++ src/client/Microsoft.Identity.Client/IPublicClientApplication.cs
@@ -163,7 +163,7 @@ namespace Microsoft.Identity.Client
         /// <summary>
         /// Used to determine if the currently available broker is able to perform Proof-of-Possession.
         /// </summary>
-        /// <returns>Boolean indicating Proof-of-Possesion is supported</returns>
+        /// <returns>Boolean indicating Proof-of-Possession is supported</returns>
         public bool IsProofOfPossessionSupportedByClient();
     }
 }
