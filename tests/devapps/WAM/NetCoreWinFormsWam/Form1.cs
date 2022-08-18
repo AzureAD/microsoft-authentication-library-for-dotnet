0fabb87e167bbddded71ff239856ced3e05e9dd8
Gladwin Johnson <gljohns@microsoft.com>
2022-08-17 18:39:43 -0700

    Add PoP support to DevApps

--- tests/devapps/WAM/NetCoreWinFormsWam/Form1.cs
+++ tests/devapps/WAM/NetCoreWinFormsWam/Form1.cs
@@ -120,7 +120,7 @@ namespace NetDesktopWinForms
                     builder = ToggleOldBroker(builder, true);
                     break;
                 case AuthMethod.WAMRuntime:
-                    builder = builder.WithBrokerPreview();
+                    builder = builder.WithBrokerPreview().WithExperimentalFeatures();
                     break;
                 case AuthMethod.SystemBrowser:
                     builder = builder.WithBrokerPreview(false);
