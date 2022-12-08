ef92928747b83a253be4d9dd8187f176eeddb51c
pmaytak <34331512+pmaytak@users.noreply.github.com>
2022-12-07 21:51:49 -0800

    Split OsClientCapabilities.

---
+++ src/client/Microsoft.Identity.Client.Services/OsCapabilitiesExtensionsConfidential.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Identity.Client
+{
+    /// <summary>
+    /// Extension methods
+    /// </summary>
+    public static class OsCapabilitiesExtensionsConfidential // TODO MSAL5: move these to their parent types
+    {
+        /// <summary>
+        /// Returns the certificate used to create this <see cref="ConfidentialClientApplication"/>, if any.
+        /// </summary>
+        public static X509Certificate2 GetCertificate(this IConfidentialClientApplication confidentialClientApplication)
+        {
+
+            if (confidentialClientApplication is ConfidentialClientApplication cca)
+            {
+                return cca.Certificate;
+            }
+
+            throw new ArgumentException("This extension method is only available for the ConfidentialClientApplication implementation " +
+                "of the IConfidentialClientApplication interface.");
+        }
+    }
+}
--- src/client/Microsoft.Identity.Client/OsCapabilitiesExtensionsPublic.cs
+++ src/client/Microsoft.Identity.Client/OsCapabilitiesExtensionsPublic.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Identity.Client
     /// <summary>
     /// Extension methods
     /// </summary>
-    public static class OsCapabilitiesExtensions // TODO MSAL5: move these to their parent types
+    public static class OsCapabilitiesExtensionsPublic // TODO MSAL5: move these to their parent types
     {
         /// <summary>
         /// Returns true if MSAL can use a system browser.
@@ -66,23 +66,6 @@ namespace Microsoft.Identity.Client
 
             throw new ArgumentException("This extension method is only available for the PublicClientApplication implementation " +
                 "of the IPublicClientApplication interface.");
-        }
-
-       
-
-        /// <summary>
-        /// Returns the certificate used to create this <see cref="ConfidentialClientApplication"/>, if any.
-        /// </summary>
-        public static X509Certificate2 GetCertificate(this IConfidentialClientApplication confidentialClientApplication)
-        {
-
-            if (confidentialClientApplication is ConfidentialClientApplication cca)
-            {
-                return cca.Certificate;
-            }
-
-            throw new ArgumentException("This extension method is only available for the ConfidentialClientApplication implementation " +
-                "of the IConfidentialClientApplication interface.");
         }
     }
 }
