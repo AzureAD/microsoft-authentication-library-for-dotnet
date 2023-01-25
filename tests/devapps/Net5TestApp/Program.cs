306e6978a435e62a82bdf132d90a7b9115770a58
trwalke <trwalke@microsoft.com>
2023-01-24 23:55:15 -0800

    revert

--- tests/devapps/Net5TestApp/Program.cs
+++ tests/devapps/Net5TestApp/Program.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
-using static System.Windows.Forms.Design.AxImporter;
 
 namespace Net5TestApp
 {
@@ -31,30 +30,17 @@ namespace Net5TestApp
 
         private static async Task<AuthenticationResult> TryAuthAsync()
         {
-            var publicClientBuilder = PublicClientApplicationBuilder.Create("e3b9ad76-9763-4827-b088-80c7a7888f79");
-            publicClientBuilder.WithB2CAuthority("https://msidlabb2c.b2clogin.com/tfp/msidlabb2c.onmicrosoft.com/B2C_1_SISOPolicy/");
-            var publicClient = publicClientBuilder.Build();
+            var pca = PublicClientApplicationBuilder.Create("04b07795-8ddb-461a-bbee-02f9e1bf7b46")
+                 .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
+                 .WithDefaultRedirectUri()
+                 .WithLogging(MyLoggingMethod, LogLevel.Info, true, false)
+                 .Build();
 
-            // Gives exception only after upgrade to 4.48+
-            var result = await publicClient.AcquireTokenByUsernamePassword(
-                    new string[] { "user.read" },
-                    "b2clocal@msidlabb2c.onmicrosoft.com",
-                    "Tree1@342")
+            var result = await pca.AcquireTokenInteractive(new[] { "https://storage.azure.com/.default" })
+                .WithUseEmbeddedWebView(true)
                 .ExecuteAsync().ConfigureAwait(false);
 
             return result;
-
-            //var pca = PublicClientApplicationBuilder.Create("04b07795-8ddb-461a-bbee-02f9e1bf7b46")
-            //     .WithTenantId("72f988bf-86f1-41af-91ab-2d7cd011db47")
-            //     .WithDefaultRedirectUri()
-            //     .WithLogging(MyLoggingMethod, LogLevel.Info, true, false)
-            //     .Build();
-
-            //var result = await pca.AcquireTokenInteractive(new[] { "https://storage.azure.com/.default" })
-            //    .WithUseEmbeddedWebView(true)
-            //    .ExecuteAsync().ConfigureAwait(false);
-
-            //return result;
         }
 
         static void MyLoggingMethod(LogLevel level, string message, bool containsPii)
