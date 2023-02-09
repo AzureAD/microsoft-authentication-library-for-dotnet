c52eca54853b657babdda01e895591da672d818c
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2023-02-09 10:29:23 -0800

    Revert "PublicClientApplicationBuilder - IsBrokerAvailable obsolete (#3928)"
    This reverts commit a927f8c716677893c116333d6d8d2d12618f102b.

--- tests/devapps/NetFxConsoleTestApp/Program.cs
+++ tests/devapps/NetFxConsoleTestApp/Program.cs
@@ -126,6 +126,8 @@ namespace NetFx
 #endif
                             .WithLogging(Log, LogLevel.Verbose, true);
 
+            Console.WriteLine($"IsBrokerAvailable: {builder.IsBrokerAvailable()}");
+
             if (s_useBroker)
             {
                 IntPtr consoleWindowHandle = GetConsoleWindow();
