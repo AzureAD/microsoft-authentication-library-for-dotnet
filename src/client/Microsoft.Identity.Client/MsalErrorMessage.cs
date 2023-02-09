7ff046a0e7fefa55b7ff2c6d66fd347526f4b117
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2023-02-09 09:15:58 -0800

    Handle exception for the scenario. (#3937)
    * Handle exception for the scenario.
    
    * Update UsernamePasswordRequest.cs
    
    * Update src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
    
    Co-authored-by: Bogdan Gavril <bogavril@microsoft.com>
    
    * Address comments
    
    * Add unit test
    
    * Address comments
    
    ---------
    
    Co-authored-by: Bogdan Gavril <bogavril@microsoft.com>

--- src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
+++ src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
@@ -457,5 +457,6 @@ namespace Microsoft.Identity.Client
 
         public const string CombinedUserAppCacheNotSupported = "Using a combined flat storage, like a file, to store both app and user tokens is not supported. Use a partitioned token cache (for ex. distributed cache like Redis) or separate files for app and user token caches. See https://aka.ms/msal-net-token-cache-serialization .";
 
+        public const string JsonParseErrorMessage = "There was an error parsing the response from the token endpoint, see inner exception for details. Verify that your app is configured correctly. If this is a B2C app, one possible cause is acquiring a token for Microsoft Graph, which is not supported. See https://aka.ms/msal-net-up";
     }
 }
