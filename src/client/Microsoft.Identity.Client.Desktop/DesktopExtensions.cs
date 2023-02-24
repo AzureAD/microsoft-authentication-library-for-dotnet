a1d2e6fa4191481998e09c36c190e64b37988767
SameerK-MSFT <83378772+SameerK-MSFT@users.noreply.github.com>
2023-02-24 13:46:37 -0800

    Update src/client/Microsoft.Identity.Client.Desktop/DesktopExtensions.cs
    Co-authored-by: Bogdan Gavril <bogavril@microsoft.com>

--- src/client/Microsoft.Identity.Client.Desktop/DesktopExtensions.cs
+++ src/client/Microsoft.Identity.Client.Desktop/DesktopExtensions.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Identity.Client.Desktop
         /// Support added is around: 
         /// 
         /// - Windows Authentication Manager (WAM) broker, the recommended authentication mechanism on Windows 10 - https://aka.ms/msal-net-wam
-        /// - WebView2 embedded web view, based on Microsoft Edge - https://aka.ms/msal-net-webview2
+        /// - Embedded web view. AAD applications use the older WebBrowser control. B2C applications use WebView2, an embedded browser based on Microsoft Edge - https://aka.ms/msal-net-webview2
         /// </summary>
         /// <remarks>These extensions live in a separate package to avoid adding dependencies to MSAL</remarks>
         public static PublicClientApplicationBuilder WithWindowsDesktopFeatures(this PublicClientApplicationBuilder builder)
