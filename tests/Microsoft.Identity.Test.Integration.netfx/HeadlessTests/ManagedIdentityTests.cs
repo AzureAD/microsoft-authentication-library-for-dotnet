682a50e1e5e63f63d83e745212b433b134d19d92
Gladwin Johnson <gljohns@microsoft.com>
2023-03-09 17:05:43 -0800

    switch endpoint

--- tests/Microsoft.Identity.Test.Integration.netfx/HeadlessTests/ManagedIdentityTests.cs
+++ tests/Microsoft.Identity.Test.Integration.netfx/HeadlessTests/ManagedIdentityTests.cs
@@ -30,7 +30,7 @@ namespace Microsoft.Identity.Test.Integration.HeadlessTests
         private static readonly string s_clientId = "client_id";
 
         //http proxy base URL 
-        private static readonly string s_baseURL = "https://msihelperservice.azurewebsites.net/";
+        private static readonly string s_baseURL = "https://service.msidlab.com/";
 
         //Shared User Assigned Client ID
         private const string UserAssignedClientID = "3b57c42c-3201-4295-ae27-d6baec5b7027";
