6e7281802ef9a36749cc82e47431af94e1b42d43
pmaytak <34331512+pmaytak@users.noreply.github.com>
2023-08-02 00:36:25 -0700

    Trigger action.

--- src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
@@ -29,6 +29,7 @@ namespace Microsoft.Identity.Client.Internal.Requests
 
         protected override async Task<AuthenticationResult> ExecuteAsync(CancellationToken cancellationToken)
         {
+            var temp = string.Empty;
             if (AuthenticationRequestParameters.Scope == null || AuthenticationRequestParameters.Scope.Count == 0)
             {
                 throw new MsalClientException(
