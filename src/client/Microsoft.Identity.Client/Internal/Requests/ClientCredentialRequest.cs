88833791343d4039409d595edbb0cc5f2f42a418
pmaytak <34331512+pmaytak@users.noreply.github.com>
2023-08-05 00:21:04 -0700

    Test3. Fix ADO perf yml.

--- src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Identity.Client.Internal.Requests
 
         protected override async Task<AuthenticationResult> ExecuteAsync(CancellationToken cancellationToken)
         {
-            string _ = "temp2";
+            string _ = "temp3";
             if (AuthenticationRequestParameters.Scope == null || AuthenticationRequestParameters.Scope.Count == 0)
             {
                 throw new MsalClientException(
