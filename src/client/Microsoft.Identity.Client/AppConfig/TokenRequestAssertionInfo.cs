36297863201163cd73ce4ada03e87482a28ce578
Gladwin Johnson <gljohns@microsoft.com>
2023-11-08 23:37:42 +0000

    more

--- src/client/Microsoft.Identity.Client/AppConfig/TokenRequestAssertionInfo.cs
+++ src/client/Microsoft.Identity.Client/AppConfig/TokenRequestAssertionInfo.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#if NET6_0
+#if NET6_0 || NET6_WIN
 using System;
 using System.Linq;
 using System.Net.Http;
