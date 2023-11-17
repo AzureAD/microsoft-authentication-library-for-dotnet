773a29c86903c33a217fad9c1de1f8f5384b1044
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2023-11-17 11:13:01 -0800

    Fix build error

--- src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/AbstractPlatformProxy.cs
+++ src/client/Microsoft.Identity.Client/PlatformsCommon/Shared/AbstractPlatformProxy.cs
@@ -9,7 +9,9 @@ using Microsoft.Identity.Client.Cache;
 using Microsoft.Identity.Client.Cache.CacheImpl;
 using Microsoft.Identity.Client.Core;
 using Microsoft.Identity.Client.Internal.Broker;
+#if SUPPORTS_OTEL
 using Microsoft.Identity.Client.Platforms.Features.OpenTelemetry;
+#endif
 using Microsoft.Identity.Client.PlatformsCommon.Interfaces;
 using Microsoft.Identity.Client.TelemetryCore.OpenTelemetry;
 using Microsoft.Identity.Client.UI;
