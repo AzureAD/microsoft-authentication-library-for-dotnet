7a05670b6e0c2c7917b4b3ef617c71863ee87be1
trwalke <trwalke@microsoft.com>
2023-12-04 22:51:01 -0800

    Migrating ECD cert creation logic

--- tests/Microsoft.Identity.Test.Unit/CryptographyTests.cs
+++ tests/Microsoft.Identity.Test.Unit/CryptographyTests.cs
@@ -42,11 +42,12 @@ namespace Microsoft.Identity.Test.Unit
         [TestMethod]
         [TestCategory("CryptographyTests")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Internal.Analyzers", "IA5352:DoNotMisuseCryptographicApi", Justification = "Suppressing RoslynAnalyzers: Rule: IA5352 - Do Not Misuse Cryptographic APIs in test only code")]
-        public void SignWithNonRsaCertificate()
+        public void SignWithNonRsaCertificate_ThrowsException()
         {
             var serviceBundle = TestCommon.CreateDefaultServiceBundle();
 
-            X509Certificate2 cert = GenerateSelfSignedCertificate();
+            X509Certificate2 cert = CertHelper.GetOrCreateTestCert(KnownTestCertType.ECD);
+
             var crypto = serviceBundle.PlatformProxy.CryptographyManager;
 
             MsalClientException ex = AssertException.Throws<MsalClientException>(() =>
@@ -56,23 +57,6 @@ namespace Microsoft.Identity.Test.Unit
 
             Assert.AreEqual(ex.ErrorCode, MsalError.CertificateNotRsa);
             Assert.AreEqual(ex.Message, MsalErrorMessage.CertMustBeRsa);
-        }
-
-        public static X509Certificate2 GenerateSelfSignedCertificate()
-        {
-            string secp256r1Oid = "1.2.840.10045.3.1.7";  //oid for prime256v1(7)  other identifier: secp256r1
-
-            string subjectName = "SelfSignedEdcCert";
-
-            var ecdsa = ECDsa.Create(ECCurve.CreateFromValue(secp256r1Oid));
-
-            var certRequest = new CertificateRequest($"CN={subjectName}", ecdsa, HashAlgorithmName.SHA256);
-
-            X509Certificate2 generatedCert = certRequest.CreateSelfSigned(DateTimeOffset.Now.AddDays(-1), DateTimeOffset.Now.AddYears(10)); // generate the cert and sign!
-
-            X509Certificate2 pfxGeneratedCert = new X509Certificate2(generatedCert.Export(X509ContentType.Pfx)); //has to be turned into pfx or Windows at least throws a security credentials not found during sslStream.connectAsClient or HttpClient request...
-
-            return pfxGeneratedCert;
         }
     }
 #endif
