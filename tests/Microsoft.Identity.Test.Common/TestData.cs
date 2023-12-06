79f951a079ce2db3290874c7358d5c3242fbbe05
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2023-12-06 05:34:12 +0000

    Fix for claims and client capabilities JSON merge bug in net6 (#4452)
    * fix
    
    * build break
    
    * more tests
    
    * code reference
    
    * test names
    
    * pr comments
    
    * pr comments
    
    ---------
    
    Co-authored-by: Gladwin Johnson <gljohns@microsoft.com>

--- tests/Microsoft.Identity.Test.Common/TestData.cs
+++ tests/Microsoft.Identity.Test.Common/TestData.cs
@@ -38,5 +38,48 @@ namespace Microsoft.Identity.Test.Common
             yield return new AuthorityWithExpectedTenantId { Authority = new Uri(TestConstants.AadAuthorityWithTestTenantId), ExpectedTenantId = TestConstants.AadTenantId }.ToObjectArray();
             yield return new AuthorityWithExpectedTenantId { Authority = new Uri(TestConstants.AuthorityWindowsNet), ExpectedTenantId = TestConstants.Utid }.ToObjectArray();
         }
+
+        /// <summary>
+        /// Provides test data for scenarios involving the merging of claims and client capabilities.
+        /// </summary>
+        /// <remarks>
+        /// Test cases include various combinations of claims, client capabilities, and the expected merged JSON result.
+        /// </remarks>
+        /// <returns>Enumerable of Object Arrays with Index 0 = Claims, Index 1 = Client Capabilities, Index 2 = Expected Merged JSON</returns>
+        public static IEnumerable<object[]> GetClaimsAndCapabilities()
+        {
+            // Test case with non-empty claims, non-empty capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.Claims, TestConstants.ClientCapabilities, TestConstants.ClientCapabilitiesAndClaimsJson };
+
+            // Test case with claims containing an access token, non-empty capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.ClaimsWithAccessToken, TestConstants.ClientCapabilities, TestConstants.ClientCapabilitiesAndClaimsJsonWithAccessToken };
+
+            // Test case with empty claims, non-empty capabilities, and the expected merged JSON being the capabilities alone
+            yield return new object[] { TestConstants.EmptyClaimsJson, TestConstants.ClientCapabilities, TestConstants.ClientCapabilitiesJson };
+
+            // Test case with claims containing an additional claim, non-empty capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.ClaimsWithAdditionalClaim, TestConstants.ClientCapabilities, TestConstants.MergedJsonWithAdditionalClaim };
+
+            // Test case with claims containing an additional key, non-empty capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.ClaimWithAdditionalKey, TestConstants.ClientCapabilities, TestConstants.MergedJsonWithAdditionalKey };
+
+            // Test case with claims containing an additional key, empty capabilities, and the expected merged JSON being the claims alone
+            yield return new object[] { TestConstants.ClaimWithAdditionalKey, new string[0], TestConstants.ClaimWithAdditionalKey };
+
+            // Test case with non-empty claims, empty capabilities, and the expected merged JSON being the claims alone
+            yield return new object[] { TestConstants.Claims, new string[0], TestConstants.Claims };
+
+            // Test case with null claims, non-empty capabilities, and the expected merged JSON being the capabilities alone
+            yield return new object[] { null, TestConstants.ClientCapabilities, TestConstants.ClientCapabilitiesJson };
+
+            // Test case with non-empty claims, null capabilities, and the expected merged JSON being the claims alone
+            yield return new object[] { TestConstants.Claims, null, TestConstants.Claims };
+
+            // Test case with claims containing an access token, null capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.ClaimsWithAccessToken, null, TestConstants.ClaimsWithAccessToken };
+
+            // Test case with claims containing an additional key and access key (different order), non-empty capabilities, and the expected merged JSON
+            yield return new object[] { TestConstants.ClaimWithAdditionalKeyAndAccessKey, TestConstants.ClientCapabilities, TestConstants.MergedJsonClaimWithAdditionalKeyAndAccessKey };
+        }
     }
 }
