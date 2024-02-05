5ce30dec09c9eb41b342f8d2ef2f3bf734afe2ac
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2024-02-05 12:41:37 -0800

    Update installEdgeDriver.ps1

--- build/installEdgeDriver.ps1
+++ build/installEdgeDriver.ps1
@@ -1,4 +1,9 @@
-$url = "https://msedgedriver.azureedge.net/119.0.2151.72/edgedriver_win64.zip" #Chrome Driver from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+# Get the complete version of Microsoft Edge
+$edgeVersion = (Get-AppxPackage -Name *Microsoft.MicrosoftEdge.Stable* | ForEach-Object Version)
+
+# Assuming the complete version can be directly used (note: this might not always work due to version format differences)
+$url = "https://msedgedriver.azureedge.net/$edgeVersion/edgedriver_win64.zip"
+
 $fileName = "edgedriver_win64.zip"
 $source = "C:\Downloads\$fileName"
 $destination = "C:\Program Files\dotnet\"
@@ -11,9 +16,11 @@ if (Test-Path "$PSScriptRoot\win-installer-helper.psm1")
     Import-Module "$PSScriptRoot\..\..\Helpers\win-installer-helper.psm1" -DisableNameChecking
 }
 
+# Ensure the Downloads directory exists
 mkdir -Path C:\Downloads\ -Force
 
-Get-File -Url $url -FileName $fileName
+# Use Invoke-WebRequest for downloading the file if Get-File is not available in your environment
+Invoke-WebRequest -Uri $url -OutFile $source
 
 echo "Expanding"
 Expand-Archive -LiteralPath "$source" -DestinationPath $destination -Force
