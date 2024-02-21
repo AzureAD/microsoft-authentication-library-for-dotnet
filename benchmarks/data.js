1a5b1d07259bb94cd04aa349bdae521f73c8853c
github-action-benchmark <github@users.noreply.github.com>
2024-02-21 08:01:48 +0000

    add AcquireTokenForClientWithCache (benchmarkdotnet) benchmark result for 72da56174131be0571b83d5b273ec0dd57c9411c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708502506621,
+  "lastUpdate": 1708502508162,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-dotnet",
   "entries": {
     "AcquireTokenNoCache": [
@@ -9000,6 +9000,54 @@ window.BENCHMARK_DATA = {
             "value": 123125.5340983073,
             "unit": "ns",
             "range": "± 1488.414647687412"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "travis.walker@microsoft.com",
+            "name": "Travis Walker",
+            "username": "trwalke"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "72da56174131be0571b83d5b273ec0dd57c9411c",
+          "message": "Trwalke/claims challenge error update (#4628)\n\n* Adding Claims challenge exception and error messages\r\n\r\n* Updating error message to be more generic.\r\nAdding claims message to scrubbed log.\r\n\r\n* Clean Up\r\n\r\n* Updating claims challenge exception to inheret from UiRequired Exception\r\n\r\n* Addressing feedback\r\n\r\n* Fixing tests\r\n\r\n* Updates\r\n\r\n* Update src/client/Microsoft.Identity.Client/MsalClaimsChallengeException.cs\r\n\r\nCo-authored-by: Peter <34331512+pmaytak@users.noreply.github.com>\r\n\r\n* Update MsalServiceExceptionFactory.cs\r\n\r\n* Update MsalServiceExceptionFactory.cs\r\n\r\n---------\r\n\r\nCo-authored-by: trwalke <trwalke@microsoft.com>\r\nCo-authored-by: Peter <34331512+pmaytak@users.noreply.github.com>",
+          "timestamp": "2024-02-20T23:55:04-08:00",
+          "tree_id": "5bdd084d5a5ab4d6cb57589dccfe9bac3897b41e",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/commit/72da56174131be0571b83d5b273ec0dd57c9411c"
+        },
+        "date": 1708502507687,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Microsoft.Identity.Test.Performance.AcquireTokenForClientCacheTests.AcquireTokenForClient_TestAsync(CacheSize: (1, 10), EnableCacheSerialization: False)",
+            "value": 12435.721106896033,
+            "unit": "ns",
+            "range": "± 45.96423211230748"
+          },
+          {
+            "name": "Microsoft.Identity.Test.Performance.AcquireTokenForClientCacheTests.AcquireTokenForClient_TestAsync(CacheSize: (1, 10), EnableCacheSerialization: True)",
+            "value": 118503.94227818081,
+            "unit": "ns",
+            "range": "± 425.41982929332244"
+          },
+          {
+            "name": "Microsoft.Identity.Test.Performance.AcquireTokenForClientCacheTests.AcquireTokenForClient_TestAsync(CacheSize: (10000, 10), EnableCacheSerialization: False)",
+            "value": 23255.31351928711,
+            "unit": "ns",
+            "range": "± 224.94238933836"
+          },
+          {
+            "name": "Microsoft.Identity.Test.Performance.AcquireTokenForClientCacheTests.AcquireTokenForClient_TestAsync(CacheSize: (10000, 10), EnableCacheSerialization: True)",
+            "value": 120194.1845703125,
+            "unit": "ns",
+            "range": "± 1326.6869404312495"
           }
         ]
       }
