22bc485f4e35a4a910d146a8bc03e06ccc038436
Bogdan Gavril <bogavril@microsoft.com>
2024-03-01 11:28:21 +0000

    Undo accidental changes to public API

--- tests/devapps/Net5TestApp/Program.cs
+++ tests/devapps/Net5TestApp/Program.cs
@@ -10,15 +10,6 @@ namespace Net5TestApp
 {
     class Program
     {
-        private const string AuthorityV1 = "https://sammyciam.ciamextensibility.com/4710d5e4-43bb-4ff9-89af-30ed8fe31c6d/";
-        private const string AuthorityV2 = "https://sammyciam.ciamextensibility.com/4710d5e4-43bb-4ff9-89af-30ed8fe31c6d/v2.0/";
-
-        private const string AuthorityV1DC = "https://sammyciam.ciamextensibility.com/4710d5e4-43bb-4ff9-89af-30ed8fe31c6d?DC=ESTS-PUB-SCUS-LZ1-FD000-TEST1";
-        private const string AuthorityV2DC = "https://sammyciam.ciamextensibility.com/4710d5e4-43bb-4ff9-89af-30ed8fe31c6d/v2.0?DC=ESTS-PUB-SCUS-LZ1-FD000-TEST1";
-
-        private const string DC = "dc=ESTS-PUB-SCUS-LZ1-FD000-TEST1";
-        private const string Scope = "api://2a247857-5770-477e-a9c0-a5f6bc8e66db/Scope2";
-
         static async Task Main(string[] args)
         {
             try
@@ -42,10 +33,7 @@ namespace Net5TestApp
             var pca = PublicClientApplicationBuilder.
                 Create("fe67c0cf-caae-49f0-9f75-e3f7e1e28724")
                 .WithExperimentalFeatures(true)
-                //.WithAuthority(AuthorityV1)
                 .WithExtraQueryParameters(DC)
-                .WithGenericAuthority(AuthorityV2DC)
-                //.WithInstanceDiscovery(false)                
                 .WithRedirectUri("http://localhost")
                 .Build();
 
