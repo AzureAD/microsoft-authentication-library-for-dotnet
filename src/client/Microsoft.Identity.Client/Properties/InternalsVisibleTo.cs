d2008b7d8532e1d0045ae8aea21966f2679e4504
Ashok Kumar Ramakrishnan <83938949+ashok672@users.noreply.github.com>
2024-03-17 22:36:20 -0700

    Update InternalsVisibleTo.cs

--- src/client/Microsoft.Identity.Client/Properties/InternalsVisibleTo.cs
+++ src/client/Microsoft.Identity.Client/Properties/InternalsVisibleTo.cs
@@ -6,7 +6,6 @@ using Microsoft.Identity.Client;
 
 [assembly: InternalsVisibleTo("Microsoft.Identity.Client.Desktop" + KeyTokens.MSAL)]
 [assembly: InternalsVisibleTo("Microsoft.Identity.Client.Broker" + KeyTokens.MSAL)]
-[assembly: InternalsVisibleTo("Microsoft.Identity.Client.SsoPolicy" + KeyTokens.MSAL)]
 
 [assembly: InternalsVisibleTo("Microsoft.Identity.Test.Unit" + KeyTokens.MSAL)]
 [assembly: InternalsVisibleTo("Microsoft.Identity.Test.Common" + KeyTokens.MSAL)]
