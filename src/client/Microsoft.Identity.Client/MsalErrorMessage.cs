120423b0af348cfe3cbf6bc3f5db86aba3045424
Ashok Kumar Ramakrishnan <83938949+ashok672@users.noreply.github.com>
2024-03-19 18:35:07 -0700

    Update src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
    Co-authored-by: Bogdan Gavril <bogavril@microsoft.com>

--- src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
+++ src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
@@ -407,7 +407,8 @@ namespace Microsoft.Identity.Client
 
         public const string MultiCloudSupportUnavailable = "Multi cloud support unavailable with broker.";
 
-        public const string BrokerAndSsoPolicyConfigurationError = "WithBroker and WithSsoPolicy cannot be used together. Broker by default supports SsoPolicy. Use WithSsoPolicy only when using embedded webview or system webview";
+        public const string BrokerAndSsoPolicyConfigurationError = "WithBroker and WithSsoPolicy cannot be used together. Broker by default supports SsoPolicy. Use WithSsoPolicy only when using embedded webview";
+
 
         public const string RequestFailureErrorMessage = "=== Token Acquisition ({0}) failed.\n\tHost: {1}.";
 
