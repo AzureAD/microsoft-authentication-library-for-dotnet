b3d8ce5eb09fea92d4f20b393f55dddf0cc4cbf0
Robert <36173263+kurtanr@users.noreply.github.com>
2024-05-09 19:48:56 +0200

    PublicClientApplicationBuilder - allow non-GUID client id for AuthorityType.Generic (#4748)
    * PublicClientApplicationBuilder - allow non-GUID client id for AuthorityType.Generic
    
    * Completely removed ClientIdMustBeAGuid validation + related test and error messages.
    
    * Moved generic authority interactive test from GenericAuthorityTests to InteractiveFlowTests.

--- src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
+++ src/client/Microsoft.Identity.Client/MsalErrorMessage.cs
@@ -245,7 +245,6 @@ namespace Microsoft.Identity.Client
 
         public const string ClientCredentialAuthenticationTypesAreMutuallyExclusive = "ClientSecret, Certificate and ClientAssertion are mutually exclusive properties. Only specify one. See https://aka.ms/msal-net-client-credentials. ";
         public const string ClientCredentialAuthenticationTypeMustBeDefined = "One client credential type required either: ClientSecret, Certificate, ClientAssertion or AppTokenProvider must be defined when creating a Confidential Client. Only specify one. See https://aka.ms/msal-net-client-credentials. ";
-        public const string ClientIdMustBeAGuid = "Error: ClientId is not a GUID. ";
 
         public static string InvalidRedirectUriReceived(string invalidRedirectUri)
         {
