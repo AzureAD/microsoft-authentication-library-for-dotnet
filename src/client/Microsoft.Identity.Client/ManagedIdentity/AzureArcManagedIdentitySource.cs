93cae2aecd32d4858c68a1903aee4bf7c3e1120a
Neha Bhargava <61847233+neha-bhargava@users.noreply.github.com>
2024-06-06 14:14:41 -0700

    Update

--- src/client/Microsoft.Identity.Client/ManagedIdentity/AzureArcManagedIdentitySource.cs
+++ src/client/Microsoft.Identity.Client/ManagedIdentity/AzureArcManagedIdentitySource.cs
@@ -198,7 +198,7 @@ namespace Microsoft.Identity.Client.ManagedIdentity
                     MsalErrorMessage.ManagedIdentityPlatformNotSupported);
             }
 
-            return path.Equals(expectedFilePath, StringComparison.OrdinalIgnoreCase);
+            return path.Equals(expectedFilePath);
         }
     }
 }
