68a892261c063f020975583a35ec38aba064a595
trwalke <trwalke@microsoft.com>
2024-10-01 00:56:59 -0700

    Resolving build errors

--- tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/CdtTests.cs
+++ tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/CdtTests.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#if NET6_0_OR_GREATER
+#if PREVIEW_CDT
 using System;
 using System.Collections.Generic;
 using System.Linq;
