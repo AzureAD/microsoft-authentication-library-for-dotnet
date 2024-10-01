ffbe6e3c4dc000b0c175925ca2601156a2f58cf6
trwalke <trwalke@microsoft.com>
2024-10-01 02:04:28 -0700

    Revert "Resolving build errors"
    This reverts commit 68a892261c063f020975583a35ec38aba064a595.

--- tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/CdtTests.cs
+++ tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/CdtTests.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-#if PREVIEW_CDT
+#if NET6_0_OR_GREATER
 using System;
 using System.Collections.Generic;
 using System.Linq;
