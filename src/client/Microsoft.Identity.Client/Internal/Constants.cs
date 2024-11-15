e7a1d1ee5a36235414532644b7f78d2bd3b7bc75
Gladwin Johnson <gljohns@microsoft.com>
2024-11-15 10:33:46 -0800

    temp

--- src/client/Microsoft.Identity.Client/Internal/Constants.cs
+++ src/client/Microsoft.Identity.Client/Internal/Constants.cs
@@ -33,6 +33,7 @@ namespace Microsoft.Identity.Client.Internal
         public const int DefaultJitterRangeInSeconds = 300;
         public static readonly TimeSpan AccessTokenExpirationBuffer = TimeSpan.FromMinutes(5);
         public const string EnableSpaAuthCode = "1";
+        public const string BearerTokenType = "bearer";
         public const string PoPTokenType = "pop";
         public const string MtlsPoPTokenType = "mtls_pop";
         public const string PoPAuthHeaderPrefix = "PoP";
--- tests/Microsoft.Identity.Test.Common/Core/Helpers/HttpSnifferClientFactory.cs
+++ tests/Microsoft.Identity.Test.Common/Core/Helpers/HttpSnifferClientFactory.cs
@@ -1,14 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Identity.Client;
 
 namespace Microsoft.Identity.Test.Common
 {
-    public class HttpSnifferClientFactory : IMsalHttpClientFactory
+    public class HttpSnifferClientFactory : IMsalMtlsHttpClientFactory
     {
         readonly HttpClient _httpClient;
 
@@ -37,6 +39,19 @@ namespace Microsoft.Identity.Test.Common
         public HttpClient GetHttpClient()
         {
             return _httpClient;
+        }
+
+        public HttpClient GetHttpClient(X509Certificate2 x509Certificate2)
+        {
+            if (x509Certificate2 == null)
+            {
+                throw new ArgumentNullException(nameof(x509Certificate2), "Certificate cannot be null for MTLS.");
+            }
+
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(x509Certificate2);
+
+            return new HttpClient(handler);
         }
     }
 }
