b8f199d8b3399d43a37c91abc58d1b28a6d899d5
Bogdan Gavril <bogavril@microsoft.com>
2024-12-31 17:40:51 +0000

    1

--- src/client/Microsoft.Identity.Client/Platforms/net/JsonObjectAttribute.cs
+++ src/client/Microsoft.Identity.Client/Platforms/net/JsonObjectAttribute.cs
@@ -6,7 +6,7 @@ using System;
 namespace Microsoft.Identity.Client.Platforms.net
 {
     /// <summary>
-    /// Dummy class to mimic Microsoft.Identity.Json.JsonObjectAttribute on Net 6 platform to reduce the number of compilation flags in the code
+    /// Dummy class to mimic Microsoft.Identity.Json.JsonObjectAttribute on .NET platform to reduce the number of compilation flags in the code
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false)]
     internal class JsonObjectAttribute : Attribute
