27ab6544b08d1751557837e6dcea03669def2197
Xinyu Xu <xinyuxu@microsoft.com>
2025-02-03 17:10:29 -0800

    use parent handle

--- src/client/Microsoft.Identity.Client.Broker/RuntimeBroker.cs
+++ src/client/Microsoft.Identity.Client.Broker/RuntimeBroker.cs
@@ -399,15 +399,26 @@ namespace Microsoft.Identity.Client.Platforms.Features.RuntimeBroker
             {
                 authParams.Properties["MSALRuntime_Username"] = acquireTokenByUsernamePasswordParameters.Username;
                 authParams.Properties["MSALRuntime_Password"] = acquireTokenByUsernamePasswordParameters.Password;
-
-                using (NativeInterop.AuthResult result = await s_lazyCore.Value.SignInSilentlyAsync(
-                    authParams,
-                    authenticationRequestParameters.CorrelationId.ToString("D"),
-                    cancellationToken).ConfigureAwait(false))
-                {
-                    var errorMessage = "Could not acquire token with username and password.";
-                    msalTokenResponse = WamAdapters.HandleResponse(result, authenticationRequestParameters, _logger, errorMessage);
-                }
+                // For Linux broker, use the interactive flow with username password to get the token
+                if (Environment.GetEnvironmentVariable("TF_BUILD") != null && DesktopOsHelper.IsLinux()) {
+                    using (NativeInterop.AuthResult result = await s_lazyCore.Value.SignInInteractivelyAsync(
+                        _parentHandle,
+                        authParams,
+                        authenticationRequestParameters.CorrelationId.ToString("D"),
+                        cancellationToken).ConfigureAwait(false))
+                    {
+                        var errorMessage = "Could not acquire token with username and password.";
+                        msalTokenResponse = WamAdapters.HandleResponse(result, authenticationRequestParameters, _logger, errorMessage);
+                    }
+                } else {
+                    using (NativeInterop.AuthResult result = await s_lazyCore.Value.SignInSilentlyAsync(
+                        authParams,
+                        authenticationRequestParameters.CorrelationId.ToString("D"),
+                        cancellationToken).ConfigureAwait(false))
+                    {
+                        var errorMessage = "Could not acquire token with username and password.";
+                        msalTokenResponse = WamAdapters.HandleResponse(result, authenticationRequestParameters, _logger, errorMessage);
+                    }
                 
             }
 
