4d37dee06de715d9fcd8dc1597d2a01516569653
Xinyu Xu <xinyuxu@microsoft.com>
2025-02-03 21:53:06 -0800

    fix import issue

--- src/client/Microsoft.Identity.Client.Broker/RuntimeBroker.cs
+++ src/client/Microsoft.Identity.Client.Broker/RuntimeBroker.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client.ApiConfig.Parameters;
 using Microsoft.Identity.Client.Cache;
