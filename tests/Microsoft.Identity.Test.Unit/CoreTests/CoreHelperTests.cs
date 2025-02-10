3ca9ea346e7c23f855cec2260f80ab120059ac72
Feng Gao <fengga@microsoft.com>
2025-02-10 12:01:00 -0800

    Update test

--- tests/Microsoft.Identity.Test.Unit/CoreTests/CoreHelperTests.cs
+++ tests/Microsoft.Identity.Test.Unit/CoreTests/CoreHelperTests.cs
@@ -9,23 +9,18 @@ using Microsoft.Identity.Client.Utils;
 
 namespace Microsoft.Identity.Test.Unit.CoreTests
 {
+    [TestClass]
     public class CoreHelperTests
     {
         [TestMethod]
         public void UrlEncodeDecodeTest()
         {
-            ClientInfo clientInfo = ClientInfo.CreateFromJson("eyJ1aWQiOiJteS11aWQiLCJ1dGlkIjoibXktdXRpZCJ9");
-            Assert.IsNotNull(clientInfo);
-            Assert.AreEqual(TestConstants.Uid, clientInfo.UniqueObjectIdentifier);
-            Assert.AreEqual(TestConstants.Utid, clientInfo.UniqueTenantIdentifier);
+            Assert.AreEqual(CoreHelpers.UrlEncode("https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize"), "https%3A%2F%2Flogin.microsoftonline.com%2Forganizations%2Foauth2%2Fv2.0%2Fauthorize");
+            Assert.AreEqual(CoreHelpers.UrlEncode("https://management.core.windows.net//.default openid profile offline_access"), "https%3A%2F%2Fmanagement.core.windows.net%2F%2F.default%20openid%20profile%20offline_access");
 
-            string originalUrl = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize?scope=openid%20profile%20offline_access&response_type=code";
-            string urlWithPlus = "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize?scope=openid+profile+offline_access&response_type=code";
-
-            string encodedUrl = CoreHelpers.UrlEncode(originalUrl);
-            Assert.AreEqual(encodedUrl, originalUrl);
-            string decodedUIrl = CoreHelpers.UrlDecode(urlWithPlus);
-            Assert.AreEqual(decodedUIrl, originalUrl);
+            Assert.AreEqual(CoreHelpers.UrlDecode("https%3A%2F%2Flogin.microsoftonline.com%2Forganizations%2Foauth2%2Fv2.0%2Fauthorize"), "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize");
+            Assert.AreEqual(CoreHelpers.UrlDecode("https%3A%2F%2Fmanagement.core.windows.net%2F%2F.default%20openid%20profile%20offline_access"), "https://management.core.windows.net//.default openid profile offline_access");
+            Assert.AreEqual(CoreHelpers.UrlDecode("https%3A%2F%2Fmanagement.core.windows.net%2F%2F.default+openid+profile+offline_access"), "https://management.core.windows.net//.default openid profile offline_access");
         }
     }
 }
