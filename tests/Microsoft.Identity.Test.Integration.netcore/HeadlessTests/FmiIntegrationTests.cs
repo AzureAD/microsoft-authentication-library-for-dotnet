e8240f2834f1cb8609844f87c448837854e87222
trwalke <trwalke@microsoft.com>
2025-02-19 22:31:54 -0800

    Adding more FMI-FIC tests

--- tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/FmiIntegrationTests.cs
+++ tests/Microsoft.Identity.Test.Integration.netcore/HeadlessTests/FmiIntegrationTests.cs
@@ -1,22 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Client.NativeInterop;
 using Microsoft.Identity.Test.Integration.Infrastructure;
 using Microsoft.Identity.Test.LabInfrastructure;
 using Microsoft.Identity.Test.Unit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Identity.Test.Common.Core.Helpers;
-using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Identity.Client.Utils;
 
 namespace Microsoft.Identity.Test.Integration.NetCore.HeadlessTests
 {
@@ -24,29 +17,61 @@ namespace Microsoft.Identity.Test.Integration.NetCore.HeadlessTests
     public class FmiIntegrationTests
     {
         private const string _fmiAppUrn = "urn:microsoft:identity:fmi";
-        private const string _fmiClientId = "4df2cbbb-8612-49c1-87c8-f334d6d065ad";
+        private const string _fmiRmaClientId = "4df2cbbb-8612-49c1-87c8-f334d6d065ad";
+        private const string _fmiNonRmaClientId = "";
+        private const string _fmiNonRmaClientIdInpersonation = "";
         private const string _fmiTenantId = "f645ad92-e38d-4d1a-b510-d1b09a74a8ca";
         private const string _fmiAuthority = "https://login.microsoftonline.com/" + _fmiTenantId;
-        private const string _fmiScope1 = "api://AzureFMITokenExchange/.default";
-        private const string _fmiScope2 = "022907d3-0f1b-48f7-badc-1ba6abab6d66/.default";
-        private const string _fmiScope3 = "api://AzureADTokenExchange/.default";
+        private const string _fmiExchangeScope = "api://AzureFMITokenExchange/.default";
+        private const string _fmiExchangeScopeAsGuid = "022907d3-0f1b-48f7-badc-1ba6abab6d66/.default";
+        private const string _fmiAadExchangeScope = "api://AzureADTokenExchange/.default";
+        private const string _fmiAadExchangeScopeAsGuid = "api://d796a5d2-0fb6-499a-b311-8bf5b3d058f7";
         private const string _fmiPath = "SomeFmiPath/fmi";
 
         [TestMethod]
-        public async Task RmaFmiCredFlow1Async()
+        public async Task RmaFmiCredFlowAsync()
         {
-            await RunRmaFlow(_fmiClientId, _fmiAuthority, _fmiScope1).ConfigureAwait(false);
+            await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiExchangeScope).ConfigureAwait(false);
         }
 
-        public async Task RmaFmiCredFlow2Async()
+        [TestMethod]
+        public async Task RmaFmiCredFlowGuidAsync()
         {
-            await RunRmaFlow(_fmiClientId, _fmiAuthority, _fmiScope2).ConfigureAwait(false);
+            await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiExchangeScopeAsGuid).ConfigureAwait(false);
         }
 
+        [TestMethod]
         public async Task FmiCredFlow2Async()
         {
-            var fmiCredential = await RunRmaFlow(_fmiClientId, _fmiAuthority, _fmiScope2).ConfigureAwait(false);
-            await RunFicFlow(fmiCredential).ConfigureAwait(false);
+            var fmiCredential = await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiExchangeScope).ConfigureAwait(false);
+            await RunFicFlow(fmiCredential, _fmiExchangeScope, _fmiAppUrn).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task FmiCredFlowAadExchangeAsync()
+        {
+            var fmiCredential = await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiExchangeScope).ConfigureAwait(false);
+            await RunFicFlow(fmiCredential, _fmiAadExchangeScope, _fmiAppUrn).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task FmiCredFlowAadExchangeGuidAsync()
+        {
+            var fmiCredential = await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiAadExchangeScopeAsGuid).ConfigureAwait(false);
+            await RunFicFlow(fmiCredential, _fmiAadExchangeScope, _fmiAppUrn).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task NonRmaFmiCredFlowAsync()
+        {
+            await RunRmaFlow(_fmiNonRmaClientId, _fmiAuthority, _fmiAadExchangeScope).ConfigureAwait(false);
+        }
+
+        [TestMethod]
+        public async Task NonRmaFmiCredFlowAadExchangeAsync()
+        {
+            var fmiCredential = await RunRmaFlow(_fmiRmaClientId, _fmiAuthority, _fmiExchangeScope).ConfigureAwait(false);
+            await RunFicFlow(fmiCredential, _fmiExchangeScope, _fmiNonRmaClientIdInpersonation).ConfigureAwait(false);
         }
 
         private async Task<string> RunRmaFlow(string clientId, string authority, string scope)
@@ -83,23 +108,23 @@ namespace Microsoft.Identity.Test.Integration.NetCore.HeadlessTests
             return authResult.AccessToken;
         }
 
-        private async Task RunFicFlow(string fmiCredential)
+        private async Task RunFicFlow(string fmiCredential, string scope, string clientId)
         {
             var confidentialApp = ConfidentialClientApplicationBuilder
-                        .Create(_fmiAppUrn)
+                        .Create(clientId)
                         .WithAuthority(_fmiAuthority, true)
                         .WithExtraQueryParameters("dc=ESTS-PUB-SCUS-LZ1-FD000-TEST1")
                         .WithExperimentalFeatures(true)
                         .WithClientAssertion((options) =>
                             {
-                                Assert.AreEqual(_fmiAppUrn, options.ClientID);
+                                Assert.AreEqual(clientId, options.ClientID);
                                 return Task.FromResult(fmiCredential);
                             })
                         .BuildConcrete();
 
             var appCacheRecorder = confidentialApp.AppTokenCache.RecordAccess();
 
-            var authResult = await confidentialApp.AcquireTokenForClient(new[] { _fmiScope3 })
+            var authResult = await confidentialApp.AcquireTokenForClient(new[] { scope })
                                                     .WithFmiPath(_fmiPath)
                                                     .ExecuteAsync()
                                                     .ConfigureAwait(false);
@@ -109,8 +134,8 @@ namespace Microsoft.Identity.Test.Integration.NetCore.HeadlessTests
             Assert.AreEqual(TokenSource.IdentityProvider, authResult.AuthenticationResultMetadata.TokenSource);
             Assert.IsTrue(appCacheRecorder.LastAfterAccessNotificationArgs.IsApplicationCache);
             Assert.IsTrue(appCacheRecorder.LastAfterAccessNotificationArgs.HasTokens);
-            CollectionAssert.AreEquivalent(new[] { _fmiScope3 }, appCacheRecorder.LastBeforeAccessNotificationArgs.RequestScopes.ToArray());
-            CollectionAssert.AreEquivalent(new[] { _fmiScope3 }, appCacheRecorder.LastAfterAccessNotificationArgs.RequestScopes.ToArray());
+            CollectionAssert.AreEquivalent(new[] { scope }, appCacheRecorder.LastBeforeAccessNotificationArgs.RequestScopes.ToArray());
+            CollectionAssert.AreEquivalent(new[] { scope }, appCacheRecorder.LastAfterAccessNotificationArgs.RequestScopes.ToArray());
             Assert.AreEqual(_fmiTenantId, appCacheRecorder.LastBeforeAccessNotificationArgs.RequestTenantId ?? "");
             Assert.AreEqual(_fmiTenantId, appCacheRecorder.LastAfterAccessNotificationArgs.RequestTenantId ?? "");
             Assert.IsTrue(authResult.AuthenticationResultMetadata.DurationTotalInMs > 0);
