3a2e26f6089741f54b3c26b6ab70cceb453ea1f9
Travis Walker <travis.walker@microsoft.com>
2025-03-03 20:28:27 -0800

    Fix null check in unit test.

--- tests/Microsoft.Identity.Test.Unit/PublicApiTests/ClientCredentialWithCertTest.cs
+++ tests/Microsoft.Identity.Test.Unit/PublicApiTests/ClientCredentialWithCertTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 #if !ANDROID && !iOS 
@@ -874,7 +874,7 @@ namespace Microsoft.Identity.Test.Unit
 
                 var token = app2.AppTokenCacheInternal.Accessor.GetAllAccessTokens()
                                                                 .Where(x =>
-                                                                x.AdditionalCacheKeyComponents != null &&
+                                                                x.AdditionalCacheKeyComponents?.Any() == true &&
                                                                 x.AdditionalCacheKeyComponents.Any()).FirstOrDefault();
 
                 Assert.IsNotNull(token);
