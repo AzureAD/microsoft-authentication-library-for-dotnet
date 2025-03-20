a291b797847fd6d00695e5faea6438c452bc553b
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2025-03-20 09:42:20 -0700

    Update ClientCredentialRequest.cs

--- src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
+++ src/client/Microsoft.Identity.Client/Internal/Requests/ClientCredentialRequest.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Identity.Client.Internal.Requests
             if (skipCache)
             {
                 AuthenticationRequestParameters.RequestContext.ApiEvent.CacheInfo = CacheRefreshReason.ForceRefreshOrClaims;
-                logger.Info("[ClientCredentialRequest] Skipped looking for a cached access token because either of ForceRefresh, Claims or AccessTokenHashToRefresh were set.");
+                logger.Info("[ClientCredentialRequest] Skipped looking for a cached access token because ForceRefresh was requested, or there are Claims but no AccessTokenHashToRefresh.");
                 authResult = await GetAccessTokenAsync(cancellationToken, logger).ConfigureAwait(false);
                 return authResult;
             }
