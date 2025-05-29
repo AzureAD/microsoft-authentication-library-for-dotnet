c1cbb0f2bc26b5fbf51402c774d8d4ce2f35c5ed
Ashok Kumar Ramakrishnan <83938949+ashok672@users.noreply.github.com>
2025-05-28 18:03:29 -0700

    Update BrokerOptions.cs

--- src/client/Microsoft.Identity.Client/ApiConfig/BrokerOptions.cs
+++ src/client/Microsoft.Identity.Client/ApiConfig/BrokerOptions.cs
@@ -79,8 +79,8 @@ namespace Microsoft.Identity.Client
         public bool MsaPassthrough { get; set; } = false;
 
         /// <summary>
-        /// Currently supported on Windows, Linux and macOS
-        /// Allows the Windows broker to list MSA, Work and School accounts as part of the <see cref="ClientApplicationBase.GetAccountsAsync()"/>
+        /// Currently supported on Windows, Linux and macOS.
+        /// Allows the Windows broker to list MSA, Work and School accounts as part of the <see cref="ClientApplicationBase.GetAccountsAsync()"/>.
         /// Linux and macOS broker will discover accounts as part of the <see cref="ClientApplicationBase.GetAccountsAsync()"/>
         /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
