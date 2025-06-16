bbaa1bfd80bfeb5e1593e0de8be8a1e92b7e1e9a
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2025-06-16 13:31:49 -0700

    MAA POC  (#5339)
    * init
    
    * pr comments
    
    * pr comments
    
    * typos
    
    ---------
    
    Co-authored-by: Gladwin Johnson <gljohns@microsoft.com>

---
+++ prototype/KeyGuardMaa/AttestationLogger.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace KeyGuard.Attestation
+{
+    internal static class AttestationLogger
+    {
+        /// <summary>Default logger that pipes native messages to <c>Console.WriteLine</c>.</summary>
+        internal static readonly NativeMethods.LogFunc ConsoleLogger = (_,
+            tag, lvl, func, line, msg) =>
+            Console.WriteLine($"[{lvl}] {tag} {func}:{line}  {msg}");
+    }
+}
