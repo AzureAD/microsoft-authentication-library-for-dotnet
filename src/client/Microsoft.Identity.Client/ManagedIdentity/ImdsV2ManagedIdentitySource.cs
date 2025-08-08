068461b344757548f01a734ce8fa88f1e4cabefb
Robbie Ginsburg <rginsburg@microsoft.com>
2025-08-08 19:17:19 -0400

    Fixed bugs discovered from unit testing in child branch

--- src/client/Microsoft.Identity.Client/ManagedIdentity/ImdsV2ManagedIdentitySource.cs
+++ src/client/Microsoft.Identity.Client/ManagedIdentity/ImdsV2ManagedIdentitySource.cs
@@ -18,6 +18,7 @@ namespace Microsoft.Identity.Client.ManagedIdentity
     {
         private const string CsrMetadataPath = "/metadata/identity/getPlatformMetadata";
         private const string ClientCredentialRequestPath = "/metadata/identity/issuecredential";
+        private const string AcquireEntraTokenPath = "/oauth2/v2.0/token";
 
         public static async Task<CsrMetadata> GetCsrMetadataAsync(
             RequestContext requestContext,
@@ -199,7 +200,7 @@ namespace Microsoft.Identity.Client.ManagedIdentity
             CuidInfo Cuid,
             string pem)
         {
-            var queryParams = $"cid={Cuid}";
+            var queryParams = $"cid={JsonHelper.SerializeToJson(Cuid)}";
             if (_requestContext.ServiceBundle.Config.ManagedIdentityId.UserAssignedId != null)
             {
                 queryParams += $"&uaid{_requestContext.ServiceBundle.Config.ManagedIdentityId.UserAssignedId}";
@@ -212,6 +213,12 @@ namespace Microsoft.Identity.Client.ManagedIdentity
                 { "x-ms-client-request-id", _requestContext.CorrelationId.ToString() }
             };
 
+            var payload = new
+            {
+                pem = pem
+            };
+            var body = JsonHelper.SerializeToJson(payload);
+
             IRetryPolicyFactory retryPolicyFactory = _requestContext.ServiceBundle.Config.RetryPolicyFactory;
             IRetryPolicy retryPolicy = retryPolicyFactory.GetRetryPolicy(RequestType.Imds);
 
@@ -222,7 +229,7 @@ namespace Microsoft.Identity.Client.ManagedIdentity
                 response = await _requestContext.ServiceBundle.HttpManager.SendRequestAsync(
                     ImdsManagedIdentitySource.GetValidatedEndpoint(_requestContext.Logger, ClientCredentialRequestPath, queryParams),
                     headers,
-                    body: new StringContent($"{{\"pem\":\"{pem}\"}}", System.Text.Encoding.UTF8, "application/json"),
+                    body: new StringContent(body, System.Text.Encoding.UTF8, "application/json"),
                     method: HttpMethod.Post,
                     logger: _requestContext.Logger,
                     doNotThrow: false,
