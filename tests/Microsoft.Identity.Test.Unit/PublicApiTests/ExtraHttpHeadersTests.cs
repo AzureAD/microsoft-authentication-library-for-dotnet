e41aac2d81eb101cce844b0271f830ce66ee82dd
Gladwin Johnson <gljohns@microsoft.com>
2025-08-15 07:57:49 -0700

    new surface

--- tests/Microsoft.Identity.Test.Unit/PublicApiTests/ExtraHttpHeadersTests.cs
+++ tests/Microsoft.Identity.Test.Unit/PublicApiTests/ExtraHttpHeadersTests.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client;
-using Microsoft.Identity.Client.Advanced;
+using Microsoft.Identity.Client.Extensibility;
 using Microsoft.Identity.Test.Common.Core.Helpers;
 using Microsoft.Identity.Test.Common.Core.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
