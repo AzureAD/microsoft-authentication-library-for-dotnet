9e29306a1d8fa43d6865336436c34c71b8905706
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2025-08-31 07:44:48 -0700

    Add new mtls pop package for MSI flows

---
+++ src/client/Microsoft.Identity.Client.MtlsPop/Attestation/AttestationLogger.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Identity.Client.MtlsPop.Attestation
+{
+    internal static class AttestationLogger
+    {
+        /// <summary>Default logger that pipes native messages to <c>Console.WriteLine</c>.</summary>
+        internal static readonly AttestationClientLib.LogFunc ConsoleLogger = (_,
+            tag, lvl, func, line, msg) =>
+            Console.WriteLine($"[{lvl}] {tag} {func}:{line}  {msg}");
+    }
+}
