2518f1b1fbf6d64d7e9c05c3df1a36671aa10240
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2025-09-18 16:51:10 -0700

    MSI V2 - Adding Attestation package for mTLS POP MSI flows (#5483)
    * init
    
    * added comments

---
+++ src/client/Microsoft.Identity.Client.MtlsPop/Attestation/AttestationResult.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Client.MtlsPop.Attestation
+{
+    /// <summary>
+    /// AttestationResult is the result of an attestation operation.
+    /// </summary>
+    /// <param name="Status"></param>
+    /// <param name="Jwt"></param>
+    /// <param name="NativeErrorCode"></param>
+    /// <param name="ErrorMessage"></param>
+    internal sealed record AttestationResult(
+        AttestationStatus Status,
+        string Jwt,
+        int NativeErrorCode,
+        string ErrorMessage);
+}
