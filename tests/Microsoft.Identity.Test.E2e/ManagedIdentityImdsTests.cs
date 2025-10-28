2d43321d4517a60629af4c76c71df7baeeab8ef5
Robbie Ginsburg <rginsburg@microsoft.com>
2025-10-28 12:45:38 -0400

    Added failure e2e test for if imdsv1 requests mtls pop token

--- tests/Microsoft.Identity.Test.E2e/ManagedIdentityImdsTests.cs
+++ tests/Microsoft.Identity.Test.E2e/ManagedIdentityImdsTests.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Client.AppConfig;
+using Microsoft.Identity.Client.MtlsPop;
 using Microsoft.Identity.Test.Common.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -62,6 +63,27 @@ namespace Microsoft.Identity.Test.E2E
             Assert.AreEqual(TokenSource.IdentityProvider, result.AuthenticationResultMetadata.TokenSource);
             Assert.AreEqual(TokenSource.Cache, second.AuthenticationResultMetadata.TokenSource);
             Assert.AreEqual(result.AccessToken, second.AccessToken);
+        }
+
+        [RunOnAzureDevOps]
+        [TestCategory("MI_E2E_Imds")]
+        [DataTestMethod]
+        [DataRow(null /*SAMI*/, null, DisplayName = "SAMI")]
+        [DataRow("4b7a4b0b-ecb2-409e-879a-1e21a15ddaf6", "clientid", DisplayName = "UAMI-ClientId")]
+        [DataRow("/subscriptions/c1686c51-b717-4fe0-9af3-24a20a41fb0c/resourcegroups/MSAL_MSI/providers/Microsoft.ManagedIdentity/userAssignedIdentities/LabVaultAccess_UAMI",
+         "resourceid", DisplayName = "UAMI-ResourceId")]
+        [DataRow("1eee55b7-168a-46be-8d19-30e830ee9611", "objectid", DisplayName = "UAMI-ObjectId")]
+        public async Task AcquireToken_OnImds_Fails_WithMtlsProofOfPossession(string id, string idType)
+        {
+            var mi = BuildMi(id, idType);
+
+            var ex = await Assert.ThrowsExceptionAsync<MsalClientException>(async () =>
+                await mi.AcquireTokenForManagedIdentity(ArmScope)
+                .WithMtlsProofOfPossession()
+                .ExecuteAsync().ConfigureAwait(false)
+            ).ConfigureAwait(false);
+
+            Assert.AreEqual(MsalError.MtlsPopTokenNotSupportedinImdsV1, ex.ErrorCode);
         }
     }
 }
