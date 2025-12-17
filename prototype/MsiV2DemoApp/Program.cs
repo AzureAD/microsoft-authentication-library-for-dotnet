31cf11d72e4e1412e9d89d9fdc5e3dfb17610e12
Robbie Ginsburg <rginsburg@microsoft.com>
2025-12-17 18:34:34 -0500

    added .WithAttestationSupport to demo app

--- prototype/MsiV2DemoApp/Program.cs
+++ prototype/MsiV2DemoApp/Program.cs
@@ -256,7 +256,7 @@ internal class Program
         bool showFullToken)
     {
         var builder = app.AcquireTokenForManagedIdentity(scope);
-        if (useMtls) builder = builder.WithMtlsProofOfPossession();
+        if (useMtls) builder = builder.WithMtlsProofOfPossession().WithAttestationSupport();
         if (forceRefresh) builder = builder.WithForceRefresh(true);
 
         var result = await Ui.WithSpinnerAsync(
