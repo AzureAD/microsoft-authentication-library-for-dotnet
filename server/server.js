288853f70ab5dce0ecc019fd3943af5f29a0b985
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2026-02-28 08:55:03 -0800

    Update Helmet configuration to change referrerPolicy to same-origin

---
+++ server/server.js
@@ -0,0 +1,11 @@
+app.use(helmet({
+  referrerPolicy: { policy: 'same-origin' },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'"],
+      imgSrc: ["'self'", 'data:', 'https:'],
+    },
+  },
+}));
