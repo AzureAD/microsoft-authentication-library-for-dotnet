b5f5c6869091257edce6c11833770ea2e41554d0
Gladwin Johnson <90415114+gladjohn@users.noreply.github.com>
2026-03-10 14:28:16 -0700

    initial

---
+++ src/client/Microsoft.Identity.Client/Internal/ClientCredential/CredentialSource.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Identity.Client.Internal.ClientCredential
+{
+    /// <summary>
+    /// Indicates whether credential material was resolved from a static (compile-time) source
+    /// or from a runtime callback.
+    /// </summary>
+    internal enum CredentialSource
+    {
+        /// <summary>The credential was supplied directly (e.g., a certificate or secret passed at build time).</summary>
+        Static,
+
+        /// <summary>The credential was obtained by invoking a user-supplied delegate at request time.</summary>
+        Callback
+    }
+}
--- src/client/Microsoft.Identity.Client/Internal/ClientCredential/SecretStringClientCredential.cs
+++ src/client/Microsoft.Identity.Client/Internal/ClientCredential/SecretStringClientCredential.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Identity.Client.Core;
-using Microsoft.Identity.Client.Internal.Requests;
 using Microsoft.Identity.Client.OAuth2;
-using Microsoft.Identity.Client.PlatformsCommon.Interfaces;
 using Microsoft.Identity.Client.TelemetryCore;
-using Microsoft.Identity.Client.Utils;
 
 namespace Microsoft.Identity.Client.Internal.ClientCredential
 {
@@ -23,15 +21,32 @@ namespace Microsoft.Identity.Client.Internal.ClientCredential
             Secret = secret;
         }
 
-        public Task<ClientCredentialApplicationResult> AddConfidentialClientParametersAsync(
-            OAuth2Client oAuth2Client,
-            AuthenticationRequestParameters requestParameters,
-            ICryptographyManager cryptographyManager, 
-            string tokenEndpoint, 
+        public Task<CredentialMaterial> GetCredentialMaterialAsync(
+            CredentialContext context,
             CancellationToken cancellationToken)
         {
-            oAuth2Client.AddBodyParameter(OAuth2Parameter.ClientSecret, Secret);
-            return Task.FromResult(ClientCredentialApplicationResult.None);
+            context.Logger.Verbose(() => $"[SecretStringClientCredential] Resolving credential material. " +
+            $"Mode={context.Mode}");
+
+            if (context.Mode == ClientAuthMode.MtlsMode)
+            {
+                context.Logger.Error("[SecretStringClientCredential] Client secret cannot be used with mTLS Proof-of-Possession.");
+
+                throw new MsalClientException(
+                    MsalError.InvalidCredentialMaterial,
+                    "A client secret cannot be used with mTLS Proof-of-Possession. " +
+                    "Use a certificate-based credential or a delegate that returns a ClientSignedAssertion " +
+                    "with a TokenBindingCertificate.");
+            }
+
+            var parameters = new Dictionary<string, string>
+            {
+                { OAuth2Parameter.ClientSecret, Secret }
+            };
+            
+            context.Logger.Verbose(() => "[SecretStringClientCredential] Secret-based credential material created successfully.");
+
+            return Task.FromResult(new CredentialMaterial(parameters, CredentialSource.Static));
         }
     }
 }
